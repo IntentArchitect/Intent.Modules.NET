38ff5250778718dc9e1e147b73ddded3a17cbe2c
dandrejvv <dandrejvv@gmail.com>
2021-03-07 08:19:21 +0200

    Services added

--- Tests/TestModules/ApiService_SelfHosted/ApiServiceSelfHostedApi/Startup.cs
+++ Tests/TestModules/ApiService_SelfHosted/ApiServiceSelfHostedApi/Startup.cs
@@ -33,6 +33,7 @@ namespace ApiServiceSelfHostedApi
 
             ConfigureSwagger(services);
 
+            services.AddTransient<Contracts.ITestService, Services.TestService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
