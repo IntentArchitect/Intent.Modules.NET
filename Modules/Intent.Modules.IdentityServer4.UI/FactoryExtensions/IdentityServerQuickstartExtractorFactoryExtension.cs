5c3a0479312398ffbd633492d97ce02bcad1b019
dandrejvv <dandrejvv@gmail.com>
2021-03-13 17:33:56 +0200

    I got it to work! We can now see the contents of the ZIP file being extracted to disk during the staging process!

--- Modules/Intent.Modules.IdentityServer4.UI/FactoryExtensions/IdentityServerQuickstartExtractorFactoryExtension.cs
+++ Modules/Intent.Modules.IdentityServer4.UI/FactoryExtensions/IdentityServerQuickstartExtractorFactoryExtension.cs
@@ -1,5 +1,6 @@
 using Intent.Engine;
 using Intent.Modules.Common.Plugins;
+using Intent.Modules.IdentityServer4.UI.Resources;
 using Intent.Plugins.FactoryExtensions;
 using Intent.RoslynWeaver.Attributes;
 using Intent.Utils;
@@ -21,32 +22,11 @@ namespace Intent.Modules.IdentityServer4.UI.FactoryExtensions
         public override int Order => 0;
 
         [IntentManaged(Mode.Ignore)]
-        const string ZipFileResourcePath = "Intent.Modules.IdentityServer4.UI" + ".Resources." + RootFolder + ".zip";
-        [IntentManaged(Mode.Ignore)]
-        const string RootFolder = "IdentityServer4.Quickstart.UI-main";
-
-        [IntentManaged(Mode.Ignore)]
-        private static readonly IReadOnlyCollection<string> TargetFolders = new string[]
-        {
-            "Quickstart",
-            "Views",
-            "wwwroot"
-        };
-
-        [IntentManaged(Mode.Ignore)]
-        private static readonly HashSet<string> FilesToIgnore = new HashSet<string>
-        {
-            Path.Combine(RootFolder, "Quickstart", "Account", "AccountController.cs").Replace("\\", "/"),
-            Path.Combine(RootFolder, "Quickstart", "Account", "ExternalController.cs").Replace("\\", "/"),
-            Path.Combine(RootFolder, "Quickstart", "TestUsers.cs").Replace("\\", "/")
-        };
-
-        [IntentManaged(Mode.Ignore)]
         public void OnStep(IApplication application, string step)
         {
             if (step == ExecutionLifeCycleSteps.AfterCommitChanges)
             {
-                Logging.Log.Info($"Extracting content of {RootFolder}.zip ...");
+                Logging.Log.Info($"Extracting content of {ResourceHelper.QuickstartFileName}.zip ...");
 
                 var targetApp = application.OutputTargets.SingleOrDefault(p => p.Parent == null);
                 if (targetApp == null)
@@ -55,45 +35,24 @@ namespace Intent.Modules.IdentityServer4.UI.FactoryExtensions
                     return;
                 }
 
-                var filecheckpath = Path.Combine(targetApp.Location, "Controllers", "Account", "AccountOptions.cs");
+                var filecheckpath = Path.Combine(targetApp.Location, "wwwroot", "favicon.ico");
                 if (File.Exists(filecheckpath)) { return; }
 
-                var zipFileStream = GetFileFromResource(ZipFileResourcePath);
-
-                ExtractFilesToRootPath(zipFileStream, targetApp.Location);
-
-                Logging.Log.Info($"Extraction complete.");
-            }
-        }
-
-        [IntentManaged(Mode.Ignore)]
-        private void ExtractFilesToRootPath(Stream zipFileStream, string rootLocation)
-        {
-            using (var archive = new ZipArchive(zipFileStream, ZipArchiveMode.Read))
-            {
-                foreach (var entry in archive.Entries.Where(p => p.Name != string.Empty))
+                ResourceHelper.ReadQuickstartFileContents(archive =>
                 {
-                    if (FilesToIgnore.Contains(entry.FullName)) { continue; }
-
-                    var pathComponents = entry.FullName.Split('/', StringSplitOptions.RemoveEmptyEntries);
-                    if (pathComponents.Intersect(TargetFolders).Any())
+                    foreach (var entry in archive.Entries
+                        .Where(p => p.Name != null && ResourceHelper.ImageFiles.Contains(p.Name)))
                     {
-                        var outpath = Path.Combine(rootLocation, entry.FullName
-                            .Replace(RootFolder + "/", string.Empty)
-                            .Replace("Quickstart/", "Controllers/"));
+                        var pathComponents = entry.FullName.Split('/', StringSplitOptions.RemoveEmptyEntries);
+                        var outpath = Path.Combine(targetApp.Location, entry.FullName.Replace(ResourceHelper.QuickstartFileName + "/", string.Empty));
                         var fileInfo = new FileInfo(outpath);
                         fileInfo.Directory.Create();
                         entry.ExtractToFile(outpath);
                     }
-                }
-            }
-        }
+                });
 
-        [IntentManaged(Mode.Ignore)]
-        private Stream GetFileFromResource(string resourcePath)
-        {
-            var stream = typeof(IdentityServerQuickstartExtractorFactoryExtension).Assembly.GetManifestResourceStream(resourcePath);
-            return stream;
+                Logging.Log.Info($"Extraction complete.");
+            }
         }
     }
 }
---
+++ Modules/Intent.Modules.IdentityServer4.UI/Resources/ResourceHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace Intent.Modules.IdentityServer4.UI.Resources
+{
+    public static class ResourceHelper
+    {
+        public const string QuickstartFileName = "IdentityServer4.Quickstart.UI-main";
+        const string ZipFileResourcePath = "Intent.Modules.IdentityServer4.UI" + ".Resources." + QuickstartFileName + ".zip";
+
+        public static readonly IReadOnlyCollection<string> ImageFiles = new string[] 
+        {
+            "favicon.ico",
+            "icon.jpg",
+            "icon.png"
+        };
+
+        public static void ReadQuickstartFileContents(Action<ZipArchive> readContentsAction)
+        {
+            var zipFileStream = GetFileFromResource(ZipFileResourcePath);
+            using (var archive = new ZipArchive(zipFileStream, ZipArchiveMode.Read))
+            {
+                readContentsAction(archive);
+            }
+        }
+
+        private static Stream GetFileFromResource(string resourcePath)
+        {
+            var stream = typeof(ResourceHelper).Assembly.GetManifestResourceStream(resourcePath);
+            return stream;
+        }
+    }
+}
