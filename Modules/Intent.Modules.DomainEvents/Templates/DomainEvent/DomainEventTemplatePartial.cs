e2a774960663bb431945b580ff7673af43ed0b92
garethbaars <gareth.baars@intentarchitect.com>
2022-02-14 21:57:28 +0200

    domain Events

--- Modules/Intent.Modules.DomainEvents/Templates/DomainEvent/DomainEventTemplatePartial.cs
+++ Modules/Intent.Modules.DomainEvents/Templates/DomainEvent/DomainEventTemplatePartial.cs
@@ -1,13 +1,13 @@
 using System.Collections.Generic;
 using Intent.Engine;
 using Intent.Modelers.Domain.Events.Api;
+using Intent.Modules.Common;
 using Intent.Modules.Common.CSharp.Templates;
 using Intent.Modules.Common.Templates;
 using Intent.Modules.DomainEvents.Templates.DomainEventBase;
 using Intent.Modules.Entities.Templates.DomainEntityState;
 using Intent.RoslynWeaver.Attributes;
 using Intent.Templates;
-using Intent.Modules.Common;
 
 [assembly: DefaultIntentManaged(Mode.Merge)]
 [assembly: IntentTemplate("Intent.ModuleBuilder.CSharp.Templates.CSharpTemplatePartial", Version = "1.0")]
--- Modules/Intent.Modules.DomainEvents/Templates/HasDomainEventInterface/HasDomainEventInterfaceTemplatePartial.cs
+++ Modules/Intent.Modules.DomainEvents/Templates/HasDomainEventInterface/HasDomainEventInterfaceTemplatePartial.cs
@@ -1,12 +1,11 @@
 using System.Collections.Generic;
 using Intent.Engine;
+using Intent.Modules.Common;
 using Intent.Modules.Common.CSharp.Templates;
 using Intent.Modules.Common.Templates;
 using Intent.Modules.DomainEvents.Templates.DomainEventBase;
 using Intent.RoslynWeaver.Attributes;
 using Intent.Templates;
-using Intent.Modules.Common;
-
 
 [assembly: DefaultIntentManaged(Mode.Merge)]
 [assembly: IntentTemplate("Intent.ModuleBuilder.CSharp.Templates.CSharpTemplatePartial", Version = "1.0")]
