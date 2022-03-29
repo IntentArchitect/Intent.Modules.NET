ff176921434ce3b75a208b36b175aa3bf449d7fb
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2022-03-29 20:32:26 +0200

    AB#397 - Upgrade client to .NET 6 and upgrade modules to .NET Core 3.1
    Upgrade to latest version of ModuleBuilder for all application, ensure no outstanding changes.

--- Modules/Intent.Modules.EntityFrameworkCore/Settings/ModuleSettingsExtensions.cs
+++ Modules/Intent.Modules.EntityFrameworkCore/Settings/ModuleSettingsExtensions.cs
@@ -1,6 +1,7 @@
 using System;
 using Intent.Configuration;
 using Intent.Engine;
+using Intent.Modules.Common.Templates;
 using Intent.RoslynWeaver.Attributes;
 
 [assembly: DefaultIntentManaged(Mode.Fully)]
