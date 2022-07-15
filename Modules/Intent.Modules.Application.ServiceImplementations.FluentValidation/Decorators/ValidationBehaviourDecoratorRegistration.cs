526a3f3b560128a0dc2d60a4200ad6d3c77ec247
dandrejvv <dandrejvv@gmail.com>
2022-07-15 08:34:18 +0200

    Moved from Azurefunctions FluentValidation to ServiceImplementation FluentValidations
    AB#878

---
+++ Modules/Intent.Modules.Application.ServiceImplementations.FluentValidation/Decorators/ValidationBehaviourDecoratorRegistration.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel;
+using Intent.Engine;
+using Intent.Modules.Application.FluentValidation.Templates.ValidationBehaviour;
+using Intent.Modules.Common.Registrations;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.Templates.TemplateDecoratorRegistration", Version = "1.0")]
+
+namespace Intent.Modules.Application.ServiceImplementations.FluentValidation.Decorators
+{
+    [Description(ValidationBehaviourDecorator.DecoratorId)]
+    public class ValidationBehaviourDecoratorRegistration : DecoratorRegistration<ValidationBehaviourTemplate, ValidationBehaviourContract>
+    {
+        public override ValidationBehaviourContract CreateDecoratorInstance(ValidationBehaviourTemplate template, IApplication application)
+        {
+            return new ValidationBehaviourDecorator(template, application);
+        }
+
+        public override string DecoratorId => ValidationBehaviourDecorator.DecoratorId;
+    }
+}
--- Modules/Intent.Modules.AzureFunctions.FluentValidation/NuGetPackages.cs
+++
@@ -1,9 +0,0 @@
-﻿using Intent.Modules.Common.VisualStudio;
-
-namespace Intent.Modules.AzureFunctions.FluentValidation
-{
-    public static class NuGetPackages
-    {
-        public static INugetPackageInfo FluentValidation = new NugetPackageInfo("FluentValidation", "9.3.0");
-    }
-}
