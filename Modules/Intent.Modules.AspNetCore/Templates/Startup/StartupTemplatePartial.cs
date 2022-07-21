7f657de6c27d055d9fee93a4c41e3dc4f99c84c9
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2022-07-21 10:17:31 +0200

    Fix issue where namespaces not always being added to startup file.
    Clean-up file, no logic changes.

--- Modules/Intent.Modules.AspNetCore/Templates/Startup/StartupTemplatePartial.cs
+++ Modules/Intent.Modules.AspNetCore/Templates/Startup/StartupTemplatePartial.cs
@@ -3,15 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Intent.Engine;
-using Intent.Eventing;
 using Intent.Modules.Common;
-using Intent.Modules.Common.CSharp;
 using Intent.Modules.Common.CSharp.DependencyInjection;
 using Intent.Modules.Common.CSharp.Templates;
-using Intent.Modules.Common.CSharp.TypeResolvers;
 using Intent.Modules.Common.Templates;
 using Intent.Modules.Common.VisualStudio;
-using Intent.Modules.Constants;
 using Intent.RoslynWeaver.Attributes;
 using Intent.Templates;
 
@@ -25,31 +21,30 @@ namespace Intent.Modules.AspNetCore.Templates.Startup
     {
         [IntentManaged(Mode.Fully)] public const string TemplateId = "Intent.AspNetCore.Startup";
 
-        private readonly IList<ContainerRegistrationRequest> _registrations = new List<ContainerRegistrationRequest>();
-
-        private readonly IList<ServiceConfigurationRequest> _serviceConfigurations =
-            new List<ServiceConfigurationRequest>();
+        private readonly List<ContainerRegistrationRequest> _containerRegistrationRequests = new();
+        private readonly List<ServiceConfigurationRequest> _serviceConfigurationRequests = new();
 
         [IntentManaged(Mode.Merge, Signature = Mode.Fully)]
         public StartupTemplate(IOutputTarget outputTarget, object model = null) : base(TemplateId, outputTarget, model)
         {
-            ExecutionContext.EventDispatcher.Subscribe<ContainerRegistrationRequest>(HandleServiceRegistration);
-            ExecutionContext.EventDispatcher.Subscribe<ServiceConfigurationRequest>(HandleServiceConfiguration);
-            //eventDispatcher.Subscribe(ContainerRegistrationForDbContextEvent.EventId, HandleDbContextRegistration);
-            //eventDispatcher.Subscribe(ServiceConfigurationRequiredEvent.EventId, HandleServiceConfiguration);
-            //eventDispatcher.Subscribe(InitializationRequiredEvent.EventId, HandleInitialization);
+            ExecutionContext.EventDispatcher.Subscribe<ContainerRegistrationRequest>(Handle);
+            ExecutionContext.EventDispatcher.Subscribe<ServiceConfigurationRequest>(Handle);
         }
 
-        private void HandleServiceRegistration(ContainerRegistrationRequest @event)
+        private void Handle(ContainerRegistrationRequest request)
         {
-            _registrations.Add(@event);
+            _containerRegistrationRequests.Add(request);
         }
 
-        private void HandleServiceConfiguration(ServiceConfigurationRequest request)
+        private void Handle(ServiceConfigurationRequest request)
         {
-            _serviceConfigurations.Add(request);
+            _serviceConfigurationRequests.Add(request);
         }
 
+        /// <remarks>
+        /// It's a bit of a smell that a "Get" adds dependencies, but we can't process AddUsings in
+        /// the Handle methods as not events might have been published yet.
+        /// </remarks>
         private string GetServiceConfigurations(string baseIndent)
         {
             var serviceConfigElements = new List<(string Code, int Priority)>();
@@ -57,7 +52,7 @@ namespace Intent.Modules.AspNetCore.Templates.Startup
             serviceConfigElements.AddRange(GetDecorators()
                 .Select(s => (s.ConfigureServices(), s.Priority)));
 
-            var serviceConfigurationRequests = _serviceConfigurations
+            var serviceConfigurationRequests = _serviceConfigurationRequests
                 .Where(x => !x.IsHandled)
                 .ToArray();
 
@@ -87,12 +82,12 @@ namespace Intent.Modules.AspNetCore.Templates.Startup
             return OutputTarget.IsNetCore2App();
         }
 
-        private string GetExtensionMethodInvocationStatement(ServiceConfigurationRequest request)
+        private static string GetExtensionMethodInvocationStatement(ServiceConfigurationRequest request)
         {
             return $"services.{request.ExtensionMethodName}({GetExtensionMethodParameterList(request)});";
         }
 
-        private string GetExtensionMethodParameterList(ServiceConfigurationRequest request)
+        private static string GetExtensionMethodParameterList(ServiceConfigurationRequest request)
         {
             if (request.ExtensionMethodParameterList?.Any() != true)
             {
@@ -153,114 +148,33 @@ app.UseEndpoints(endpoints =>
             return GetCodeInNeatLines(endpointMappings, "                ");
         }
 
-        private string GetCodeInNeatLines(List<(string Code, int Priority)> codeSections, string baseIndent)
+        private string GetCodeInNeatLines(IEnumerable<(string Code, int Priority)> codeSections, string baseIndent)
         {
-            codeSections.Sort(Comparer<(string Code, int Priority)>
-                .Create((x, y) => x.Priority.CompareTo(y.Priority)));
-
             var sb = new StringBuilder();
-            base.PushIndent(baseIndent);
+            PushIndent(baseIndent);
 
-            foreach (var element in codeSections)
+            foreach (var element in codeSections.OrderBy(x => x.Priority))
             {
                 var codeLines = element.Code
                     .Trim()
                     .Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
                 foreach (var line in codeLines)
                 {
-                    sb.Append(base.CurrentIndent).AppendLine(line);
+                    sb.Append(CurrentIndent).AppendLine(line);
                 }
             }
 
-            base.PopIndent();
+            PopIndent();
             return sb.ToString();
         }
 
-        //private void HandleDbContextRegistration(ApplicationEvent @event)
-        //{
-        //    var registration = new DbContextContainerRegistration(
-        //        @event.TryGetValue(ContainerRegistrationForDbContextEvent.UsingsKey),
-        //        @event.GetValue(ContainerRegistrationForDbContextEvent.ConcreteTypeKey),
-        //        @event.TryGetValue(ContainerRegistrationForDbContextEvent.ConcreteTypeTemplateIdKey) != null ? TemplateDependency.OnTemplate(@event.TryGetValue(ContainerRegistrationForDbContextEvent.ConcreteTypeTemplateIdKey)) : null,
-        //        @event.TryGetValue(ContainerRegistrationForDbContextEvent.OptionsKey),
-        //        @event.TryGetValue(ContainerRegistrationForDbContextEvent.NugetDependency),
-        //        @event.TryGetValue(ContainerRegistrationForDbContextEvent.NugetDependencyVersion));
-        //    _dbContextRegistrations.Add(registration);
-        //    if (registration.NugetPackage != null)
-        //    {
-        //        AddNugetDependency(registration.NugetPackage);
-        //    }
-        //}
-
-        //private void HandleServiceConfiguration(ApplicationEvent @event)
-        //{
-        //    _serviceConfigurations.Add(new Initializations(
-        //        usings: @event.GetValue(ServiceConfigurationRequiredEvent.UsingsKey),
-        //        code: @event.GetValue(ServiceConfigurationRequiredEvent.CallKey),
-        //        method: @event.TryGetValue(ServiceConfigurationRequiredEvent.MethodKey),
-        //        priority: int.TryParse(@event.TryGetValue(ServiceConfigurationRequiredEvent.PriorityKey), out var priority) ? priority : 0,
-        //        templateDependency: null));
-        //}
-
-        //private void HandleInitialization(ApplicationEvent @event)
-        //{
-        //    _initializations.Add(new Initializations(
-        //        usings: @event.GetValue(InitializationRequiredEvent.UsingsKey),
-        //        code: @event.GetValue(InitializationRequiredEvent.CallKey),
-        //        method: @event.TryGetValue(InitializationRequiredEvent.MethodKey),
-        //        priority: int.TryParse(@event.TryGetValue(InitializationRequiredEvent.PriorityKey), out var priority) ? priority : 0,
-        //        templateDependency: @event.TryGetValue(InitializationRequiredEvent.TemplateDependencyIdKey) != null ? TemplateDependency.OnTemplate(@event.TryGetValue(InitializationRequiredEvent.TemplateDependencyIdKey)) : null));
-        //}
-
-        //public string ServiceConfigurations()
-        //{
-        //    var configurations = _serviceConfigurations.Select(x => x.Code).ToList();
-
-        //    if (!configurations.Any())
-        //    {
-        //        return string.Empty;
-        //    }
-
-        //    const string tabbing = "            ";
-        //    return Environment.NewLine +
-        //           configurations
-        //               .Select(x => x.Trim())
-        //               .Select(x => x.StartsWith("#") ? x : $"{tabbing}{x}")
-        //               .Aggregate((x, y) => $"{x}{Environment.NewLine}" +
-        //                                    $"{y}");
-        //}
-
-        //public void AddDecorator(StartupDecorator decorator)
-        //{
-        //    _decorators.Add(decorator);
-        //}
-
-        //public IEnumerable<StartupDecorator> GetDecorators()
-        //{
-        //    return _decorators;
-        //}
-
-        //public string Configurations()
-        //{
-        //    var configurations = _initializations.Select(x => x.Code).ToList();
-
-        //    if (!configurations.Any())
-        //    {
-        //        return string.Empty;
-        //    }
-
-        //    const string tabbing = "            ";
-        //    return Environment.NewLine +
-        //           configurations
-        //               .Select(x => x.Trim())
-        //               .Select(x => x.StartsWith("#") ? x : $"{tabbing}{x}")
-        //               .Aggregate((x, y) => $"{x}{Environment.NewLine}" +
-        //                                    $"{y}");
-        //}
-
-        public string Registrations()
+        /// <remarks>
+        /// It's a bit of a smell that a "Get" adds dependencies, but we can't process AddUsings in
+        /// the Handle methods as not events might have been published yet.
+        /// </remarks>
+        public string GetContainerRegistrations()
         {
-            var containerRegistrationRequests = _registrations
+            var containerRegistrationRequests = _containerRegistrationRequests
                 .Where(x => !x.IsHandled)
                 .ToArray();
 
@@ -283,45 +197,6 @@ app.UseEndpoints(endpoints =>
             return string.Concat(containerRegistrationRequests.Select(DefineServiceRegistration));
         }
 
-        //public string Methods()
-        //{
-        //    var methods = _initializations.Concat(_serviceConfigurations)
-        //        .Where(x => !string.IsNullOrWhiteSpace(x.Method))
-        //        .OrderBy(x => x.Priority)
-        //        .Select(x => x.Method)
-        //        .ToList();
-
-        //    if (_dbContextRegistrations.Any())
-        //    {
-        //        var dbContextRegistration = string.Empty;
-        //        dbContextRegistration += $"{Environment.NewLine}        private void ConfigureDbContext(IServiceCollection services)";
-        //        dbContextRegistration += $"{Environment.NewLine}        {{";
-        //        dbContextRegistration += _dbContextRegistrations.Select(DefineDbContextRegistration).Aggregate((x, y) => x + y);
-        //        dbContextRegistration += $"{Environment.NewLine}        }}";
-        //        methods.Add(dbContextRegistration);
-        //    }
-
-        //    if (!methods.Any())
-        //    {
-        //        return string.Empty;
-        //    }
-
-        //    const string tabbing = "        ";
-        //    return Environment.NewLine +
-        //           Environment.NewLine +
-        //           methods
-        //               .Select(x => x.Trim())
-        //               .Select(x => $"{tabbing}{x}")
-        //               .Aggregate((x, y) => $"{x}{Environment.NewLine}" +
-        //                                    $"{Environment.NewLine}" +
-        //                                    $"{y}");
-        //}
-
-        //private string DefineDbContextRegistration(DbContextContainerRegistration x)
-        //{
-        //    return $"{Environment.NewLine}            services.AddDbContext<{UseType(x.ConcreteType)}>({(x.Options != null ? $"x => x{x.Options}" : string.Empty)});";
-        //}
-
         private string DefineServiceRegistration(ContainerRegistrationRequest x)
         {
             string UseTypeOf(string type)
@@ -342,119 +217,22 @@ app.UseEndpoints(endpoints =>
                 : $"{Environment.NewLine}            services.{RegistrationType(x)}<{UseType(x.ConcreteType)}>();";
         }
 
-        private string RegistrationType(ContainerRegistrationRequest registration)
+        private static string RegistrationType(ContainerRegistrationRequest registration)
         {
-            switch (registration.Lifetime)
+            return registration.Lifetime switch
             {
-                case ContainerRegistrationRequest.LifeTime.Singleton:
-                    return "AddSingleton";
-                case ContainerRegistrationRequest.LifeTime.PerServiceCall:
-                    return "AddScoped";
-                case ContainerRegistrationRequest.LifeTime.Transient:
-                    return "AddTransient";
-                default:
-                    return "AddTransient";
-            }
+                ContainerRegistrationRequest.LifeTime.Singleton => "AddSingleton",
+                ContainerRegistrationRequest.LifeTime.PerServiceCall => "AddScoped",
+                _ => "AddTransient"
+            };
         }
 
         [IntentManaged(Mode.Fully, Body = Mode.Ignore)]
         protected override CSharpFileConfig DefineFileConfig()
         {
             return new CSharpFileConfig(
-                className: $"Startup",
+                className: "Startup",
                 @namespace: $"{OutputTarget.GetNamespace()}");
         }
-
-        //public override IEnumerable<ITemplateDependency> GetTemplateDependencies()
-        //{
-        //    return base.GetTemplateDependencies().Concat(GetServiceRegistrations().SelectMany(x => x.TemplateDependencies)
-        //        //.Where(x => x.InterfaceType != null && x.InterfaceTypeTemplateDependency != null)
-        //        //.Select(x => x.InterfaceTypeTemplateDependency)
-        //        //.Union(_registrations
-        //        //    .Where(x => x.ConcreteTypeTemplateDependency != null)
-        //        //    .Select(x => x.ConcreteTypeTemplateDependency))
-        //        .Union(_initializations
-        //            .Where(x => x.TemplateDependancy != null)
-        //            .Select(x => x.TemplateDependancy))
-        //        .Union(_dbContextRegistrations
-        //            .Where(x => x.ConcreteTypeTemplateDependency != null)
-        //            .Select(x => x.ConcreteTypeTemplateDependency))
-        //        .ToList());
-        //}
-
-        //public IEnumerable<string> DeclareUsings()
-        //{
-        //    return _dbContextRegistrations.Select(x => x.Usings)
-        //        .Concat(_initializations.Select(x => x.Usings))
-        //        .Concat(_serviceConfigurations.Select(x => x.Usings))
-        //        .Select(x => x.Split(';'))
-        //        .SelectMany(x => x)
-        //        .Where(x => !string.IsNullOrWhiteSpace(x))
-        //        .Select(x => x.Trim());
-        //}
-
-        //private bool IsNetCore2App() // Dirty way to get this info. Should not have this dependency
-        //{
-        //    return Project.TargetFramework().StartsWith("netcoreapp2");
-        //}
-
-        //internal class ContainerRegistration
-        //{
-        //    public ContainerRegistration(string interfaceType, string concreteType, string lifetime, ITemplateDependency interfaceTypeTemplateDependency, ITemplateDependency concreteTypeTemplateDependency)
-        //    {
-        //        InterfaceType = interfaceType;
-        //        ConcreteType = concreteType;
-        //        Lifetime = lifetime ?? "Transient";
-        //        InterfaceTypeTemplateDependency = interfaceTypeTemplateDependency;
-        //        ConcreteTypeTemplateDependency = concreteTypeTemplateDependency;
-        //    }
-
-        //    public string InterfaceType { get; }
-        //    public string ConcreteType { get; }
-        //    public string Lifetime { get; }
-        //    public ITemplateDependency InterfaceTypeTemplateDependency { get; }
-        //    public ITemplateDependency ConcreteTypeTemplateDependency { get; }
-        //}
-
-        //internal class DbContextContainerRegistration
-        //{
-        //    public DbContextContainerRegistration(string usings, string concreteType,
-        //        ITemplateDependency concreteTypeTemplateDependency, string options, string nugetDependency,
-        //        string nugetDependencyVersion)
-        //    {
-        //        Usings = usings;
-        //        ConcreteType = concreteType;
-        //        ConcreteTypeTemplateDependency = concreteTypeTemplateDependency;
-        //        Options = options;
-        //        if (!string.IsNullOrWhiteSpace(nugetDependency) && !string.IsNullOrWhiteSpace(nugetDependencyVersion))
-        //        {
-        //            NugetPackage = new NugetPackageInfo(nugetDependency, nugetDependencyVersion);
-        //        }
-        //    }
-
-        //    public string Usings { get; }
-        //    public string ConcreteType { get; }
-        //    public ITemplateDependency ConcreteTypeTemplateDependency { get; }
-        //    public string Options { get; }
-        //    public NugetPackageInfo NugetPackage { get; }
-        //}
-
-        //internal class Initializations
-        //{
-        //    public string Usings { get; }
-        //    public string Code { get; }
-        //    public string Method { get; }
-        //    public int Priority { get; }
-        //    public ITemplateDependency TemplateDependancy { get; }
-
-        //    public Initializations(string usings, string code, string method, int priority, ITemplateDependency templateDependency)
-        //    {
-        //        Usings = usings;
-        //        Code = code;
-        //        Method = method;
-        //        Priority = priority;
-        //        TemplateDependancy = templateDependency;
-        //    }
-        //}
     }
 }
