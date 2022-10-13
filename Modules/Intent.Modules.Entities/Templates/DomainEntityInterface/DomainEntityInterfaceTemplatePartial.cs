d15ebac75690b025c251d95af7a025ad43afeca7
dandrejvv <dandrejvv@gmail.com>
2022-10-13 14:29:23 +0200

    This should fix the issue where a nullable Enum didn't reflect in the Interface

--- Modules/Intent.Modules.Entities/Templates/DomainEntityInterface/DomainEntityInterfaceTemplatePartial.cs
+++ Modules/Intent.Modules.Entities/Templates/DomainEntityInterface/DomainEntityInterfaceTemplatePartial.cs
@@ -33,6 +33,7 @@ namespace Intent.Modules.Entities.Templates.DomainEntityInterface
         public DomainEntityInterfaceTemplate(IOutputTarget outputTarget, ClassModel model) : base(TemplateId, outputTarget, model)
         {
             _metadataManager = ExecutionContext.MetadataManager;
+            AddTypeSource(DomainEnumTemplate.TemplateId).WithCollectionFormat("ICollection<{0}>");
             AddTypeSource("Domain.ValueObject", "ICollection<{0}>");
         }
 
