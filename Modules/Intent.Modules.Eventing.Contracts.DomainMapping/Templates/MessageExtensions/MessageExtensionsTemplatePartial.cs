8fd1b91ab7c82564e57a7726064d3bd8917bf159
dandrejvv <dandrejvv@gmail.com>
2022-12-11 23:47:22 +0200

    Code that can be leveraged later in case

--- Modules/Intent.Modules.Eventing.Contracts.DomainMapping/Templates/MessageExtensions/MessageExtensionsTemplatePartial.cs
+++ Modules/Intent.Modules.Eventing.Contracts.DomainMapping/Templates/MessageExtensions/MessageExtensionsTemplatePartial.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Intent.Engine;
 using Intent.Eventing.Contracts.DomainMapping.Api;
+using Intent.Metadata.Models;
+using Intent.Modelers.Domain.Api;
 using Intent.Modelers.Eventing.Api;
 using Intent.Modules.Common;
 using Intent.Modules.Common.CSharp.Builder;
@@ -10,6 +13,7 @@ using Intent.Modules.Common.Templates;
 using Intent.Modules.Eventing.Contracts.Templates.IntegrationEventMessage;
 using Intent.RoslynWeaver.Attributes;
 using Intent.Templates;
+using JetBrains.Annotations;
 
 [assembly: DefaultIntentManaged(Mode.Fully)]
 [assembly: IntentTemplate("Intent.ModuleBuilder.CSharp.Templates.CSharpTemplatePartial", Version = "1.0")]
@@ -37,10 +41,13 @@ namespace Intent.Modules.Eventing.Contracts.DomainMapping.Templates.MessageExten
                         method.Static();
                         method.AddParameter(GetTypeName(model.GetMapFromDomainMapping()), "projectFrom", param => param.WithThisModifier());
 
+                        var domainEntity = ((IElement)model.GetMapFromDomainMapping().Element).AsClassModel();
+                        
                         var codeLines = new CSharpStatementAggregator();
                         codeLines.Add($"return new {GetTypeName(model.InternalElement)}");
                         codeLines.Add(new CSharpStatementBlock()
                             .AddStatements(model.Properties.Select(x => $"{x.Name.ToCSharpIdentifier(CapitalizationBehaviour.AsIs)} = projectFrom.{string.Join(".", x.InternalElement.MappedElement.Path.Select(y => y.Name))},"))
+                            //.AddStatements(GetMessagePropertyAssignments("projectFrom", domainEntity, model.Properties))
                             .WithSemicolon());
                         method.AddStatements(codeLines.ToList());
                     });
@@ -61,5 +68,90 @@ namespace Intent.Modules.Eventing.Contracts.DomainMapping.Templates.MessageExten
         {
             return CSharpFile.ToString();
         }
+        
+        // When we do need more advanced mapping, we can use this. 
+        
+        // private IEnumerable<CSharpStatement> GetMessagePropertyAssignments(string domainEntityVar, ClassModel domainModel, IList<PropertyModel> messageProperties)
+        // {
+        //     var codeLines = new List<CSharpStatement>();
+        //     foreach (var propertyModel in messageProperties)
+        //     {
+        //         var mappedPropertyElement = propertyModel.InternalElement.MappedElement; 
+        //         
+        //         if (mappedPropertyElement?.Element == null
+        //             && domainModel.Attributes.All(p => p.Name != propertyModel.Name))
+        //         {
+        //             codeLines.Add($"#warning No matching property found for {propertyModel.Name}");
+        //             continue;
+        //         }
+        //
+        //         var entityVarExpr = !string.IsNullOrWhiteSpace(domainEntityVar) ? $"{domainEntityVar}." : string.Empty;
+        //         switch (mappedPropertyElement?.Element?.SpecializationTypeId)
+        //         {
+        //             default:
+        //                 var mappedPropertyName = mappedPropertyElement?.Element?.Name ?? "<null>";
+        //                 codeLines.Add($"#warning No matching type for Domain: {mappedPropertyName} and Message: {propertyModel.Name}");
+        //                 break;
+        //             case null:
+        //             case AttributeModel.SpecializationTypeId:
+        //                 var attribute = mappedPropertyElement.Element.AsAttributeModel()
+        //                                 ?? domainModel.Attributes.First(p => p.Name == propertyModel.Name);
+        //                 
+        //                 codeLines.Add($"{propertyModel.Name.ToPascalCase()} = {entityVarExpr}{attribute.Name.ToPascalCase()},");
+        //
+        //                 break;
+        //             case AssociationTargetEndModel.SpecializationTypeId:
+        //             {
+        //                 var association = mappedPropertyElement.Element.AsAssociationTargetEndModel();
+        //                 var targetType = association.Element.AsClassModel();
+        //                 var attributeName = association.Name.ToPascalCase();
+        //
+        //                 if (association.Association.AssociationType == AssociationType.Aggregation)
+        //                 {
+        //                     codeLines.Add($@"#warning Field not a composite association: {propertyModel.Name.ToPascalCase()}");
+        //                     break;
+        //                 }
+        //
+        //                 if (association.Multiplicity is Multiplicity.One or Multiplicity.ZeroToOne)
+        //                 {
+        //                     if (association.IsNullable)
+        //                     {
+        //                         codeLines.Add(
+        //                             $"{propertyModel.Name.ToPascalCase()} = {entityVarExpr}{attributeName} != null ? {GetCreateMethodName(targetType, attributeName)}({entityVarExpr}{propertyModel.Name.ToPascalCase()}) : null,");
+        //                     }
+        //                     else
+        //                     {
+        //                         codeLines.Add($"{entityVarExpr}{attributeName} = {GetCreateMethodName(targetType, attributeName)}({entityVarExpr}{propertyModel.Name.ToPascalCase()}),");
+        //                     }
+        //                 }
+        //                 else
+        //                 {
+        //                     codeLines.Add(
+        //                         $"{entityVarExpr}{attributeName} = {entityVarExpr}{propertyModel.Name.ToPascalCase()}{(association.IsNullable ? "?" : "")}.Select({GetCreateMethodName(targetType, attributeName)}).ToList(),");
+        //                 }
+        //
+        //                 var @class = this.CSharpFile.Classes.First();
+        //                 @class.AddMethod(this.GetTypeName(targetType.InternalElement),
+        //                     GetCreateMethodName(targetType, attributeName),
+        //                     method => method.Private()
+        //                         .AddAttribute(CSharpIntentManagedAttribute.Fully())
+        //                         .AddParameter(this.GetTypeName((IElement)propertyModel.TypeReference.Element), "dto")
+        //                         .AddStatement($"return new {targetType.Name.ToPascalCase()}")
+        //                         .AddStatement(new CSharpStatementBlock()
+        //                             .AddStatements(GetMessagePropertyAssignments("projectFrom", targetType,
+        //                                 ((IElement)propertyModel.TypeReference.Element).ChildElements.Where(x => x.IsPropertyModel()).Select(x => x.AsPropertyModel()).ToList()))
+        //                             .WithSemicolon()));
+        //             }
+        //                 break;
+        //         }
+        //     }
+        //
+        //     return codeLines;
+        // }
+        
+        // private string GetCreateMethodName(ClassModel classModel, [CanBeNull] string attributeName)
+        // {
+        //     return $"Create{(!string.IsNullOrEmpty(attributeName) ? attributeName : string.Empty)}{classModel.Name.ToPascalCase()}";
+        // }
     }
 }
