ef923f4f64ba075812979af8a22b10081b78845d
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2023-02-16 15:26:39 +0200

    AB#1801 - ASP.NET Identity management
    - Renamed module, now updates JWT configuration so that can consume generated JWTs.

---
+++ Modules/Intent.Modules.AspNetCore.Identity.AccountController/Templates/AccountController/AccountControllerTemplatePartial.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using Intent.Engine;
+using Intent.Modules.Common;
+using Intent.Modules.Common.CSharp.Templates;
+using Intent.Modules.Common.Templates;
+using Intent.RoslynWeaver.Attributes;
+using Intent.Templates;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.CSharp.Templates.CSharpTemplatePartial", Version = "1.0")]
+
+namespace Intent.Modules.AspNetCore.Identity.AccountController.Templates.AccountController
+{
+    [IntentManaged(Mode.Fully, Body = Mode.Merge)]
+    partial class AccountControllerTemplate : CSharpTemplateBase<object>
+    {
+        public const string TemplateId = "Intent.AspNetCore.Identity.AccountController.AccountController";
+
+        [IntentManaged(Mode.Fully, Body = Mode.Ignore)]
+        public AccountControllerTemplate(IOutputTarget outputTarget, object model = null) : base(TemplateId, outputTarget, model)
+        {
+        }
+
+        public override void BeforeTemplateExecution()
+        {
+            var randomBytesBuffer = new byte[32];
+            Random.Shared.NextBytes(randomBytesBuffer);
+
+            this.ApplyAppSetting("JwtToken", new
+            {
+                Issuer = OutputTarget.ExecutionContext.GetApplicationConfig().Name,
+                Audience = OutputTarget.ExecutionContext.GetApplicationConfig().Name,
+                SigningKey = Convert.ToBase64String(randomBytesBuffer)
+            });
+        }
+
+        [IntentManaged(Mode.Fully, Body = Mode.Ignore)]
+        protected override CSharpFileConfig DefineFileConfig()
+        {
+            return new CSharpFileConfig(
+                className: $"AccountController",
+                @namespace: $"{this.GetNamespace()}",
+                relativeLocation: $"{this.GetFolderPath()}");
+        }
+    }
+}
---
+++ Modules/Intent.Modules.AspNetCore.Identity.AccountController/Templates/AccountEmailSender/AccountEmailSenderTemplatePartial.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Intent.Engine;
+using Intent.Modules.AspNetCore.Identity.AccountController.Templates.AccountEmailSenderInterface;
+using Intent.Modules.Common;
+using Intent.Modules.Common.CSharp.DependencyInjection;
+using Intent.Modules.Common.CSharp.Templates;
+using Intent.Modules.Common.Templates;
+using Intent.RoslynWeaver.Attributes;
+using Intent.Templates;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.CSharp.Templates.CSharpTemplatePartial", Version = "1.0")]
+
+namespace Intent.Modules.AspNetCore.Identity.AccountController.Templates.AccountEmailSender
+{
+    [IntentManaged(Mode.Fully, Body = Mode.Merge)]
+    partial class AccountEmailSenderTemplate : CSharpTemplateBase<object>
+    {
+        public const string TemplateId = "Intent.AspNetCore.Identity.AccountController.AccountEmailSender";
+
+        [IntentManaged(Mode.Fully, Body = Mode.Ignore)]
+        public AccountEmailSenderTemplate(IOutputTarget outputTarget, object model = null) : base(TemplateId, outputTarget, model)
+        {
+        }
+
+        public override void BeforeTemplateExecution()
+        {
+            ExecutionContext.EventDispatcher.Publish(ContainerRegistrationRequest.ToRegister(this).ForInterface(GetTemplate<AccountEmailSenderInterfaceTemplate>(AccountEmailSenderInterfaceTemplate.TemplateId)));
+        }
+
+        [IntentManaged(Mode.Fully, Body = Mode.Ignore)]
+        protected override CSharpFileConfig DefineFileConfig()
+        {
+            return new CSharpFileConfig(
+                className: $"AccountEmailSender",
+                @namespace: $"{this.GetNamespace()}",
+                relativeLocation: $"{this.GetFolderPath()}");
+        }
+    }
+}
