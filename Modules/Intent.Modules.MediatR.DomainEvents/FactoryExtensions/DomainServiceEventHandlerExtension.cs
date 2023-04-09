10e4eb2c78ad62a2f4d4342cc5914fbdc515d2e2
Gareth Baars <gareth.baars@intentarchitect.com>
2023-04-09 10:38:30 +0200

    MediatR.DomainEvents 4.1.0-pre.0 - Upgraded DomainEvents to integrate with DomainServices; Enhanced the associations too.

---
+++ Modules/Intent.Modules.MediatR.DomainEvents/FactoryExtensions/DomainServiceEventHandlerExtension.cs
@@ -0,0 +1,60 @@
+using System.Linq;
+using System.Reflection;
+using Intent.Engine;
+using Intent.Metadata.Models;
+using Intent.Modelers.Domain.Api;
+using Intent.Modelers.Domain.Events.Api;
+using Intent.Modules.Common;
+using Intent.Modules.Common.CSharp.Builder;
+using Intent.Modules.Common.CSharp.Templates;
+using Intent.Modules.Common.Plugins;
+using Intent.Modules.Common.Templates;
+using Intent.Modules.DomainEvents.Templates.DomainEvent;
+using Intent.Modules.MediatR.DomainEvents.Templates;
+using Intent.Modules.MediatR.DomainEvents.Templates.DomainEventNotification;
+using Intent.Plugins.FactoryExtensions;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.Templates.FactoryExtension", Version = "1.0")]
+
+namespace Intent.Modules.MediatR.DomainEvents.FactoryExtensions
+{
+    [IntentManaged(Mode.Fully, Body = Mode.Merge)]
+    public class DomainServiceEventHandlerExtension : FactoryExtensionBase
+    {
+        public override string Id => "Intent.MediatR.DomainEvents.DomainServiceEventHandlerExtension";
+
+        [IntentManaged(Mode.Ignore)]
+        public override int Order => 0;
+
+        protected override void OnAfterTemplateRegistrations(IApplication application)
+        {
+            var domainEvents = application.MetadataManager.Domain(application).GetDomainEventModels();
+            foreach (var domainEvent in domainEvents)
+            {
+                foreach (var targetHandler in domainEvent.AssociatedClasses().Select(x => x.Element).Distinct().ToList())
+                {
+                    var template = application.FindTemplateInstance<ICSharpFileBuilderTemplate>(TemplateDependency.OnModel("Domain.DomainServices.Implementation", targetHandler));
+                    template?.CSharpFile.OnBuild(file =>
+                    {
+                        file.AddUsing("System");
+                        file.AddUsing("System.Threading");
+                        file.AddUsing("System.Threading.Tasks");
+
+                        var @class = file.Classes.First();
+                        @class.ImplementsInterface($"{template.UseType("MediatR.INotificationHandler")}<{template.GetTypeName(DomainEventNotificationTemplate.TemplateId)}<{template.GetTypeName(DomainEventTemplate.TemplateId, domainEvent)}>>");
+                        @class.AddMethod("Task", "Handle", method =>
+                        {
+                            method.Async();
+                            method.AddAttribute(CSharpIntentManagedAttribute.IgnoreBody());
+                            method.AddParameter($"{template.GetDomainEventNotificationName()}<{template.GetTypeName(DomainEventTemplate.TemplateId, domainEvent)}>", "notification");
+                            method.AddParameter($"CancellationToken", "cancellationToken");
+                            method.AddStatement("throw new NotImplementedException(\"Implement your handler logic here...\");");
+                        });
+                    });
+                }
+            }
+        }
+    }
+}
--- Modules/Intent.Modules.MediatR.DomainEvents/Templates/TemplateExtensions.cs
+++ Modules/Intent.Modules.MediatR.DomainEvents/Templates/TemplateExtensions.cs
@@ -13,32 +13,32 @@ namespace Intent.Modules.MediatR.DomainEvents.Templates
 {
     public static class TemplateExtensions
     {
-        public static string GetAggregateManagerName<T>(this IntentTemplateBase<T> template) where T : Intent.Modelers.Domain.Api.ClassModel
+        public static string GetAggregateManagerName<T>(this IIntentTemplate<T> template) where T : Intent.Modelers.Domain.Api.ClassModel
         {
             return template.GetTypeName(AggregateManagerTemplate.TemplateId, template.Model);
         }
 
-        public static string GetAggregateManagerName(this IntentTemplateBase template, Intent.Modelers.Domain.Api.ClassModel model)
+        public static string GetAggregateManagerName(this IIntentTemplate template, Intent.Modelers.Domain.Api.ClassModel model)
         {
             return template.GetTypeName(AggregateManagerTemplate.TemplateId, model);
         }
 
-        public static string GetDefaultDomainEventHandlerName<T>(this IntentTemplateBase<T> template) where T : Intent.Modelers.Domain.Events.Api.DomainEventModel
+        public static string GetDefaultDomainEventHandlerName<T>(this IIntentTemplate<T> template) where T : Intent.Modelers.Domain.Events.Api.DomainEventModel
         {
             return template.GetTypeName(DefaultDomainEventHandlerTemplate.TemplateId, template.Model);
         }
 
-        public static string GetDefaultDomainEventHandlerName(this IntentTemplateBase template, Intent.Modelers.Domain.Events.Api.DomainEventModel model)
+        public static string GetDefaultDomainEventHandlerName(this IIntentTemplate template, Intent.Modelers.Domain.Events.Api.DomainEventModel model)
         {
             return template.GetTypeName(DefaultDomainEventHandlerTemplate.TemplateId, model);
         }
 
-        public static string GetDomainEventNotificationName<T>(this IntentTemplateBase<T> template)
+        public static string GetDomainEventNotificationName(this IIntentTemplate template)
         {
             return template.GetTypeName(DomainEventNotificationTemplate.TemplateId);
         }
 
-        public static string GetDomainEventServiceName<T>(this IntentTemplateBase<T> template)
+        public static string GetDomainEventServiceName(this IIntentTemplate template)
         {
             return template.GetTypeName(DomainEventServiceTemplate.TemplateId);
         }
