3b532b577fd44a136963d2c5b71f4c94fbb3a5b7
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2023-05-11 03:11:29 +0200

    Fix missing dependency.

--- Modules/Intent.Modules.Azure.BlobStorage/NuGetPackages.cs
+++ Modules/Intent.Modules.Azure.BlobStorage/NuGetPackages.cs
@@ -1,3 +1,6 @@
+using Intent.Engine;
+using Intent.Modules.Common.CSharp.VisualStudio;
+using System;
 using Intent.Modules.Common.VisualStudio;
 
 namespace Intent.Modules.Azure.BlobStorage;
@@ -5,4 +8,19 @@ namespace Intent.Modules.Azure.BlobStorage;
 public static class NuGetPackages
 {
     public static readonly INugetPackageInfo AzureStorageBlobs = new NugetPackageInfo("Azure.Storage.Blobs", "12.13.0");
+
+    public static INugetPackageInfo MicrosoftExtensionsConfigurationBinder(IOutputTarget outputTarget) => new NugetPackageInfo("Microsoft.Extensions.Configuration.Binder", GetVersion(outputTarget.GetProject()));
+
+    private static string GetVersion(ICSharpProject project)
+    {
+        return project switch
+        {
+            _ when project.IsNetCore2App() => throw new Exception(".NET Core 2.x is no longer supported."),
+            _ when project.IsNetCore3App() => "3.0.0",
+            _ when project.IsNetApp(5) => "5.0.0",
+            _ when project.IsNetApp(6) => "6.0.0",
+            _ when project.IsNetApp(7) => "7.0.0",
+            _ => "6.0.0"
+        };
+    }
 }
