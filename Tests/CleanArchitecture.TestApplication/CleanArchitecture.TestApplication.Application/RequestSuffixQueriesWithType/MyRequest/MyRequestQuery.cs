678b0309e73300731ad22b36dae7d70fe9276a91
Joel Turner <joel.turner@intentarchitect.com>
2023-05-25 15:02:27 +0200

    Tests updated

--- Tests/CleanArchitecture.TestApplication/CleanArchitecture.TestApplication.Application/RequestSuffixQueriesWithType/MyRequest/MyRequestQuery.cs
+++ Tests/CleanArchitecture.TestApplication/CleanArchitecture.TestApplication.Application/RequestSuffixQueriesWithType/MyRequest/MyRequestQuery.cs
@@ -1,5 +1,6 @@
 using CleanArchitecture.TestApplication.Application.Common.Interfaces;
 using Intent.RoslynWeaver.Attributes;
+using MediatR;
 
 [assembly: DefaultIntentManaged(Mode.Fully)]
 [assembly: IntentTemplate("Intent.Application.MediatR.QueryModels", Version = "1.0")]
