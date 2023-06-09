c884bcb5eae5b81a29692091acdddc4815355524
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2023-06-09 17:31:50 +0200

    Halt build-all-tests.ps1 if compilation fails and provide more feedback

--- build-all-tests.ps1
+++ build-all-tests.ps1
@@ -1,3 +1,21 @@
-Get-ChildItem Tests/**/*.sln -Recurse -Depth 2 | % { dotnet build $_ }
+Write-Host "Searching for .sln files..."
 
+$slnFiles = Get-ChildItem Tests/**/*.sln -Recurse -Depth 2
+$count = 0
+
+foreach ($slnFile in $slnFiles) {
+    $count++
+    Write-Host
+    Write-Host "Building $count of $($slnFiles.Count): $slnFile..."
+    dotnet build $slnFile
+
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Exited early as build failed for $slnFile"
+        Pause
+        exit
+    }
+}
+
+Write-Host
+Write-Host "All solutions built successfully."
 Pause
