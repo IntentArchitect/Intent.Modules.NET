0fb4d42a6fe66e0075d24e587ac381dfad8d2731
dandrejvv <dandrejvv@gmail.com>
2023-06-21 14:18:00 +0200

    Added powershell script to also run unit tests like the CI/CD server would but only locally

---
+++ run-all-tests.ps1
@@ -0,0 +1,22 @@
+Write-Host "Searching for .sln files..."
+
+$slnFiles = Get-ChildItem Tests/**/*.sln -Recurse -Depth 2
+$count = 0
+
+foreach ($slnFile in $slnFiles) {
+    $count++
+    Write-Host
+    Write-Host "Preparing $count of $($slnFiles.Count): $slnFile..."
+	$Env:TF_BUILD = $true
+    dotnet test $slnFile
+
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Exited early as test run failed unexpectantly for $slnFile"
+        Pause
+        exit
+    }
+}
+
+Write-Host
+Write-Host "All solutions completed tests run."
+Pause
