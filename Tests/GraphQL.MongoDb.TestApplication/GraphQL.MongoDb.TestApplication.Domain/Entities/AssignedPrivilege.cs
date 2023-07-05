763d0d5de7be7ed10e193eba717af8c0cf87318d
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2023-07-05 08:29:46 +0200

    Update tests.

--- Tests/GraphQL.MongoDb.TestApplication/GraphQL.MongoDb.TestApplication.Domain/Entities/AssignedPrivilege.cs
+++ Tests/GraphQL.MongoDb.TestApplication/GraphQL.MongoDb.TestApplication.Domain/Entities/AssignedPrivilege.cs
@@ -11,6 +11,13 @@ namespace GraphQL.MongoDb.TestApplication.Domain.Entities
     [DefaultIntentManaged(Mode.Fully, Targets = Targets.Methods, Body = Mode.Ignore, AccessModifiers = AccessModifiers.Public)]
     public class AssignedPrivilege
     {
+        private string? _id;
+
+        public string Id
+        {
+            get => _id ??= Guid.NewGuid().ToString();
+            set => _id = value;
+        }
         public string PrivilegeId { get; set; }
     }
 }
