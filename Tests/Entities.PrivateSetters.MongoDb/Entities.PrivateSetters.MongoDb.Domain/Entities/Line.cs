860e302038fb0bf5dac5d835f8935bb607af7d41
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2023-07-05 08:23:58 +0200

    Opened and saved Domain designers for any application which was using Document DB

--- Tests/Entities.PrivateSetters.MongoDb/Entities.PrivateSetters.MongoDb.Domain/Entities/Line.cs
+++ Tests/Entities.PrivateSetters.MongoDb/Entities.PrivateSetters.MongoDb.Domain/Entities/Line.cs
@@ -11,13 +11,18 @@ namespace Entities.PrivateSetters.MongoDb.Domain.Entities
     [DefaultIntentManaged(Mode.Fully, Targets = Targets.Methods, Body = Mode.Ignore, AccessModifiers = AccessModifiers.Public)]
     public class Line
     {
+        private string? _id;
         [IntentManaged(Mode.Fully, Body = Mode.Merge)]
         public Line(string description, int quantity)
         {
             Description = description;
             Quantity = quantity;
         }
-        public string Id { get; private set; }
+        public string Id
+        {
+            get => _id ??= Guid.NewGuid().ToString();
+            private set => _id = value;
+        }
 
         public string Description { get; private set; }
 
