01a116e9c6478edb2481464e98dc33919527908d
dandrejvv <dandrejvv@gmail.com>
2023-07-12 22:27:34 +0200

    Updated release notes.
    Renamed module to "Basic Auditing" from "Basic Audit".

---
+++ Modules/Intent.Modules.EntityFrameworkCore.BasicAuditing/Api/ClassModelStereotypeExtensions.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Intent.Metadata.Models;
+using Intent.Modelers.Domain.Api;
+using Intent.Modules.Common;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.Templates.Api.ApiElementModelExtensions", Version = "1.0")]
+
+namespace Intent.EntityFrameworkCore.BasicAuditing.Api
+{
+    public static class ClassModelStereotypeExtensions
+    {
+        public static BasicAudit GetBasicAudit(this ClassModel model)
+        {
+            var stereotype = model.GetStereotype("Basic Audit");
+            return stereotype != null ? new BasicAudit(stereotype) : null;
+        }
+
+
+        public static bool HasBasicAudit(this ClassModel model)
+        {
+            return model.HasStereotype("Basic Audit");
+        }
+
+        public static bool TryGetBasicAudit(this ClassModel model, out BasicAudit stereotype)
+        {
+            if (!HasBasicAudit(model))
+            {
+                stereotype = null;
+                return false;
+            }
+
+            stereotype = new BasicAudit(model.GetStereotype("Basic Audit"));
+            return true;
+        }
+
+        public class BasicAudit
+        {
+            private IStereotype _stereotype;
+
+            public BasicAudit(IStereotype stereotype)
+            {
+                _stereotype = stereotype;
+            }
+
+            public string Name => _stereotype.Name;
+
+        }
+
+    }
+}
---
+++ Modules/Intent.Modules.EntityFrameworkCore.BasicAuditing/release-notes.md
@@ -0,0 +1,7 @@
+### Version 1.0.0
+
+- New: Adds basic auditing fields to an Entity using the `ICurrentUserService` to resolve the user that interacted with the Entity. Fields that are included are:
+  * Created By
+  * Created Date
+  * Updated By
+  * Updated Date
