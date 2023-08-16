d68b72be57fdf6f142a82497a8d89c38e89cdc7e
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2023-08-16 07:44:48 +0200

    Fix build

--- Tests/RichDomain/RichDomain.Domain/Entities/Department.cs
+++ Tests/RichDomain/RichDomain.Domain/Entities/Department.cs
@@ -10,9 +10,5 @@ namespace RichDomain.Domain.Entities
     [DefaultIntentManaged(Mode.Fully, Targets = Targets.Methods | Targets.Constructors, Body = Mode.Ignore, AccessModifiers = AccessModifiers.Public)]
     public partial class Department : IDepartment
     {
-        public Department()
-        {
-            Name = null!;
-        }
     }
 }
