a3a8d2a241b53dca59532201e416440c7422cd2b
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2023-09-04 15:23:51 +0200

    Revert this update, all other modules using the shared enums projects actually need a version bump.

--- Tests/CleanArchitecture.TestApplication/CleanArchitecture.TestApplication.Application/Validation/EnumDescriptions.cs
+++ Tests/CleanArchitecture.TestApplication/CleanArchitecture.TestApplication.Application/Validation/EnumDescriptions.cs
@@ -8,9 +8,7 @@ namespace CleanArchitecture.TestApplication.Application.Validation
 {
     public enum EnumDescriptions
     {
-        [Description("My First Option")]
         Option1,
-        [Description("My Second Option")]
         Option2 = 2
     }
 }
