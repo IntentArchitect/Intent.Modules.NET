8720626badc5dc1260fa63c6a5d92086efc49cb9
dandrejvv <dandrejvv@gmail.com>
2023-09-27 14:13:28 +0200

    Updated tests

--- Tests/CosmosDBMultiTenancy/CosmosDBMultiTenancy.Application/DependencyInjection.cs
+++ Tests/CosmosDBMultiTenancy/CosmosDBMultiTenancy.Application/DependencyInjection.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using AutoMapper;
 using CosmosDBMultiTenancy.Application.Common.Behaviours;
+using CosmosDBMultiTenancy.Application.Common.Validation;
 using FluentValidation;
 using Intent.RoslynWeaver.Attributes;
 using MediatR;
@@ -27,6 +28,7 @@ namespace CosmosDBMultiTenancy.Application
                 cfg.AddOpenBehavior(typeof(CosmosDBUnitOfWorkBehaviour<,>));
             });
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddScoped<IValidatorProvider, ValidatorProvider>();
             return services;
         }
     }
