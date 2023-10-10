36a2076d073249fa358cc7652a807126f7ddc393
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2023-10-10 14:01:02 +0200

    Deleting Intent.Modules.MongoDb.Finbuckle VS project and contents
    Was from before we "re-did" the MongoDB module and the Intent Application in the Intent.Modules.NET.isln solution was already deleted.

--- Modules/Intent.Modules.MongoDb.Finbuckle/NugetPackages.cs
+++
@@ -1,8 +0,0 @@
-﻿using Intent.Modules.Common.VisualStudio;
-
-namespace Intent.Modules.MongoDb.Finbuckle;
-
-public static class NugetPackages
-{
-    public static readonly INugetPackageInfo FinbuckleMultiTenantMongoFramework = new NugetPackageInfo("Finbuckle.MultiTenant.MongoFramework", "0.5.1");
-}
