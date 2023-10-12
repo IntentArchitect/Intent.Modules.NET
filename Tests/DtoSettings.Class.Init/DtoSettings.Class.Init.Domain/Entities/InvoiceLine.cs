02179c9e60e9ca78905c97283bcc28dccbe07942
dandrejvv <dandrejvv@gmail.com>
2023-10-12 14:05:10 +0200

    Created a test case for DTO Settings { Type: Class, Accessor: Init }
    Made the metadata shared for quicker setup.

---
+++ Tests/DtoSettings.Class.Init/DtoSettings.Class.Init.Domain/Entities/InvoiceLine.cs
@@ -0,0 +1,18 @@
+using System;
+using Intent.RoslynWeaver.Attributes;
+
+namespace DtoSettings.Class.Init.Domain.Entities
+{
+    public class InvoiceLine
+    {
+        public Guid Id { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string Currency { get; set; }
+
+        public Guid InvoiceId { get; set; }
+    }
+}
