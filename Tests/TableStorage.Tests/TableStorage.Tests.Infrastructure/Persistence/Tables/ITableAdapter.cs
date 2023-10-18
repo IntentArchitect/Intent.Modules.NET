641f58fa9b6cd2d035f73fb08d1916b513fa8df4
Joel Turner <joel.turner@intentarchitect.com>
2023-10-18 15:36:45 +0200

    build fix

--- Tests/TableStorage.Tests/TableStorage.Tests.Infrastructure/Persistence/Tables/ITableAdapter.cs
+++ Tests/TableStorage.Tests/TableStorage.Tests.Infrastructure/Persistence/Tables/ITableAdapter.cs
@@ -3,7 +3,7 @@ using Azure.Data.Tables;
 using Intent.RoslynWeaver.Attributes;
 
 [assembly: DefaultIntentManaged(Mode.Fully)]
-[assembly: IntentTemplate("Intent.Azure.TableStorage.TableStorageTableIAdapterInterface", Version = "1.0")]
+[assembly: IntentTemplate("Intent.Azure.TableStorage.TableStorageTableAdapterInterface", Version = "1.0")]
 
 namespace TableStorage.Tests.Infrastructure.Persistence.Tables
 {
