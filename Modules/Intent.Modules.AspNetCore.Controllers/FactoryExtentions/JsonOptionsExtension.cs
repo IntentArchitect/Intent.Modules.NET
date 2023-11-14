c4980bdb19e6bb3559c6909b207a828dd519230e
Shaine Gordon <sgordon@realmdigital.co.za>
2023-11-14 17:44:55 +0200

    added API settings and extension for AddJsonOptions

---
+++ Modules/Intent.Modules.AspNetCore.Controllers/FactoryExtentions/JsonOptionsExtension.cs
@@ -0,0 +1,90 @@
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Intent.Engine;
+using Intent.Modules.AspNetCore.Controllers.Settings;
+using Intent.Modules.AspNetCore.Controllers.Templates;
+using Intent.Modules.Common;
+using Intent.Modules.Common.CSharp.Builder;
+using Intent.Modules.Common.CSharp.Templates;
+using Intent.Modules.Common.Plugins;
+using Intent.Modules.Common.Templates;
+using Intent.Plugins.FactoryExtensions;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.Templates.FactoryExtension", Version = "1.0")]
+
+namespace Intent.Modules.AspNetCore.Controllers.FactoryExtentions
+{
+    [IntentManaged(Mode.Fully, Body = Mode.Merge)]
+    public class JsonOptionsExtension : FactoryExtensionBase
+    {
+        public override string Id => "Intent.AspNetCore.Controllers.JsonOptionsExtension";
+
+        [IntentManaged(Mode.Ignore)]
+        public override int Order => 0;
+
+        protected override void OnAfterTemplateRegistrations(IApplication application)
+        {
+            SetupEnumsAsStrings(application);
+        }
+
+        private static void SetupEnumsAsStrings(IApplication application)
+        {
+            var template = application.FindTemplateInstance<ICSharpFileBuilderTemplate>(TemplateDependency.OnTemplate("Intent.AspNetCore.Startup"));
+
+            if (template is null)
+            {
+                return;
+            }
+
+            var enumsAsStrings = template.ExecutionContext.Settings.GetAPISettings().EnumsAsStrings();
+            var ignoreCycles = template.ExecutionContext.Settings.GetAPISettings().IgnoreJSONReferenceCycles();
+            if (!enumsAsStrings && !ignoreCycles)
+            {
+                return;
+            }
+
+            template.CSharpFile.AfterBuild(file =>
+            {
+                var @class = file.Classes.First();
+                if (@class.FindMethod("ConfigureServices")
+                        .FindStatement(s => s.HasMetadata("configure-services-controllers-generic")) is not
+                    CSharpInvocationStatement controllersStatement)
+                {
+                    return;
+                }
+
+                controllersStatement.WithoutSemicolon();
+                controllersStatement.InsertBelow(new CSharpInvocationStatement(".AddJsonOptions"), stmt =>
+                {
+                    var invocation = (CSharpInvocationStatement)stmt;
+                    CSharpLambdaBlock lambda;
+                    if (!invocation.Statements.Any())
+                    {
+                        lambda = new CSharpLambdaBlock("options");
+                        invocation.AddArgument(lambda);
+                    }
+                    else
+                    {
+                        lambda = invocation.Statements.First() as CSharpLambdaBlock;
+                    }
+
+                    if (enumsAsStrings)
+                    {
+                        lambda.AddStatement(
+                            "options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());");
+                    }
+
+                    if (ignoreCycles)
+                    {
+                        lambda.AddStatement(
+                            "options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;");
+                    }
+                });
+
+            }, 11);
+        }
+    }
+}
