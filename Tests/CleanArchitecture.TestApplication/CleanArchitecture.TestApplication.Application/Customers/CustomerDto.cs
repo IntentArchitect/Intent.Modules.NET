79c941c54ede606c357cbb31e8b015690ad92a95
Gareth Baars <gareth.baars@intentarchitect.com>
2023-11-22 13:32:12 +0100

    Added basic test for CRUD Advanced Mapping in CleanArchitecture.TestApplication

---
+++ Tests/CleanArchitecture.TestApplication/CleanArchitecture.TestApplication.Application/Customers/CustomerDto.cs
@@ -0,0 +1,42 @@
+using System;
+using AutoMapper;
+using CleanArchitecture.TestApplication.Application.Common.Mappings;
+using CleanArchitecture.TestApplication.Domain.Entities.DDD;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.Application.Dtos.DtoModel", Version = "1.0")]
+
+namespace CleanArchitecture.TestApplication.Application.Customers
+{
+    public class CustomerDto : IMapFrom<Customer>
+    {
+        public CustomerDto()
+        {
+            Name = null!;
+            Surname = null!;
+            Email = null!;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+
+        public static CustomerDto Create(Guid id, string name, string surname, string email)
+        {
+            return new CustomerDto
+            {
+                Id = id,
+                Name = name,
+                Surname = surname,
+                Email = email
+            };
+        }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Customer, CustomerDto>();
+        }
+    }
+}
