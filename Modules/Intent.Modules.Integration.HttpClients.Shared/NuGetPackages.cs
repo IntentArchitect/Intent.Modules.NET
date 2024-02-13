d12d3a5bde831935ab24a068d713ea76eb308b6f
Stephan Johnson <stephanjohnson@users.noreply.github.com>
2024-02-13 10:47:18 +0200

    Updated module versions and fixed package references
    The commit includes updates to the version numbers of several modules. The changes are mainly in the .imodspec files where the version number has been incremented. Additionally, there's a change in NuGetPackages.cs file where the MicrosoftExtensionsHttp method now returns an instance of NugetPackageInfo instead of NugetPackageInfo directly. Also, it corrects the version number for when GetMaxNetAppVersion() returns (5, 0). Lastly, a reference to 'Microsoft.Extensions.Http' in a test project file was downgraded from version "8.0.0" to "7.0.0".

--- Modules/Intent.Modules.Integration.HttpClients.Shared/NuGetPackages.cs
+++ Modules/Intent.Modules.Integration.HttpClients.Shared/NuGetPackages.cs
@@ -6,15 +6,17 @@ namespace Intent.Modules.Integration.HttpClients.Shared;
 
 public static class NuGetPackages
 {
-    public static NugetPackageInfo MicrosoftExtensionsHttp(IOutputTarget outputTarget) => new(
+
+    public static INugetPackageInfo MicrosoftExtensionsHttp(IOutputTarget outputTarget) => new NugetPackageInfo(
         name: "Microsoft.Extensions.Http",
-        version: outputTarget.GetProject().GetMaxNetAppVersion() switch
+        version: outputTarget.GetMaxNetAppVersion() switch
         {
-            (5, 0) => "6.0.0",
+            (5, 0) => "5.0.0",
             (6, 0) => "6.0.0",
             (7, 0) => "7.0.0",
             _ => "8.0.0"
         });
+
     public static readonly INugetPackageInfo MicrosoftAspNetCoreWebUtilities = new NugetPackageInfo("Microsoft.AspNetCore.WebUtilities", "2.2.0");
     public static readonly INugetPackageInfo IdentityModelAspNetCore = new NugetPackageInfo("IdentityModel.AspNetCore", "4.3.0");
 }
