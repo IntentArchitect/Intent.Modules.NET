d491e13dabd67b1e143be93c6e099d1fdfbb59cd
Stephan Johnson <stephanjohnson@users.noreply.github.com>
2024-02-20 09:26:36 +0200

    Updated Intent.Entities module version
    The version of the Intent.Entities module has been updated across multiple test configurations. The previous version was 5.0.2 and it has been upgraded to 5.0.3-pre.0, which includes pre-release changes for testing purposes.

---
+++ Tests/EntityFrameworkCore.Repositories.TestApplication/EntityFrameworkCore.Repositories.TestApplication.Domain/Contracts/BaseContract.cs
@@ -0,0 +1,14 @@
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.Entities.DataContract", Version = "1.0")]
+
+namespace EntityFrameworkCore.Repositories.TestApplication.Domain.Contracts
+{
+    public record BaseContract
+    {
+        public BaseContract()
+        {
+        }
+    }
+}
