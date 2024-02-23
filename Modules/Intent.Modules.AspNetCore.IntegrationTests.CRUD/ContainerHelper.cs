1b87e75b284167e35386d40a066af79c5f155e09
Stephan Johnson <stephanjohnson@users.noreply.github.com>
2024-02-23 10:24:15 +0200

    Updated test filtering and trait configuration
    Modified the Azure pipeline to filter tests based on requirements rather than categories. Introduced a new ContainerHelper class for CosmosDB requirement checks. Refactored the EndpointTestImplementationFactoryExtension to add traits based on requirements instead of fixing Cosmos tests specifically. Added RequirementsExtensions for checking if an element requires CosmosDB. Updated module version and release notes accordingly.
    Refactored test filtering and trait configuration
    
    Updated the Azure pipeline to filter tests based on requirements rather than categories. Introduced a new ContainerHelper class for CosmosDB requirement checks. Refactored the EndpointTestImplementationFactoryExtension to add traits based on requirements instead of fixing Cosmos tests specifically. Added RequirementsExtensions for checking if an element requires CosmosDB. Updated module version and release notes accordingly.

---
+++ Modules/Intent.Modules.AspNetCore.IntegrationTests.CRUD/ContainerHelper.cs
@@ -0,0 +1,28 @@
+﻿using Intent.Modules.AspNetCore.IntegrationTesting.Templates;
+using Intent.Modules.Common.Templates;
+using Intent.Modules.Metadata.RDBMS.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Intent.Modules.AspNetCore.IntegrationTests.CRUD
+{
+    internal class ContainerHelper
+    {
+        private static HashSet<DatabaseSettingsExtensions.DatabaseProviderOptionsEnum> _supportedDBs = new() { DatabaseSettingsExtensions.DatabaseProviderOptionsEnum.SqlServer, DatabaseSettingsExtensions.DatabaseProviderOptionsEnum.Postgresql };
+        public static bool RequireCosmosContainer(IntentTemplateBase template)
+        {
+            return template.ExecutionContext.InstalledModules.Any(p => p.ModuleId == "Intent.CosmosDB") ||
+                template.ExecutionContext.InstalledModules.Any(p => p.ModuleId == "Intent.EntityFrameworkCore") && template.ExecutionContext.Settings.GetDatabaseSettings().DatabaseProvider().AsEnum() == DatabaseSettingsExtensions.DatabaseProviderOptionsEnum.Cosmos;
+        }
+
+        public static bool RequireRdbmsEFContainer(IntentTemplateBase template)
+        {
+            return template.ExecutionContext.InstalledModules.Any(p => p.ModuleId == "Intent.EntityFrameworkCore") &&
+                _supportedDBs.Contains(template.ExecutionContext.Settings.GetDatabaseSettings().DatabaseProvider().AsEnum());
+        }
+    }
+}
--- Modules/Intent.Modules.AspNetCore.IntegrationTests.CRUD/release-notes.md
+++ Modules/Intent.Modules.AspNetCore.IntegrationTests.CRUD/release-notes.md
@@ -1,3 +1,7 @@
-﻿### Version 1.0.0
+﻿### Version 1.0.1
+
+- Improvement: Updated trait configuration and setup to allow for multiple types of trait configurations.
+
+### Version 1.0.0
 
 - New Feature: Asp.Net Core Integration Testing CRUD module.
