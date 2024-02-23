f41aba7acda0ff0be966d4653a978aec27b46f1c
Stephan Johnson <stephanjohnson@users.noreply.github.com>
2024-02-23 12:00:49 +0200

    Added new integration tests for concurrency and customer search
    Two new integration tests have been added. The first test checks the behavior of the system when an entity's Etag is changed by a previous operation, expecting a conflict to occur. The second test verifies that customers can be correctly retrieved by their name from the database. Both tests include setup instructions for excluding them in CI/CD environments where Cosmos DB Linux Emulator Docker image does not run.

--- Tests/AdvancedMappingCrud.Cosmos.Tests/AdvancedMappingCrud.Cosmos.Tests.IntegrationTests/Tests/Concurrency/UpdateEntityAfterEtagWasChangedByPreviousOperationTestTests.cs
+++ Tests/AdvancedMappingCrud.Cosmos.Tests/AdvancedMappingCrud.Cosmos.Tests.IntegrationTests/Tests/Concurrency/UpdateEntityAfterEtagWasChangedByPreviousOperationTestTests.cs
@@ -12,5 +12,23 @@ namespace AdvancedMappingCrud.Cosmos.Tests.IntegrationTests.Tests
         public UpdateEntityAfterEtagWasChangedByPreviousOperationTestTests(IntegrationTestWebAppFactory factory) : base(factory)
         {
         }
+
+        /// <summary>
+        /// The Cosmos DB Linux Emulator Docker image does not run on Microsoft's CI environment (GitHub, Azure DevOps).")] // https://github.com/Azure/azure-cosmos-db-emulator-docker/issues/45.
+        /// Filter this test out of your CI/CD if appropriate e.g. dotnet test --filter Category!=ExcludeOnCI
+        /// </summary>
+        [Fact]
+        [Trait("Requirement", "CosmosDB")]
+        public async Task UpdateEntityAfterEtagWasChangedByPreviousOperationTest_ShouldFailWithConflict()
+        {
+            //Arrange
+            var client = new ConcurrencyHttpClient(CreateClient());
+
+            //Act
+
+            //Assert
+            var exception = await Assert.ThrowsAsync<HttpClientRequestException>(async () => await client.UpdateEntityAfterEtagWasChangedByPreviousOperationTestAsync());
+            Assert.Equal(HttpStatusCode.InternalServerError, exception.StatusCode);
+        }
     }
 }
--- Tests/AdvancedMappingCrud.Cosmos.Tests/AdvancedMappingCrud.Cosmos.Tests.IntegrationTests/Tests/Customers/FindCustomerByNameTests.cs
+++ Tests/AdvancedMappingCrud.Cosmos.Tests/AdvancedMappingCrud.Cosmos.Tests.IntegrationTests/Tests/Customers/FindCustomerByNameTests.cs
@@ -11,6 +11,30 @@ namespace AdvancedMappingCrud.Cosmos.Tests.IntegrationTests.Tests
     {
         public FindCustomerByNameTests(IntegrationTestWebAppFactory factory) : base(factory)
         {
+
+        }
+
+        /// <summary>
+        /// The Cosmos DB Linux Emulator Docker image does not run on Microsoft's CI environment (GitHub, Azure DevOps).")] // https://github.com/Azure/azure-cosmos-db-emulator-docker/issues/45.
+        /// Filter this test out of your CI/CD if appropriate e.g. dotnet test --filter Category!=ExcludeOnCI
+        /// </summary>
+        [Fact]
+        [Trait("Requirement", "CosmosDB")]
+        public async Task GFindCustomerByName_ShouldGetCustomerByNamed()
+        {
+            //Arrange
+            var client = new CustomersHttpClient(CreateClient());
+
+            var dataFactory = new TestDataFactory(WebAppFactory);
+            var customerId = await dataFactory.CreateCustomer();
+            var customer = await client.GetCustomerByIdAsync(customerId);
+
+            //Act
+            var foundCustomer = await client.FindCustomerByNameAsync(customer.Name);
+
+            //Assert
+            Assert.NotNull(foundCustomer);
+            Assert.Equal(customer.Name, foundCustomer.Name);
         }
     }
 }
