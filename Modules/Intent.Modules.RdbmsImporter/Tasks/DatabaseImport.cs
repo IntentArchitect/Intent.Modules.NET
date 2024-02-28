c645bb46d39831c10893abdd1d1a933357847beb
Joel Turner <joel.turner@intentarchitect.com>
2024-02-28 10:13:04 +0200

    module added

---
+++ Modules/Intent.Modules.RdbmsImporter/Tasks/DatabaseImport.cs
@@ -0,0 +1,23 @@
+﻿using Intent.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intent.Modules.RdbmsImporter.Tasks
+{
+	public class DatabaseImport : IModuleTask
+	{
+		public string TaskTypeId => "Intent.Modules.RdbmsImporter.Tasks.DatabaseImport";
+
+		public string TaskTypeName => "Rdbms Database Import";
+
+		public int Order => 0;
+
+		public string Execute(params string[] args)
+		{
+			return "hello";
+		}
+	}
+}
