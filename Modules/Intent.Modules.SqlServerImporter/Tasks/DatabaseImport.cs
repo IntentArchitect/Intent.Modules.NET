eff5afa1f7dd5c471bd62cb6de053a7da953e183
Joel Turner <joel.turner@intentarchitect.com>
2024-02-29 13:34:17 +0200

    wip

--- Modules/Intent.Modules.SqlServerImporter/Tasks/DatabaseImport.cs
+++ Modules/Intent.Modules.SqlServerImporter/Tasks/DatabaseImport.cs
@@ -5,11 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Intent.Modules.RdbmsImporter.Tasks
+namespace Intent.Modules.SqlServerImporter.Tasks
 {
 	public class DatabaseImport : IModuleTask
 	{
-		public string TaskTypeId => "Intent.Modules.RdbmsImporter.Tasks.DatabaseImport";
+		public string TaskTypeId => "Intent.Modules.SqlServerImporter.Tasks.DatabaseImport";
 
 		public string TaskTypeName => "Rdbms Database Import";
 
