892ba6564c8a27932a28ab07a13a4f3aff13af70
Stephan Johnson <stephanjohnson@users.noreply.github.com>
2024-03-22 07:49:59 +0200

    Enhanced HttpClient configuration in Dapr module
    The HttpClient configuration in the Dapr AspNetCore ServiceInvocation module has been improved. New code was added to handle distinct service models and publish AppSettingRegistrationRequest events before template execution. The base address for http is now set using a new GetConfigKey method, which provides more flexibility. A ServiceModelComparer class was also introduced to help with handling distinct service models.

--- Modules/Intent.Modules.Dapr.AspNetCore.ServiceInvocation/Templates/HttpClientConfiguration/HttpClientConfigurationTemplatePartial.cs
+++ Modules/Intent.Modules.Dapr.AspNetCore.ServiceInvocation/Templates/HttpClientConfiguration/HttpClientConfigurationTemplatePartial.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Intent.Engine;
 using Intent.Modelers.Types.ServiceProxies.Api;
 using Intent.Modules.Application.Contracts.Clients.Templates.ServiceContract;
 using Intent.Modules.Common;
 using Intent.Modules.Common.CSharp.Builder;
+using Intent.Modules.Common.CSharp.Configuration;
 using Intent.Modules.Common.CSharp.Templates;
 using Intent.Modules.Common.Templates;
 using Intent.Modules.Dapr.AspNetCore.ServiceInvocation.Templates.HttpClient;
+using Intent.Modules.Integration.HttpClients.Shared.Templates;
 using Intent.Modules.Integration.HttpClients.Shared.Templates.HttpClientConfiguration;
 using Intent.Modules.Integration.HttpClients.Shared.Templates.HttpClientHeaderDelegatingHandler;
 using Intent.RoslynWeaver.Attributes;
@@ -25,6 +28,7 @@ namespace Intent.Modules.Dapr.AspNetCore.ServiceInvocation.Templates.HttpClientC
     public partial class HttpClientConfigurationTemplate : HttpClientConfigurationBase
     {
         public const string TemplateId = "Intent.Dapr.AspNetCore.ServiceInvocation.HttpClientConfigurationTemplate";
+        private readonly IList<ServiceProxyModel> _typedModels;
 
         [IntentManaged(Mode.Fully, Body = Mode.Ignore)]
         public HttpClientConfigurationTemplate(IOutputTarget outputTarget, IList<ServiceProxyModel> model)
@@ -35,10 +39,48 @@ namespace Intent.Modules.Dapr.AspNetCore.ServiceInvocation.Templates.HttpClientC
                   HttpClientTemplate.TemplateId,
                   (options, proxy, template) =>
                   {
-                      options.AddStatement($"http.BaseAddress = new Uri($\"http://{template.ExecutionContext.GetDaprApplicationName(model.First().InternalElement.MappedElement.ApplicationId)}\");");
+                      options.AddStatement($"http.BaseAddress = new Uri($\"http://{GetConfigKey(proxy, "Uri")}\");");
                   }
                   )
-        { 
+        {
+            _typedModels = model;
+        }
+
+        public override void BeforeTemplateExecution()
+        {
+            base.BeforeTemplateExecution();
+            foreach (var proxy in _typedModels.Distinct(new ServiceModelComparer()))
+            {
+                ExecutionContext.EventDispatcher.Publish(new AppSettingRegistrationRequest(GetConfigKey(proxy, "Uri"), $"http://{ExecutionContext.GetDaprApplicationName(proxy.InternalElement.MappedElement.ApplicationId)}/"));
+            }
+        }
+
+        private static string GetConfigKey(IServiceProxyModel proxy, string key)
+        {
+            return $"HttpClients:{proxy.Name.ToPascalCase()}{(string.IsNullOrEmpty(key) ? string.Empty : ":")}{key?.ToPascalCase()}";
+        }
+
+        private static string GetConfigKey(ServiceProxyModel proxy, string key)
+        {
+            return $"HttpClients:{proxy.Name.ToPascalCase()}{(string.IsNullOrEmpty(key) ? string.Empty : ":")}{key?.ToPascalCase()}";
+        }
+
+        class ServiceModelComparer : IEqualityComparer<ServiceProxyModel>
+        {
+            public bool Equals(ServiceProxyModel x, ServiceProxyModel y)
+            {
+                if (x == null || y == null)
+                {
+                    return false;
+                }
+
+                return Equals(x.Mapping.ElementId, y.Mapping.ElementId);
+            }
+
+            public int GetHashCode(ServiceProxyModel obj)
+            {
+                return obj.Mapping.ElementId.GetHashCode();
+            }
         }
     }
 }
