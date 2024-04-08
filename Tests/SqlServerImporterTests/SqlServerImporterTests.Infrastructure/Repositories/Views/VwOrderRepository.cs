6efe6ba3945c0b4eb515802c587d6b2d4e46fc60
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2024-04-08 12:22:41 +0200

    Update tests.

--- Tests/SqlServerImporterTests/SqlServerImporterTests.Infrastructure/Repositories/Views/VwOrderRepository.cs
+++ Tests/SqlServerImporterTests/SqlServerImporterTests.Infrastructure/Repositories/Views/VwOrderRepository.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using Intent.RoslynWeaver.Attributes;
+using Microsoft.EntityFrameworkCore;
 using SqlServerImporterTests.Domain.Entities.Views;
 using SqlServerImporterTests.Domain.Repositories;
 using SqlServerImporterTests.Domain.Repositories.Views;
@@ -18,8 +19,15 @@ namespace SqlServerImporterTests.Infrastructure.Repositories.Views
     [IntentManaged(Mode.Merge, Signature = Mode.Fully)]
     public class VwOrderRepository : RepositoryBase<VwOrder, VwOrder, ApplicationDbContext>, IVwOrderRepository
     {
+        private readonly ApplicationDbContext _dbContext;
         public VwOrderRepository(ApplicationDbContext dbContext, IMapper mapper) : base(dbContext, mapper)
         {
+            _dbContext = dbContext;
+        }
+
+        public void Add(VwOrder entity)
+        {
+            _dbContext.Database.ExecuteSqlInterpolated($"INSERT INTO VwOrders (Id, CustomerId, OrderDate, RefNo) VALUES({entity.Id}, {entity.CustomerId}, {entity.OrderDate}, {entity.RefNo})");
         }
     }
 }
