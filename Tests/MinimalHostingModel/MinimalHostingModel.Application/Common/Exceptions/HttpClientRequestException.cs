433e55fb138c0989e4cf97ad76394585f8685467
Stephan Johnson <stephanjohnson@users.noreply.github.com>
2024-04-12 07:57:37 +0200

    Updated Intent tests

--- Tests/MinimalHostingModel/MinimalHostingModel.Application/Common/Exceptions/HttpClientRequestException.cs
+++ Tests/MinimalHostingModel/MinimalHostingModel.Application/Common/Exceptions/HttpClientRequestException.cs
@@ -25,7 +25,15 @@ namespace MinimalHostingModel.Application.Common.Exceptions
             ResponseHeaders = responseHeaders;
             ReasonPhrase = reasonPhrase;
             ResponseContent = responseContent;
+            if (responseContent is not null &&
+            responseHeaders.TryGetValue("Content-Type", out var contentTypeValues) &&
+            contentTypeValues.FirstOrDefault() == "application/problem+json; charset=utf-8")
+            {
+                ProblemDetails = System.Text.Json.JsonSerializer.Deserialize<ProblemDetailsWithErrors>(responseContent);
+            }
         }
+
+        public ProblemDetailsWithErrors? ProblemDetails { get; private set; }
 
         public Uri RequestUri { get; private set; }
         public HttpStatusCode StatusCode { get; private set; }
@@ -41,8 +49,15 @@ namespace MinimalHostingModel.Application.Common.Exceptions
         {
             var fullRequestUri = new Uri(baseAddress, request.RequestUri!);
             var content = await response.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
+
             var headers = response.Headers.ToDictionary(k => k.Key, v => v.Value);
-            return new HttpClientRequestException(fullRequestUri, response.StatusCode, headers, response.ReasonPhrase, content);
+            var contentHeaders = response.Content.Headers.ToDictionary(k => k.Key, v => v.Value);
+            var allHeaders = headers
+            .Concat(contentHeaders)
+            .GroupBy(kvp => kvp.Key)
+            .ToDictionary(group => group.Key, group => group.Last().Value);
+
+            return new HttpClientRequestException(fullRequestUri, response.StatusCode, allHeaders, response.ReasonPhrase, content);
         }
 
         private static string GetMessage(
