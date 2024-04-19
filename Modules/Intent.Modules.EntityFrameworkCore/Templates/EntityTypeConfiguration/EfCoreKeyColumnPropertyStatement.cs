57a5af4f0d624bfecdc93cc5efd21d5e916d533f
Stephan Johnson <stephanjohnson@users.noreply.github.com>
2024-04-19 17:44:42 +0200

    Refactor EF Core key column property statement
    The code has been refactored to improve readability and maintainability. The ternary operators have been replaced with if statements for better clarity. A method for checking whether a configuration is required has been added, which checks if the attribute has a column or if it requires non-conventional ordinal primary key configuration. Redundant methods and comments have also been removed from the EntityTypeConfigurationTemplatePartial class.

--- Modules/Intent.Modules.EntityFrameworkCore/Templates/EntityTypeConfiguration/EfCoreKeyColumnPropertyStatement.cs
+++ Modules/Intent.Modules.EntityFrameworkCore/Templates/EntityTypeConfiguration/EfCoreKeyColumnPropertyStatement.cs
@@ -17,17 +17,21 @@ public class EfCoreKeyColumnPropertyStatement : CSharpStatement
 
     public override string GetText(string indentation)
     {
-        var name = !string.IsNullOrWhiteSpace(_model.GetColumn()?.Name())
-            ? $@"
-{indentation}    .HasColumnName(""{EscapeHelper.EscapeName(_model.GetColumn()?.Name())}"")"
-            : string.Empty;
+        var name = string.Empty;
+        if (!string.IsNullOrWhiteSpace(_model.GetColumn()?.Name()))
+        {
+            name = $@"
+{indentation}    .HasColumnName(""{EscapeHelper.EscapeName(_model.GetColumn().Name())}"")";
+        }
 
-        var type = !string.IsNullOrWhiteSpace(_model.GetColumn()?.Type())
-            ? $@"
-{indentation}    .HasColumnType(""{_model.GetColumn()?.Type()}"")"
-            : string.Empty;
+        var type = string.Empty;
+        if (!string.IsNullOrWhiteSpace(_model.GetColumn()?.Type()))
+        {
+            type = $@"
+{indentation}    .HasColumnType(""{_model.GetColumn().Type()}"")";
+        }
 
-        var valueGeneratedOnAdd = IsNonConventionalOrdinalKey()
+        var valueGeneratedOnAdd = NonConventionalOrdinalPrimaryKeyRequiresConfiguration(_model)
             ? $@"
 {indentation}    .ValueGeneratedOnAdd()"
             : string.Empty;
@@ -35,14 +39,23 @@ public class EfCoreKeyColumnPropertyStatement : CSharpStatement
         return @$"{indentation}builder.Property(x => x.{_model.Name.ToPascalCase()}){name}{type}{valueGeneratedOnAdd};";
     }
 
-    private bool IsNonConventionalOrdinalKey()
+    public static bool RequiresConfiguration(AttributeModel attribute)
     {
+        return attribute.HasColumn() || NonConventionalOrdinalPrimaryKeyRequiresConfiguration(attribute);
+    }
+
+    private static bool NonConventionalOrdinalPrimaryKeyRequiresConfiguration(AttributeModel attribute)
+    {
+        // Conventional Ordinal Primary Key Types: short, int, long, Guid
+        // Non-Conventional Ordinal Primary Key Types: byte, decimal, double, float
         var byteId = "A4E9102F-C1C8-4902-A417-CA418E1874D2";
-        var decimalId = "675C7B84-997A-44E0-82B9-CD724C07C9E6";
+        var decimalId = "675c7b84-997a-44e0-82b9-cd724c07c9e6";
         var doubleId = "24A77F70-5B97-40DD-8F9A-4208AD5F9219";
         var floatId = "341929E9-E3E7-46AA-ACB3-B0438421F4C4";
         var nonConventionalOrdinalTypeWhitelist = new[] { byteId, decimalId, doubleId, floatId };
 
-        return nonConventionalOrdinalTypeWhitelist.Contains(_model.InternalElement.TypeReference.Element.Id.ToUpper());
+        return (!attribute.InternalElement.ParentElement.IsClassModel() ||
+               attribute.Class.GetExplicitPrimaryKey().All(keyAttribute => keyAttribute.Equals(attribute))) &&
+               nonConventionalOrdinalTypeWhitelist.Contains(attribute.InternalElement.TypeReference.Element.Id);
     }
 }
