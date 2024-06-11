899bc7694244f62d859af92c33b693b2d6ec75a0
Joel Turner <joel.turner@intentarchitect.com>
2024-06-11 17:34:37 +0200

    Last touches

--- Tests/Dapper.Tests/Dapper.Tests.Infrastructure/Repositories/CustomerRepository.cs
+++ Tests/Dapper.Tests/Dapper.Tests.Infrastructure/Repositories/CustomerRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Dapper;
 using Dapper.Tests.Domain.Entities;
 using Dapper.Tests.Domain.Repositories;
 using Intent.RoslynWeaver.Attributes;
