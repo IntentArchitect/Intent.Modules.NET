f8fe91ce91d01b75c8b65832ecc2e4a9efd59867
Joel Turner <joel.turner@intentarchitect.com>
2024-06-24 17:19:34 +0200

    Modelling relationships as IsCollection and IsNullable causing EF mapping problems

--- Modules/Intent.Modules.EntityFrameworkCore/Templates/EntityTypeConfiguration/EntityTypeConfigurationTemplateRegistration.cs
+++ Modules/Intent.Modules.EntityFrameworkCore/Templates/EntityTypeConfiguration/EntityTypeConfigurationTemplateRegistration.cs
@@ -66,7 +66,7 @@ namespace Intent.Modules.EntityFrameworkCore.Templates.EntityTypeConfiguration
                        !type.AsClassModel().HasTable() &&
                        !type.AsClassModel().AssociatedFromClasses().Any(x => x.IsNullable || x.IsCollection) && // cannot have aggregations pointing to it
                        !type.AsClassModel().AssociatedClasses.Any(x => x.IsCollection && x.OtherEnd().IsCollection) && // cannot have any many to many associated with it
-                       (executionContext.Settings.GetDatabaseSettings().DatabaseProvider().IsCosmos() || !type.AsClassModel().AssociationEnds().Any(x => x.IsTargetEnd() && x.OtherEnd().IsNullable)); // cannot have an aggregate relationship outward
+                       (executionContext.Settings.GetDatabaseSettings().DatabaseProvider().IsCosmos() || !type.AsClassModel().AssociationEnds().Any(x => x.IsTargetEnd() && x.OtherEnd().IsNullable && !x.OtherEnd().IsCollection)); // cannot have an aggregate relationship outward
             }
 
             return type.IsValueObject(executionContext);
