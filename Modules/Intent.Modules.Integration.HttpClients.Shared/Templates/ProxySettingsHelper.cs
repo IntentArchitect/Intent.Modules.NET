a5089f151a2f1d2ef919e620b66884e0e3d8f10b
Joel Turner <joel.turner@intentarchitect.com>
2024-07-16 12:07:14 +0200

    Small Fix, tests bump

--- Modules/Intent.Modules.Integration.HttpClients.Shared/Templates/ProxySettingsHelper.cs
+++ Modules/Intent.Modules.Integration.HttpClients.Shared/Templates/ProxySettingsHelper.cs
@@ -15,13 +15,20 @@ namespace Intent.Modules.Integration.HttpClients.Shared.Templates
         {
             if (!string.IsNullOrEmpty(proxy.InternalElement.MappedElement?.ApplicationId))
             {
-                var settings = application.GetApplicationConfig(proxy.InternalElement.MappedElement.ApplicationId);
-                //API Settings
-                var apiSettings = settings.ModuleSetting.FirstOrDefault(s => s.Id == "4bd0b4e9-7b53-42a9-bb4a-277abb92a0eb");
-                //SerializeEnumsAsStrings
-                if (bool.TryParse(apiSettings?.GetSetting("97f3a1e3-2455-41e8-b28e-709f2db04230")?.Value?.ToPascalCase(), out var result))
+                try
                 {
-                    return result;
+                    var settings = application.GetApplicationConfig(proxy.InternalElement.MappedElement.ApplicationId);
+                    //API Settings
+                    var apiSettings = settings.ModuleSetting.FirstOrDefault(s => s.Id == "4bd0b4e9-7b53-42a9-bb4a-277abb92a0eb");
+                    //SerializeEnumsAsStrings
+                    if (bool.TryParse(apiSettings?.GetSetting("97f3a1e3-2455-41e8-b28e-709f2db04230")?.Value?.ToPascalCase(), out var result))
+                    {
+                        return result;
+                    }
+                }
+                catch // This is if the applicationId can't be found need a TryGetApplicationConfig, will be added 4.3
+                {
+                    return false;
                 }
             }
             return null;
