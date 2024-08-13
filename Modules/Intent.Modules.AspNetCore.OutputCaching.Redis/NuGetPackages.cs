2bdd6abb660f5fb8dfadd1b896ea22d38d8b6f3e
Joel Turner <joel.turner@intentarchitect.com>
2024-08-13 12:52:54 +0200

    Nuget Upgrade Test

--- Modules/Intent.Modules.AspNetCore.OutputCaching.Redis/NuGetPackages.cs
+++ Modules/Intent.Modules.AspNetCore.OutputCaching.Redis/NuGetPackages.cs
@@ -1,18 +1,34 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Intent.Engine;
+using Intent.Modules.Common.CSharp.Nuget;
+using Intent.Modules.Common.CSharp.VisualStudio;
 using Intent.Modules.Common.VisualStudio;
+using Intent.RoslynWeaver.Attributes;
+using NuGet.Versioning;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.CSharp.Templates.NugetPackages", Version = "1.0")]
 
 namespace Intent.Modules.AspNetCore.OutputCaching.Redis
 {
-    public static class NugetPackages
+    public class NugetPackages
     {
+        public const string StackExchangeRedisPackageName = "Microsoft.AspNetCore.OutputCaching.StackExchangeRedis";
 
-        public static NugetPackageInfo MicrosoftAspNetCoreOutputCachingStackExchangeRedis(IOutputTarget outputTarget) => new NugetPackageInfo(
-            name: "Microsoft.AspNetCore.OutputCaching.StackExchangeRedis",
-            version: outputTarget.GetMaxNetAppVersion() switch
-            {
-                (>= 8, 0) => "8.0.7",
-                _ => throw new Exception($"Unsupported Framework `{outputTarget.GetMaxNetAppVersion().Major}` for NuGet package 'Microsoft.AspNetCore.OutputCaching.StackExchangeRedis'")
-            });
+        static NugetPackages()
+        {
+            NugetRegistry.Register(StackExchangeRedisPackageName,
+                (framework) => framework switch
+                    {
+                        ( >= 8, 0) => new PackageVersion("8.0.7"),
+                        _ => throw new Exception($"Unsupported Framework `{framework.Major}` for NuGet package '{StackExchangeRedisPackageName}'"),
+                    }
+                );
+        }
+
+        public static NugetPackageInfo StackExchangeRedis(IOutputTarget outputTarget) => NugetRegistry.GetVersion(StackExchangeRedisPackageName, outputTarget.GetMaxNetAppVersion());
     }
+
 }
