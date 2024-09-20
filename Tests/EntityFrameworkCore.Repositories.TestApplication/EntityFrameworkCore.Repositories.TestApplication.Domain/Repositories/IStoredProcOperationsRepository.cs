abd51e6fb99a668e0a697ed5c957f85db8208f1a
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2024-09-20 10:20:23 +0200

    Improvement: Updated to respect new Stored Procedure stereotypes introduced in version `1.1.1` of the `Intent.Modelers.Domain.StoredProcedures` module.
    (Tests)

---
+++ Tests/EntityFrameworkCore.Repositories.TestApplication/EntityFrameworkCore.Repositories.TestApplication.Domain/Repositories/IStoredProcOperationsRepository.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using EntityFrameworkCore.Repositories.TestApplication.Domain.Contracts;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.EntityFrameworkCore.Repositories.CustomRepositoryInterface", Version = "1.0")]
+
+namespace EntityFrameworkCore.Repositories.TestApplication.Domain.Repositories
+{
+    public interface IStoredProcOperationsRepository
+    {
+        Task<SpResult> MyProc(IEnumerable<SpParameter> param, CancellationToken cancellationToken = default);
+    }
+}
