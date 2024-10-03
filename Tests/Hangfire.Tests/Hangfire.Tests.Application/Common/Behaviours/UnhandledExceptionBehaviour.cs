461a1cc238f8aab61c634527f4ced7da77802ece
JP Seini <jp.seini@intentarchitect.com>
2024-10-03 13:58:17 +0200

    Integration tests for hangfire

---
+++ Tests/Hangfire.Tests/Hangfire.Tests.Application/Common/Behaviours/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using Intent.RoslynWeaver.Attributes;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.Application.MediatR.Behaviours.UnhandledExceptionBehaviour", Version = "1.0")]
+
+namespace Hangfire.Tests.Application.Common.Behaviours
+{
+    public class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : notnull
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public UnhandledExceptionBehaviour(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(
+            TRequest request,
+            RequestHandlerDelegate<TResponse> next,
+            CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (ValidationException ex)
+            {
+                // Do not log Fluent Validation Exceptions
+                throw;
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, "CleanArchitecture Request: Unhandled Exception for Request {Name} {@Request}", requestName, request);
+                throw;
+            }
+        }
+    }
+}
---
+++ Tests/Hangfire.Tests/Hangfire.Tests.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+using Hangfire.Tests.Application.Common.Interfaces;
+using Hangfire.Tests.Domain.Common.Interfaces;
+using Hangfire.Tests.Infrastructure.Persistence;
+using Hangfire.Tests.Infrastructure.Services;
+using Intent.RoslynWeaver.Attributes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.Infrastructure.DependencyInjection.DependencyInjection", Version = "1.0")]
+
+namespace Hangfire.Tests.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>((sp, options) =>
+            {
+                options.UseInMemoryDatabase("DefaultConnection");
+                options.UseLazyLoadingProxies();
+            });
+            services.AddScoped<IUnitOfWork>(provider => provider.GetRequiredService<ApplicationDbContext>());
+            services.AddScoped<IDomainEventService, DomainEventService>();
+            return services;
+        }
+    }
+}
