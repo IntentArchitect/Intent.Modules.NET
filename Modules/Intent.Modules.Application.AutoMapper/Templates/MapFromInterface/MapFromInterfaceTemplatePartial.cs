749b1a6a0377c773e3b0bf37a4ba4dbf5b8ecfcc
JP Seini <jp.seini@intentarchitect.com>
2024-11-08 17:53:47 +0200

    `MappingProfile` and `IMapFrom` files converted to use Builder pattern, while also improving the qualify of generated code.

--- Modules/Intent.Modules.Application.AutoMapper/Templates/MapFromInterface/MapFromInterfaceTemplatePartial.cs
+++ Modules/Intent.Modules.Application.AutoMapper/Templates/MapFromInterface/MapFromInterfaceTemplatePartial.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Intent.Engine;
 using Intent.Modules.Common;
+using Intent.Modules.Common.CSharp.Builder;
 using Intent.Modules.Common.CSharp.Templates;
 using Intent.Modules.Common.Templates;
 using Intent.RoslynWeaver.Attributes;
@@ -13,7 +14,7 @@ using Intent.Templates;
 namespace Intent.Modules.Application.AutoMapper.Templates.MapFromInterface
 {
     [IntentManaged(Mode.Merge, Signature = Mode.Fully)]
-    partial class MapFromInterfaceTemplate : CSharpTemplateBase<object>
+    public partial class MapFromInterfaceTemplate : CSharpTemplateBase<object>, ICSharpFileBuilderTemplate
     {
         [IntentManaged(Mode.Fully)]
         public const string TemplateId = "Intent.Application.AutoMapper.MapFromInterface";
@@ -21,8 +22,22 @@ namespace Intent.Modules.Application.AutoMapper.Templates.MapFromInterface
         [IntentManaged(Mode.Merge, Signature = Mode.Fully)]
         public MapFromInterfaceTemplate(IOutputTarget outputTarget, object model = null) : base(TemplateId, outputTarget, model)
         {
+            CSharpFile = new CSharpFile(this.GetNamespace(), this.GetFolderPath())
+                .AddInterface($"IMapFrom", @interface =>
+                {
+                    @interface.Internal();
+                    @interface.AddGenericParameter("T");
+
+                    @interface.AddMethod(CSharpTypeVoid.DefaultInstance, "Mapping", method =>
+                    {
+                        method.AddParameter(UseType("AutoMapper.Profile"), "profile");
+                    });
+                });
             AddNugetDependency(NugetPackages.AutoMapper(outputTarget));
         }
+
+        [IntentManaged(Mode.Fully)]
+        public CSharpFile CSharpFile { get; }
 
         [IntentManaged(Mode.Fully, Body = Mode.Ignore)]
         protected override CSharpFileConfig DefineFileConfig()
@@ -30,6 +45,12 @@ namespace Intent.Modules.Application.AutoMapper.Templates.MapFromInterface
             return new CSharpFileConfig(
                 className: $"IMapFrom",
                 @namespace: $"{OutputTarget.GetNamespace()}");
+        }
+
+        [IntentManaged(Mode.Fully)]
+        public override string TransformText()
+        {
+            return CSharpFile.ToString();
         }
 
     }
--- Modules/Intent.Modules.Application.AutoMapper/Templates/MappingProfile/MappingProfileTemplatePartial.cs
+++ Modules/Intent.Modules.Application.AutoMapper/Templates/MappingProfile/MappingProfileTemplatePartial.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using Intent.Engine;
 using Intent.Modules.Common;
+using Intent.Modules.Common.CSharp.Builder;
 using Intent.Modules.Common.CSharp.Templates;
+using Intent.Modules.Common.CSharp.VisualStudio;
 using Intent.Modules.Common.Templates;
 using Intent.RoslynWeaver.Attributes;
 using Intent.Templates;
@@ -13,7 +15,7 @@ using Intent.Templates;
 namespace Intent.Modules.Application.AutoMapper.Templates.MappingProfile
 {
     [IntentManaged(Mode.Merge, Signature = Mode.Fully)]
-    partial class MappingProfileTemplate : CSharpTemplateBase<object>
+    public partial class MappingProfileTemplate : CSharpTemplateBase<object>, ICSharpFileBuilderTemplate
     {
         [IntentManaged(Mode.Fully)]
         public const string TemplateId = "Intent.Application.AutoMapper.MappingProfile";
@@ -21,8 +23,60 @@ namespace Intent.Modules.Application.AutoMapper.Templates.MappingProfile
         [IntentManaged(Mode.Merge, Signature = Mode.Fully)]
         public MappingProfileTemplate(IOutputTarget outputTarget, object model = null) : base(TemplateId, outputTarget, model)
         {
+            AddUsing("System");
+            AddUsing("System.Linq");
+            AddUsing("System.Reflection");
+
+            CSharpFile = new CSharpFile(this.GetNamespace(), this.GetFolderPath())
+                .AddClass($"MappingProfile", @class =>
+                {
+                    @class.WithBaseType(UseType("AutoMapper.Profile"));
+
+                    @class.AddConstructor(ctor =>
+                    {
+                        ctor.AddParameter("string", "exampleParam", param =>
+                        {
+                            param.IntroduceReadonlyField();
+                        });
+                        ctor.AddInvocationStatement("ApplyMappingsFromAssembly", cfg =>
+                        {
+                            cfg.AddArgument(new CSharpInvocationStatement("Assembly.GetExecutingAssembly").WithoutSemicolon());
+                        });
+                    });
+
+                    @class.AddMethod("void", "ApplyMappingsFromAssembly", method =>
+                    {
+                        method.Private();
+
+                        method.AddParameter("Assembly", "assembly");
+
+                        method.AddObjectInitStatement("var types", @"assembly.GetExportedTypes()
+                .Where(t => Array.Exists(t.GetInterfaces(), i =>
+                    i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapFrom<>)))
+                .ToList();");
+
+                        method.AddForEachStatement("type", "types", iteration =>
+                        {
+                            iteration.AddObjectInitStatement("var instance",
+                                new CSharpInvocationStatement("Activator.CreateInstance")
+                                    .AddArgument("type")
+                                    .AddArgument("true"));
+
+                            iteration.AddObjectInitStatement("var methodInfo", @"type.GetMethod(""Mapping"")
+                    ?? type.GetInterface(""IMapFrom`1"")?.GetMethod(""Mapping"");");
+
+                            var instance = outputTarget.GetProject().GetLanguageVersion().Major < 12 ? "new object[] { this }" : "[this]";
+                            iteration.AddStatement($"methodInfo?.Invoke(instance, {instance});", cfg => cfg.SeparatedFromPrevious());
+                        });
+
+                    });
+                });
+
             AddNugetDependency(NugetPackages.AutoMapper(outputTarget));
         }
+
+        [IntentManaged(Mode.Fully)]
+        public CSharpFile CSharpFile { get; }
 
         [IntentManaged(Mode.Fully, Body = Mode.Ignore)]
         protected override CSharpFileConfig DefineFileConfig()
@@ -30,6 +84,12 @@ namespace Intent.Modules.Application.AutoMapper.Templates.MappingProfile
             return new CSharpFileConfig(
                 className: $"MappingProfile",
                 @namespace: $"{OutputTarget.GetNamespace()}");
+        }
+
+        [IntentManaged(Mode.Fully)]
+        public override string TransformText()
+        {
+            return CSharpFile.ToString();
         }
 
     }
