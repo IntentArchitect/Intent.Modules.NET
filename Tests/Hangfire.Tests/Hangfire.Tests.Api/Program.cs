040011b5b48b889de931c370d9984eb37c150a2f
JP Seini <jp.seini@intentarchitect.com>
2024-12-05 10:32:37 +0200

    Update tests

--- Tests/Hangfire.Tests/Hangfire.Tests.Api/Program.cs
+++ Tests/Hangfire.Tests/Hangfire.Tests.Api/Program.cs
@@ -1,6 +1,12 @@
 using System;
+using Hangfire.Tests.Api.Configuration;
+using Hangfire.Tests.Api.Filters;
+using Hangfire.Tests.Application;
+using Hangfire.Tests.Infrastructure;
 using Intent.RoslynWeaver.Attributes;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
@@ -22,27 +28,52 @@ namespace Hangfire.Tests.Api
 
             try
             {
-                Log.Information("Starting web host");
-                CreateHostBuilder(args).Build().Run();
+                var builder = WebApplication.CreateBuilder(args);
+
+                builder.Host.UseSerilog((context, services, configuration) => configuration
+                    .ReadFrom.Configuration(context.Configuration)
+                    .ReadFrom.Services(services));
+
+                builder.Services.AddControllers(
+                    opt =>
+                    {
+                        opt.Filters.Add<ExceptionFilter>();
+                    });
+                builder.Services.AddApplication(builder.Configuration);
+                builder.Services.ConfigureApplicationSecurity(builder.Configuration);
+                builder.Services.ConfigureHealthChecks(builder.Configuration);
+                builder.Services.ConfigureProblemDetails();
+                builder.Services.ConfigureApiVersioning();
+                builder.Services.ConfigureHangfire(builder.Configuration);
+                builder.Services.AddInfrastructure(builder.Configuration);
+                builder.Services.ConfigureSwagger(builder.Configuration);
+
+                // Add services to the container.
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                app.UseSerilogRequestLogging();
+                app.UseExceptionHandler();
+                app.UseHttpsRedirection();
+                app.UseRouting();
+                app.UseAuthentication();
+                app.UseAuthorization();
+                app.MapDefaultHealthChecks();
+                app.MapControllers();
+                app.UseHangfire(builder.Configuration);
+                app.UseSwashbuckle(builder.Configuration);
+
+                app.Run();
             }
             catch (Exception ex)
             {
-                Log.Fatal(ex, "Host terminated unexpectedly");
+                Log.Fatal(ex, "Application terminated unexpectedly");
             }
             finally
             {
                 Log.CloseAndFlush();
             }
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseSerilog((context, services, configuration) => configuration
-                    .ReadFrom.Configuration(context.Configuration)
-                    .ReadFrom.Services(services))
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
     }
 }
