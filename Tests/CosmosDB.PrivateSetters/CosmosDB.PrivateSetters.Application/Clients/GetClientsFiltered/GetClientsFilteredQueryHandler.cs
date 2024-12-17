6fbaa5c6e3ddf09865275570d498fd2188492ff4
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2024-12-17 15:21:57 +0200

    Somehow failed to commit these.

---
+++ Tests/CosmosDB.PrivateSetters/CosmosDB.PrivateSetters.Application/Clients/GetClientsFiltered/GetClientsFilteredQueryHandler.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using CosmosDB.PrivateSetters.Domain.Repositories;
+using CosmosDB.PrivateSetters.Domain.Repositories.Documents;
+using Intent.RoslynWeaver.Attributes;
+using MediatR;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.Application.MediatR.QueryHandler", Version = "1.0")]
+
+namespace CosmosDB.PrivateSetters.Application.Clients.GetClientsFiltered
+{
+    [IntentManaged(Mode.Merge, Signature = Mode.Fully)]
+    public class GetClientsFilteredQueryHandler : IRequestHandler<GetClientsFilteredQuery, List<ClientDto>>
+    {
+        private readonly IClientRepository _clientRepository;
+        private readonly IMapper _mapper;
+
+        [IntentManaged(Mode.Merge)]
+        public GetClientsFilteredQueryHandler(IClientRepository clientRepository, IMapper mapper)
+        {
+            _clientRepository = clientRepository;
+            _mapper = mapper;
+        }
+
+        [IntentManaged(Mode.Fully, Body = Mode.Fully)]
+        public async Task<List<ClientDto>> Handle(GetClientsFilteredQuery request, CancellationToken cancellationToken)
+        {
+            IQueryable<IClientDocument> FilterClients(IQueryable<IClientDocument> queryable)
+            {
+                if (request.Type != null)
+                {
+                    queryable = queryable.Where(x => x.Type == request.Type);
+                }
+
+                if (request.Name != null)
+                {
+                    queryable = queryable.Where(x => x.Name == request.Name);
+                }
+                return queryable;
+            }
+
+            var entity = await _clientRepository.FindAllAsync(FilterClients, cancellationToken);
+            return entity.MapToClientDtoList(_mapper);
+        }
+    }
+}
