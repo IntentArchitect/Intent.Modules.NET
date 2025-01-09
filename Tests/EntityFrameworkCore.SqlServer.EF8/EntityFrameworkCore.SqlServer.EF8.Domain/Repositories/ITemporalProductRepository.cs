c2a13ad7fe8da02dbf1b413e38af96c6dfbe5fff
JP Seini <jp.seini@intentarchitect.com>
2025-01-09 13:53:54 +0200

    Updated tests

--- Tests/EntityFrameworkCore.SqlServer.EF8/EntityFrameworkCore.SqlServer.EF8.Domain/Repositories/ITemporalProductRepository.cs
+++ Tests/EntityFrameworkCore.SqlServer.EF8/EntityFrameworkCore.SqlServer.EF8.Domain/Repositories/ITemporalProductRepository.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
+using EntityFrameworkCore.SqlServer.EF8.Domain.Common;
 using EntityFrameworkCore.SqlServer.EF8.Domain.Entities;
 using Intent.RoslynWeaver.Attributes;
 
@@ -17,5 +20,8 @@ namespace EntityFrameworkCore.SqlServer.EF8.Domain.Repositories
         Task<TemporalProduct?> FindByIdAsync(Guid id1, CancellationToken cancellationToken = default);
         [IntentManaged(Mode.Fully)]
         Task<List<TemporalProduct>> FindByIdsAsync(Guid[] id1s, CancellationToken cancellationToken = default);
+        Task<List<TemporalHistory<TemporalProduct>>> FindHistoryAsync(TemporalHistoryQueryOptions historyOptions, CancellationToken cancellationToken = default);
+        Task<List<TemporalHistory<TemporalProduct>>> FindHistoryAsync(TemporalHistoryQueryOptions historyOptions, Expression<Func<TemporalProduct, bool>> filterExpression, CancellationToken cancellationToken = default);
+        Task<List<TemporalHistory<TemporalProduct>>> FindHistoryAsync(TemporalHistoryQueryOptions historyOptions, Expression<Func<TemporalProduct, bool>> filterExpression, Func<IQueryable<TemporalProduct>, IQueryable<TemporalProduct>> queryOptions, CancellationToken cancellationToken = default);
     }
 }
--- Tests/EntityFrameworkCore.SqlServer.EF8/EntityFrameworkCore.SqlServer.EF8.Infrastructure/Repositories/TemporalProductRepository.cs
+++ Tests/EntityFrameworkCore.SqlServer.EF8/EntityFrameworkCore.SqlServer.EF8.Infrastructure/Repositories/TemporalProductRepository.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
+using EntityFrameworkCore.SqlServer.EF8.Domain.Common;
 using EntityFrameworkCore.SqlServer.EF8.Domain.Entities;
 using EntityFrameworkCore.SqlServer.EF8.Domain.Repositories;
 using EntityFrameworkCore.SqlServer.EF8.Infrastructure.Persistence;
 using Intent.RoslynWeaver.Attributes;
+using Microsoft.EntityFrameworkCore;
 
 [assembly: DefaultIntentManaged(Mode.Fully)]
 [assembly: IntentTemplate("Intent.EntityFrameworkCore.Repositories.Repository", Version = "1.0")]
@@ -28,6 +31,64 @@ namespace EntityFrameworkCore.SqlServer.EF8.Infrastructure.Repositories
         public async Task<List<TemporalProduct>> FindByIdsAsync(Guid[] id1s, CancellationToken cancellationToken = default)
         {
             return await FindAllAsync(x => id1s.Contains(x.Id1), cancellationToken);
+        }
+
+        public async Task<List<TemporalHistory<TemporalProduct>>> FindHistoryAsync(
+            TemporalHistoryQueryOptions historyOptions,
+            CancellationToken cancellationToken = default)
+        {
+            return await FindHistoryAsync(historyOptions, null, null, cancellationToken);
+        }
+
+        public async Task<List<TemporalHistory<TemporalProduct>>> FindHistoryAsync(
+            TemporalHistoryQueryOptions historyOptions,
+            Expression<Func<TemporalProduct, bool>> filterExpression,
+            CancellationToken cancellationToken = default)
+        {
+            return await FindHistoryAsync(historyOptions, filterExpression, null, cancellationToken);
+        }
+
+        public async Task<List<TemporalHistory<TemporalProduct>>> FindHistoryAsync(
+            TemporalHistoryQueryOptions historyOptions,
+            Expression<Func<TemporalProduct, bool>> filterExpression,
+            Func<IQueryable<TemporalProduct>, IQueryable<TemporalProduct>> queryOptions,
+            CancellationToken cancellationToken = default)
+        {
+            var internalDateFrom = (historyOptions is null || historyOptions.DateFrom == null || historyOptions.DateFrom == DateTime.MinValue) ? DateTime.MinValue : historyOptions!.DateFrom.Value;
+            var internalDateTo = (historyOptions is null || historyOptions.DateTo == null || historyOptions.DateTo == DateTime.MinValue) ? DateTime.MinValue : historyOptions!.DateTo.Value;
+            var queryType = (historyOptions is null || historyOptions?.QueryType == null) ? TemporalHistoryQueryType.All : historyOptions.QueryType.Value;
+            var dbSet = GetSet();
+            var queryable = GetEntityQueryable(queryType);
+
+            if (filterExpression != null)
+            {
+                queryable = queryable.Where(filterExpression);
+            }
+
+            if (queryOptions != null)
+            {
+                queryable = queryOptions(queryable);
+            }
+            return await queryable.Select(entity => new TemporalHistory<TemporalProduct>(entity, EF.Property<DateTime>(entity, "StartDate"), EF.Property<DateTime>(entity, "EndDate"))).ToListAsync(cancellationToken);
+
+            IQueryable<TemporalProduct> GetEntityQueryable(TemporalHistoryQueryType queryType)
+            {
+                switch (queryType)
+                {
+                    case TemporalHistoryQueryType.All:
+                        return dbSet.TemporalAll();
+                    case TemporalHistoryQueryType.AsOf:
+                        return dbSet.TemporalAsOf(internalDateFrom);
+                    case TemporalHistoryQueryType.Between:
+                        return dbSet.TemporalBetween(internalDateFrom, internalDateTo);
+                    case TemporalHistoryQueryType.ContainedIn:
+                        return dbSet.TemporalContainedIn(internalDateFrom, internalDateTo);
+                    case TemporalHistoryQueryType.FromTo:
+                        return dbSet.TemporalFromTo(internalDateFrom, internalDateTo);
+                    default:
+                        return dbSet.TemporalBetween(internalDateFrom, internalDateTo);
+                }
+            }
         }
     }
 }
