b04ba44e60c90f009e6c9967e95888075c05bb8b
Dandré <dandrejvv@users.noreply.github.com>
2025-02-01 22:37:05 +0200

    Removed Loader Settings stereotype

--- Modules/Intent.Modules.Blazor.Components.MudBlazor/ComponentRenderer/ComponentViewBuilder.cs
+++ Modules/Intent.Modules.Blazor.Components.MudBlazor/ComponentRenderer/ComponentViewBuilder.cs
@@ -23,31 +23,31 @@ public class ComponentViewBuilder : IRazorComponentBuilder
 
     public IEnumerable<IRazorFileNode> BuildComponent(IElement component, IRazorFileNode parentNode)
     {
-        var view = new ComponentViewModel(component);
-        if (view.HasLoaderSettings())
-        {
-            var isLoadingProperty = _bindingManager.GetElementBinding(view, "2cfd43b2-2a18-4ac0-8cf3-d1aec9d7e699", isTargetNullable: false);
-            var loadingCode = IRazorCodeDirective.Create(new CSharpStatement($"@if ({isLoadingProperty})"), _componentTemplate.RazorFile);
-            loadingCode.AddHtmlElement("MudProgressLinear", loadingBar =>
-            {
-                loadingBar.AddAttribute("Color", "Color.Primary");
-                loadingBar.AddAttribute("Indeterminate", "true");
-                loadingBar.AddAttribute("Class", "my-7");
-            });
-            parentNode.AddChildNode(loadingCode);
-
-            var errorMessageProperty = _bindingManager.GetElementBinding(view, "2e482e27-b176-43cf-b80a-33123036142a", isTargetNullable: true);
-            if (errorMessageProperty != null)
-            {
-                var errorCode = IRazorCodeDirective.Create(new CSharpStatement($"@if (!{isLoadingProperty} && {errorMessageProperty} != null)"), _componentTemplate.RazorFile);
-                errorCode.AddHtmlElement("MudAlert", alert =>
-                {
-                    alert.AddAttribute("Severity", "Severity.Error");
-                    alert.WithText("@" + errorMessageProperty);
-                });
-                parentNode.AddChildNode(errorCode);
-            }
-        }
+        // var view = new ComponentViewModel(component);
+        // if (view.HasLoaderSettings())
+        // {
+        //     var isLoadingProperty = _bindingManager.GetElementBinding(view, "2cfd43b2-2a18-4ac0-8cf3-d1aec9d7e699", isTargetNullable: false);
+        //     var loadingCode = IRazorCodeDirective.Create(new CSharpStatement($"@if ({isLoadingProperty})"), _componentTemplate.RazorFile);
+        //     loadingCode.AddHtmlElement("MudProgressLinear", loadingBar =>
+        //     {
+        //         loadingBar.AddAttribute("Color", "Color.Primary");
+        //         loadingBar.AddAttribute("Indeterminate", "true");
+        //         loadingBar.AddAttribute("Class", "my-7");
+        //     });
+        //     parentNode.AddChildNode(loadingCode);
+        //
+        //     var errorMessageProperty = _bindingManager.GetElementBinding(view, "2e482e27-b176-43cf-b80a-33123036142a", isTargetNullable: true);
+        //     if (errorMessageProperty != null)
+        //     {
+        //         var errorCode = IRazorCodeDirective.Create(new CSharpStatement($"@if (!{isLoadingProperty} && {errorMessageProperty} != null)"), _componentTemplate.RazorFile);
+        //         errorCode.AddHtmlElement("MudAlert", alert =>
+        //         {
+        //             alert.AddAttribute("Severity", "Severity.Error");
+        //             alert.WithText("@" + errorMessageProperty);
+        //         });
+        //         parentNode.AddChildNode(errorCode);
+        //     }
+        // }
 
         //var mudGrid = new HtmlElement("MudGrid", _componentTemplate.RazorFile);
         //parentNode.AddChildNode(mudGrid);
