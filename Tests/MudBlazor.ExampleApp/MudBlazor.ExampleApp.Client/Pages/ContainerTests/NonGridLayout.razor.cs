d2f25847f6139da995195468300c8affb032aa2f
Joel Turner <joel.turner@intentarchitect.com>
2025-02-24 16:39:22 +0200

    Added scenarios around container generation issues.

---
+++ Tests/MudBlazor.ExampleApp/MudBlazor.ExampleApp.Client/Pages/ContainerTests/NonGridLayout.razor.cs
@@ -0,0 +1,14 @@
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.Blazor.Templates.Client.RazorComponentCodeBehindTemplate", Version = "1.0")]
+
+namespace MudBlazor.ExampleApp.Client.Pages.ContainerTests
+{
+    public partial class NonGridLayout
+    {
+        protected override async Task OnInitializedAsync()
+        {
+        }
+    }
+}
