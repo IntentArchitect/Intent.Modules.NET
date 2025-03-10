c150bbe6a56f0cade649c3cd82f3067d025380c4
JP Seini <jp.seini@intentarchitect.com>
2025-03-10 13:34:18 +0200

    Updated tests

---
+++ Tests/AdvancedMappingCrud.RichDomain.Tests/AdvancedMappingCrud.RichDomain.Tests.Domain/Contracts/ColumnDC.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.Entities.DataContract", Version = "1.0")]
+
+namespace AdvancedMappingCrud.RichDomain.Tests.Domain.Contracts
+{
+    public record ColumnDC
+    {
+        public ColumnDC(string customColumn)
+        {
+            CustomColumn = customColumn;
+        }
+
+        [Column("CUSTOM_COLUMN")]
+        public string CustomColumn { get; init; }
+    }
+}
