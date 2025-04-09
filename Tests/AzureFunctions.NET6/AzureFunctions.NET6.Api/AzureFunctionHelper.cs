e43713c876a7ba9176d5d2cdeb8f974ece4d93af
Dandré <dandre.jvv@intentarchitect.com>
2025-04-09 18:47:12 +0200

    Fix

--- Tests/AzureFunctions.NET6/AzureFunctions.NET6.Api/AzureFunctionHelper.cs
+++ Tests/AzureFunctions.NET6/AzureFunctions.NET6.Api/AzureFunctionHelper.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using Intent.RoslynWeaver.Attributes;
 using Microsoft.AspNetCore.Http;
 
