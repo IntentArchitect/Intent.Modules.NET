0a5e46155353d9e9f1665f8596ad6d09c7437b2b
Dandré <dandre.jvv@intentarchitect.com>
2025-04-15 14:56:25 +0200

    Update

--- Tests/AdvancedMappingCrud.Repositories.Tests/AdvancedMappingCrud.Repositories.Tests.Infrastructure/Repositories/ServiceToServiceInvocations/ServiceStoredProcRepository.cs
+++ Tests/AdvancedMappingCrud.Repositories.Tests/AdvancedMappingCrud.Repositories.Tests.Infrastructure/Repositories/ServiceToServiceInvocations/ServiceStoredProcRepository.cs
@@ -20,7 +20,7 @@ namespace AdvancedMappingCrud.Repositories.Tests.Infrastructure.Repositories.Ser
         }
 
         [IntentManaged(Mode.Fully, Body = Mode.Ignore)]
-        public List<Domain.Contracts.ServiceToServiceInvocations.GetDataEntry> GetData()
+        public List<GetDataEntry> GetData()
         {
             // TODO: Implement GetData (ServiceStoredProcRepository) functionality
             throw new NotImplementedException("Your implementation here...");
