691c36222cad73916875d4aa8fddc01f2dbb9293
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2025-05-28 16:15:12 +0200

    Update tests.

---
+++ Tests/Publish.CleanArch.MassTransit.OutboxNone.TestApplication/Publish.CleanArch.MassTransit.OutboxNone.TestApplication.Application/AssemblyAttributes.cs
@@ -0,0 +1,7 @@
+using System.Diagnostics.CodeAnalysis;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.Eventing.Contracts.AssemblyAttributes", Version = "1.0")]
+[assembly: SuppressMessage("Formatting", "IDE0130:Namespace does not match folder structure.", Target = "MassTransit.Messages.Shared", Scope = "namespaceanddescendants", Justification = "Message namespaces need to be consistent between applications for deserialization to work")]
+[assembly: SuppressMessage("Formatting", "IDE0130:Namespace does not match folder structure.", Target = "MassTransit.Messages.Shared.Baskets", Scope = "namespaceanddescendants", Justification = "Message namespaces need to be consistent between applications for deserialization to work")]
