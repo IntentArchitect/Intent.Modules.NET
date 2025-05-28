e1cb2a225b2386b57150502756fe5632528381d4
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2025-05-28 23:14:49 +0200

    Fix issue breaking tests.

--- Tests/Subscribe.CleanArchDapr.TestApplication/Subscribe.CleanArchDapr.TestApplication.Application/AssemblyAttributes.cs
+++ Tests/Subscribe.CleanArchDapr.TestApplication/Subscribe.CleanArchDapr.TestApplication.Application/AssemblyAttributes.cs
@@ -3,4 +3,5 @@ using Intent.RoslynWeaver.Attributes;
 
 [assembly: DefaultIntentManaged(Mode.Fully)]
 [assembly: IntentTemplate("Intent.Eventing.Contracts.AssemblyAttributes", Version = "1.0")]
+
 [assembly: SuppressMessage("Formatting", "IDE0130:Namespace does not match folder structure.", Target = "Publish.CleanArchDapr.TestApplication.Eventing.Messages", Scope = "namespaceanddescendants", Justification = "Message namespaces need to be consistent between applications for deserialization to work")]
