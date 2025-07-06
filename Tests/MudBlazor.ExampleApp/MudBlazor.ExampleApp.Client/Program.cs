3fd6b0e07c827db0894c8bc9e09d3d8e8815f2a1
JP Seini <jp.seini@intentarchitect.com>
2025-07-06 20:38:07 +0200

    Update tests

--- Tests/MudBlazor.ExampleApp/MudBlazor.ExampleApp.Client/Program.cs
+++ Tests/MudBlazor.ExampleApp/MudBlazor.ExampleApp.Client/Program.cs
@@ -16,6 +16,8 @@ namespace MudBlazor.ExampleApp.Client
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddClientServices(builder.Configuration);
 
+            builder.Services.AddAuthorizationCore();
+
             builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
