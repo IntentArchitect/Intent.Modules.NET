09df5468f00f9ff545670141f4e18e97bf66f8f7
Dandré <dandre.jvv@intentarchitect.com>
2025-07-10 23:03:21 +0200

    Update

---
+++ Modules/Intent.Modules.SqlServerImporter/Tasks/Models/ImportFilterModel.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Intent.Modules.SqlServerImporter.Tasks.Models;
+
+public class ImportFilterModel
+{
+    [JsonPropertyName("schemas")]
+    public List<string> Schemas { get; set; } = [];
+
+    [JsonPropertyName("include_tables")]
+    public List<FilterTableModel> IncludeTables { get; set; } = [];
+
+    [JsonPropertyName("include_views")]
+    public List<FilterViewModel> IncludeViews { get; set; } = [];
+
+    [JsonPropertyName("include_stored_procedures")]
+    public List<string> IncludeStoredProcedures { get; set; } = [];
+
+    [JsonPropertyName("exclude_tables")]
+    public List<string> ExcludeTables { get; set; } = [];
+
+    [JsonPropertyName("exclude_views")]
+    public List<string> ExcludeViews { get; set; } = [];
+
+    [JsonPropertyName("exclude_stored_procedures")]
+    public List<string> ExcludeStoredProcedures { get; set; } = [];
+}
+
+public class FilterTableModel
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = null!;
+
+    [JsonPropertyName("exclude_columns")]
+    public List<string> ExcludeColumns { get; set; } = [];
+}
+
+public class FilterViewModel
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = null!;
+
+    [JsonPropertyName("exclude_columns")]
+    public List<string> ExcludeColumns { get; set; } = [];
+}
+
+public class FilterLoadInputModel
+{
+    public string ImportFilterFilePath { get; set; } = null!;
+    public string? PackageFileName { get; set; }
+}
+
+public class FilterSaveInputModel
+{
+    public string ImportFilterFilePath { get; set; } = null!;
+    public string? PackageFileName { get; set; }
+    public ImportFilterModel FilterData { get; set; } = null!;
+}
+
+public class PackageLocationInputModel
+{
+    public string ApplicationId { get; set; } = null!;
+    public string PackageId { get; set; } = null!;
+} 
