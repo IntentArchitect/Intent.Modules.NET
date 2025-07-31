2de2757089426686c8fa4b646696b3da39f47558
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2025-07-31 17:39:57 +0200

    Add integration test.

--- Tests/EfCoreSoftDelete/EfCoreSoftDelete.IntegrationTests/Tests/Customers/GetDeletedCustomerByIdTests.cs
+++ Tests/EfCoreSoftDelete/EfCoreSoftDelete.IntegrationTests/Tests/Customers/GetDeletedCustomerByIdTests.cs
@@ -1,3 +1,4 @@
+using EfCoreSoftDelete.IntegrationTests.HttpClients.Customers;
 using Intent.RoslynWeaver.Attributes;
 
 [assembly: DefaultIntentManaged(Mode.Fully)]
@@ -11,6 +12,29 @@ namespace EfCoreSoftDelete.IntegrationTests.Tests.Customers
     {
         public GetDeletedCustomerByIdTests(IntegrationTestWebAppFactory factory) : base(factory)
         {
+
+        }
+
+        [Fact]
+        public async Task ItShouldNotDeleteOwnedEntities()
+        {
+            // Arrange
+            var client = new CustomersHttpClient(CreateClient());
+
+            var dataFactory = new TestDataFactory(WebAppFactory);
+            var customerId = await dataFactory.CreateCustomer();
+
+            // Act
+            await client.DeleteCustomerAsync(customerId, TestContext.Current.CancellationToken);
+
+            // Assert
+            var customer = await client.GetDeletedCustomerByIdAsync(customerId, TestContext.Current.CancellationToken);
+            Assert.NotNull(customer);
+            Assert.True(customer.IsDeleted);
+            Assert.NotNull(customer.PrimaryAddress?.PrimaryBuilding);
+            Assert.NotNull(customer.PrimaryAddress?.OtherBuildings?.SingleOrDefault());
+            Assert.NotNull(customer.OtherAddresses?.SingleOrDefault()?.PrimaryBuilding);
+            Assert.NotNull(customer.OtherAddresses?.SingleOrDefault()?.OtherBuildings?.SingleOrDefault());
         }
     }
 }
