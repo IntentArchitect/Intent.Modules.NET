63e24b8a2e14bb683ef34f004a75bb7ee30335eb
Joel Turner <joel.turner@intentarchitect.com>
2025-08-12 23:30:41 +0200

    Add dropping off css

---
+++ Modules/Intent.Modules.AI.Blazor/FactoryExtensions/WireupCssExtension.cs
@@ -0,0 +1,79 @@
+using Intent.Engine;
+using Intent.Modules.Common;
+using Intent.Modules.Common.CSharp.RazorBuilder;
+using Intent.Modules.Common.Plugins;
+using Intent.Plugins.FactoryExtensions;
+using Intent.RoslynWeaver.Attributes;
+using Intent.Utils;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.Templates.FactoryExtension", Version = "1.0")]
+
+namespace Intent.Modules.AI.Blazor.FactoryExtensions
+{
+    [IntentManaged(Mode.Fully, Body = Mode.Merge)]
+    public class WireupCssExtension : FactoryExtensionBase
+    {
+        public override string Id => "Intent.AI.Blazor.WireupCssExtension";
+
+        [IntentManaged(Mode.Ignore)]
+        public override int Order => 0;
+
+        protected override void OnAfterTemplateRegistrations(IApplication application)
+        {
+            var app = application.FindTemplateInstance<IRazorFileTemplate>("Intent.Blazor.Templates.Server.AppRazorTemplate")?.RazorFile;
+
+            if (app == null)
+            {
+                Logging.Log.Warning("Unable to install ux-theme.css. App.razor could not be found.");
+                return;
+            }
+
+            app.AfterBuild(file =>
+            {
+                // Add Blazorise dependencies
+                var baseElement = file.SelectHtmlElements("/html/head/link").SingleOrDefault(x => x.HasAttribute("href", "_content/MudBlazor/MudBlazor.min.css"));
+                if (baseElement != null)
+                {
+                    baseElement.AddBelow(
+                        new HtmlElement("link", app)
+                            .AddAttribute("rel", "stylesheet")
+                            .AddAttribute("href", "ux-theme.css"));
+                }
+            }, 100);
+
+        }
+
+    }
+
+    //Should Add Blazor Dep for this if we want to do that.
+    public static class RazorFileExtensions
+    {
+
+        public static IEnumerable<IHtmlElement> SelectHtmlElements(this IRazorFile razorFile, string selector)
+        {
+            return razorFile.ChildNodes.OfType<IHtmlElement>().SelectHtmlElements(selector.Split("/", StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        public static IEnumerable<IHtmlElement> SelectHtmlElements(this IEnumerable<IHtmlElement> nodes, string[] parts)
+        {
+            var firstPart = parts.FirstOrDefault();
+            var foundNodes = nodes.Where(x => x.Name == firstPart).ToList();
+            foreach (var found in foundNodes)
+            {
+                if (parts.Length == 1)
+                {
+                    yield return found;
+                }
+
+                foreach (var foundChildren in found.ChildNodes.OfType<IHtmlElement>().SelectHtmlElements(parts.Skip(1).ToArray()))
+                {
+                    yield return foundChildren;
+                }
+            }
+        }
+    }
+}
---
+++ Modules/Intent.Modules.AI.Blazor/Templates/StaticContentTemplateRegistrations/MudBlazorTemplateThemeCSSStaticContentTemplateRegistration.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using Intent.Engine;
+using Intent.Modules.Common.CSharp.Templates;
+using Intent.Modules.Common;
+using Intent.Modules.Common.Templates.StaticContent;
+using Intent.Registrations;
+using Intent.RoslynWeaver.Attributes;
+using Intent.Templates;
+using System.Linq;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.Templates.StaticContentTemplateRegistration", Version = "1.0")]
+
+namespace Intent.Modules.AI.Blazor.Templates.StaticContentTemplateRegistrations
+{
+    public class MudBlazorTemplateThemeCSSStaticContentTemplateRegistration : StaticContentTemplateRegistration
+    {
+        public new const string TemplateId = "Intent.Modules.AI.Blazor.Templates.StaticContentTemplateRegistrations.MudBlazorTemplateThemeCSSStaticContentTemplateRegistration";
+
+        public MudBlazorTemplateThemeCSSStaticContentTemplateRegistration() : base(TemplateId)
+        {
+        }
+
+        public override string ContentSubFolder => "MudBlazor";
+
+
+        public override string[] BinaryFileGlobbingPatterns => new string[] { "*.jpg", "*.png", "*.xlsx", "*.ico", "*.pdf" };
+
+
+        [IntentManaged(Mode.Fully, Body = Mode.Ignore)]
+        public override IReadOnlyDictionary<string, string> Replacements(IOutputTarget outputTarget) => new Dictionary<string, string>
+        {
+            ["ApplicationName"] = outputTarget.ApplicationName(),
+            ["Namespace"] = outputTarget.GetNamespace()
+
+        };
+
+        protected override OverwriteBehaviour GetDefaultOverrideBehaviour(IOutputTarget outputTarget)
+        {
+            return OverwriteBehaviour.OnceOff;
+        }
+
+        protected override void Register(ITemplateInstanceRegistry registry, IApplication application)
+        {
+            if (!application.GetInstalledModules().Any(m => m.ModuleId == "Intent.Blazor.Components.MudBlazor"))
+                return;
+
+            base.Register(registry, application);
+        }
+    }
+}
