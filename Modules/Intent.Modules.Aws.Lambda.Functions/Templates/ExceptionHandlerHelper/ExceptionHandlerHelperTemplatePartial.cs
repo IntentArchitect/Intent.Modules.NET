43630f9ef7b4d31a3a57634f50b28a6881aa1c0c
Dandré <dandrejvv@users.noreply.github.com>
2025-09-11 20:58:11 +0200

    Refactor ExceptionHandlerHelperTemplate to enhance exception handling logic and improve code readability

--- Modules/Intent.Modules.Aws.Lambda.Functions/Templates/ExceptionHandlerHelper/ExceptionHandlerHelperTemplatePartial.cs
+++ Modules/Intent.Modules.Aws.Lambda.Functions/Templates/ExceptionHandlerHelper/ExceptionHandlerHelperTemplatePartial.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Linq;
 using Intent.Engine;
 using Intent.Modules.Common;
 using Intent.Modules.Common.CSharp.Builder;
 using Intent.Modules.Common.CSharp.Templates;
+using Intent.Modules.Common.Templates;
 using Intent.RoslynWeaver.Attributes;
+using Intent.Templates;
 
 [assembly: DefaultIntentManaged(Mode.Fully)]
 [assembly: IntentTemplate("Intent.ModuleBuilder.CSharp.Templates.CSharpTemplatePartial", Version = "1.0")]
@@ -15,8 +19,11 @@ public partial class ExceptionHandlerHelperTemplate : CSharpTemplateBase<object>
     public const string TemplateId = "Intent.Aws.Lambda.Functions.ExceptionHandlerHelper";
 
     [IntentManaged(Mode.Fully, Body = Mode.Ignore)]
-    public ExceptionHandlerHelperTemplate(IOutputTarget outputTarget, object model = null) : base(TemplateId, outputTarget, model)
+    public ExceptionHandlerHelperTemplate(IOutputTarget outputTarget, object? model = null) : base(TemplateId, outputTarget, model)
     {
+        AddTypeSource("Application.ForbiddenAccessException");
+        AddTypeSource("Domain.NotFoundException");
+
         CSharpFile = new CSharpFile(this.GetNamespace(), this.GetFolderPath())
             .AddUsing("System")
             .AddUsing("System.Collections.Generic")
@@ -41,40 +48,8 @@ public partial class ExceptionHandlerHelperTemplate : CSharpTemplateBase<object>
                 {
                     m.Private().Static();
                     m.AddParameter("Exception", "exception");
-                    m.AddSwitchStatement("exception", sw =>
-                    {
-                        sw.AddCase("ValidationException validationEx", cs =>
-                        {
-                            cs.AddStatement("var errors = new List<ValidationError>();");
-                            cs.AddForEachStatement("error", "validationEx.Errors",
-                                loop => { loop.AddStatement("errors.Add(new ValidationError(error.PropertyName, error.ErrorMessage, error.ErrorCode));"); });
-                            cs.AddStatement(
-                                "return HttpResults.BadRequest(new ResponseErrors(\"ValidationError\", \"One or more validation errors occurred\", 400, errors));");
-                        });
-                        sw.AddCase("NotFoundException notFoundEx",
-                            cs =>
-                            {
-                                cs.AddStatement(
-                                    @"return HttpResults.NotFound(new ResponseDetail(""NotFound"", ""Resource not found"", 404, notFoundEx.Message ?? ""The requested resource was not found""));");
-                            });
-                        sw.AddCase("ForbiddenAccessException forbiddenEx",
-                            cs =>
-                            {
-                                cs.AddStatement(
-                                    @"return HttpResults.NewResult(HttpStatusCode.Forbidden, new ResponseDetail(""Forbidden"", ""Access forbidden"", 403, forbiddenEx.Message ?? ""You do not have permission to access this resource""));");
-                            });
-                        sw.AddCase("UnauthorizedAccessException unauthorizedEx",
-                            cs =>
-                            {
-                                cs.AddStatement(
-                                    @"return HttpResults.NewResult(HttpStatusCode.Unauthorized, new ResponseDetail(""Unauthorized"", ""Authentication required"", 401, unauthorizedEx.Message ?? ""Authentication is required to access this resource""));");
-                            });
-                        sw.AddDefault(cs =>
-                        {
-                            cs.AddStatement(
-                                @"return HttpResults.NewResult(HttpStatusCode.InternalServerError, new ResponseDetail(""InternalServerError"", ""An error occurred while processing your request"", 500, ""Please try again later or contact support if the problem persists""));");
-                        });
-                    });
+                    m.AddSwitchStatement("exception",
+                        stmt => stmt.AddMetadata("exception-switch", true));
                 });
 
                 cls.AddNestedRecord("ResponseDetail", rec=>
@@ -109,7 +84,59 @@ public partial class ExceptionHandlerHelperTemplate : CSharpTemplateBase<object>
                         ctor.AddParameter("string", "Code");
                     });
                 });
-            });
+            })
+            .OnBuild(InstallKnownExceptions);
+    }
+
+    private void InstallKnownExceptions(CSharpFile file)
+    {
+        var priClass = file.Classes.First(p => p.Name == "ExceptionHandlerHelper");
+        var switchStatement = GetExceptionSwitchStatement(priClass);
+
+        if (ExecutionContext
+            .FindTemplateInstances<IClassProvider>(TemplateDependency.OnTemplate("Application.Validation"))
+            .Concat(ExecutionContext
+                .FindTemplateInstances<IClassProvider>(TemplateDependency.OnTemplate("Application.Validation.Dto")))
+            .Any())
+        {
+            file.AddUsing("FluentValidation");
+            switchStatement.AddCase("ValidationException validationEx", block => block
+                .AddStatement("var errors = new List<ValidationError>();")
+                .AddForEachStatement("error", "validationEx.Errors",
+                    loop => loop.AddStatement("errors.Add(new ValidationError(error.PropertyName, error.ErrorMessage, error.ErrorCode));"))
+                .AddReturn(@"HttpResults.BadRequest(new ResponseErrors(""ValidationError"", ""One or more validation errors occurred"", 400, errors))"));
+        }
+
+        var forbidException = ExecutionContext.FindTemplateInstance(
+            TemplateDependency.OnTemplate("Application.ForbiddenAccessException"));
+        if (forbidException is not null)
+        {
+            switchStatement.AddCase($"{GetTypeName(forbidException)} forbiddenEx", block => block
+                .AddReturn(@"HttpResults.NewResult(HttpStatusCode.Forbidden, new ResponseDetail(""Forbidden"", ""Access forbidden"", 403, forbiddenEx.Message ?? ""You do not have permission to access this resource""))"));
+        }
+
+        switchStatement.AddCase(UseType("System.UnauthorizedAccessException") + " unauthorizedEx",
+            block => block.AddReturn(@"HttpResults.NewResult(HttpStatusCode.Unauthorized, new ResponseDetail(""Unauthorized"", ""Authentication required"", 401, unauthorizedEx.Message ?? ""Authentication is required to access this resource""))"));
+
+        var notFoundException = ExecutionContext.FindTemplateInstance(
+            TemplateDependency.OnTemplate("Domain.NotFoundException"));
+        if (notFoundException is not null)
+        {
+            switchStatement.AddCase($"{GetTypeName(notFoundException)} notFoundEx", block => block
+                .AddReturn(@"HttpResults.NotFound(new ResponseDetail(""NotFound"", ""Resource not found"", 404, notFoundEx.Message ?? ""The requested resource was not found""))"));
+        }
+
+        switchStatement.AddDefault(block =>
+        {
+            block.AddReturn(@"HttpResults.NewResult(HttpStatusCode.InternalServerError, new ResponseDetail(""InternalServerError"", ""An error occurred while processing your request"", 500, ""Please try again later or contact support if the problem persists""))");
+        });
+    }
+
+    private static CSharpSwitchStatement GetExceptionSwitchStatement(CSharpClass priClass)
+    {
+        return (CSharpSwitchStatement)priClass
+            .FindMethod("HandleException")
+            .FindStatement(p => p.HasMetadata("exception-switch"));
     }
 
     [IntentManaged(Mode.Fully)] public CSharpFile CSharpFile { get; }
