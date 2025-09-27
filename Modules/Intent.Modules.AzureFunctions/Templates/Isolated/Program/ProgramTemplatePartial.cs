31083dd92bfee111ea62a57a656139f9df435d6e
Joel Turner <joel.turner@intentarchitect.com>
2025-09-27 10:22:06 +0200

    Program File Module aligned with correct event handling

--- Modules/Intent.Modules.AzureFunctions/Templates/Isolated/Program/ProgramTemplatePartial.cs
+++ Modules/Intent.Modules.AzureFunctions/Templates/Isolated/Program/ProgramTemplatePartial.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Intent.Engine;
 using Intent.Modules.AzureFunctions.Settings;
 using Intent.Modules.AzureFunctions.Templates.Isolated.GlobalExceptionMiddleware;
 using Intent.Modules.Common;
+using Intent.Modules.Common.CSharp.AppStartup;
 using Intent.Modules.Common.CSharp.Builder;
 using Intent.Modules.Common.CSharp.Configuration;
 using Intent.Modules.Common.CSharp.DependencyInjection;
@@ -14,6 +12,12 @@ using Intent.Modules.Common.Templates;
 using Intent.Modules.Common.VisualStudio;
 using Intent.RoslynWeaver.Attributes;
 using Intent.Templates;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using static Intent.Modules.Common.CSharp.AppStartup.IAppStartupFile;
 
 [assembly: DefaultIntentManaged(Mode.Fully)]
 [assembly: IntentTemplate("Intent.ModuleBuilder.CSharp.Templates.CSharpTemplatePartial", Version = "1.0")]
@@ -25,12 +29,16 @@ namespace Intent.Modules.AzureFunctions.Templates.Isolated.Program
     {
         public const string TemplateId = "Intent.AzureFunctions.Isolated.Program";
 
-        private readonly IList<ServiceConfigurationRequest> _serviceConfigurations = [];
+        private readonly Lazy<bool> _hasMultipleInstances;
+        private readonly List<ContainerRegistrationRequest> _containerRegistrationRequests = new();
+        private readonly List<ServiceConfigurationRequest> _serviceConfigurationRequests = new();
+        private readonly CSharpLambdaBlock _configureServicesBlock;
+        private readonly IServiceConfigurationContext _serviceConfigurationContext;
 
         [IntentManaged(Mode.Fully, Body = Mode.Ignore)]
         public ProgramTemplate(IOutputTarget outputTarget, object model = null) : base(TemplateId, outputTarget, model)
         {
-            ExecutionContext.EventDispatcher.Subscribe<ServiceConfigurationRequest>(HandleServiceConfigurationRequest);
+            _hasMultipleInstances = new Lazy<bool>(() => ExecutionContext.FindTemplateInstances<ITemplate>(TemplateId).Count() > 1);
 
             if (CanRunTemplate())
             {
@@ -47,35 +55,31 @@ namespace Intent.Modules.AzureFunctions.Templates.Isolated.Program
                 ExecutionContext.EventDispatcher.Publish(new RemoveNugetPackageEvent(NugetPackages.MicrosoftAzureFunctionsExtensionsPackageName, outputTarget));
             }
 
-            //var configStatements = new CSharpLambdaBlock("(ctx, services)")
-            //    .AddStatement("var configuration = ctx.Configuration;")
-            //                    .AddStatement("services.AddApplicationInsightsTelemetryWorkerService();")
-            //                    .AddStatement("services.ConfigureFunctionsApplicationInsights();")
-            //                    .AddInvocationStatement($"services.Configure<{UseType("Microsoft.Extensions.Logging.LoggerFilterOptions")}>", conf => conf
-            //                        .AddArgument(new CSharpLambdaBlock("options"), arg =>
-            //                        {
-            //                            arg.AddStatement(
-            //                                "// The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs. Application Insights requires an explicit override.");
-            //                            arg.AddStatement(
-            //                                "// Log levels can also be configured using appsettings.json. For more information, see https://learn.microsoft.com/en-us/azure/azure-monitor/app/worker-service#ilogger-logs");
-            //                            arg.AddStatement(
-            //                                """const string applicationInsightsLoggerProvider = "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider";""");
-            //                            arg.AddStatement("var toRemove = options.Rules.FirstOrDefault(rule => rule.ProviderName == applicationInsightsLoggerProvider);");
-            //                            arg.AddIfStatement("toRemove is not null", fi =>
-            //                            {
-            //                                fi.AddStatement("options.Rules.Remove(toRemove);");
-            //                            });
-            //                        }));
 
-            //var globalExceptionConfigStatement = new CSharpLambdaBlock("(ctx, builder)");
+            var isBuilt = false;
+            OnEmitOrPublished<ServiceConfigurationRequest>(request =>
+            {
+                if (isBuilt)
+                {
+                    return;
+                }
 
-            //if (ExecutionContext.Settings.GetAzureFunctionsSettings().UseGlobalExceptionMiddleware())
-            //{
-            //    configStatements.AddStatement("services.AddSingleton<GlobalExceptionMiddleware>();");
-            //    globalExceptionConfigStatement.AddStatement($"builder.UseMiddleware<GlobalExceptionMiddleware>();");
-            //}
+                _serviceConfigurationRequests.Add(request);
+            });
+
+            OnEmitOrPublished<ContainerRegistrationRequest>(request =>
+            {
+                if (isBuilt)
+                {
+                    return;
+                }
+
+                _containerRegistrationRequests.Add(request);
+            });
 
             var configStatements = new CSharpLambdaBlock("(ctx, services)");
+            _configureServicesBlock = configStatements;
+            _serviceConfigurationContext = new ServiceConfigurationContext("configuration", "services");
 
             CSharpFile = new CSharpFile(this.GetNamespace(), this.GetFolderPath())
                 .AddUsing("Microsoft.Azure.Functions.Worker")
@@ -85,6 +89,8 @@ namespace Intent.Modules.AzureFunctions.Templates.Isolated.Program
                 .AddUsing("System.Linq")
                 .AddTopLevelStatements(tls =>
                 {
+
+
                     configStatements.AddStatement("var configuration = ctx.Configuration;")
                                 .AddStatement("services.AddApplicationInsightsTelemetryWorkerService();")
                                 .AddStatement("services.ConfigureFunctionsApplicationInsights();")
@@ -103,7 +109,7 @@ namespace Intent.Modules.AzureFunctions.Templates.Isolated.Program
                                             fi.AddStatement("options.Rules.Remove(toRemove);");
                                         });
                                     }));
-
+                    InitializeMissingServiceConfigMetadata(configStatements);
                     var globalExceptionConfigStatement = new CSharpLambdaBlock("(ctx, builder)");
 
                     if (ExecutionContext.Settings.GetAzureFunctionsSettings().UseGlobalExceptionMiddleware())
@@ -125,34 +131,43 @@ namespace Intent.Modules.AzureFunctions.Templates.Isolated.Program
 
                     tls.AddStatement(new CSharpAssignmentStatement("var host", hostConfigStatement));
                     tls.AddStatement("host.Run();", s => s.SeparatedFromPrevious());
-                })
-                .AfterBuild(file =>
-                {
-                    configStatements.AddStatements(GetServiceConfigurationStatementList());
 
-                    foreach (var request in GetRelevantServiceConfigurationRequests())
+                    foreach (var request in _serviceConfigurationRequests)
                     {
-                        foreach (var templateDependency in request.TemplateDependencies)
-                        {
-                            var template = GetTemplate<IClassProvider>(templateDependency);
-                            if (template != null)
-                            {
-                                AddUsing(template.Namespace);
-                            }
-
-                            AddTemplateDependency(templateDependency);
-                        }
-
-                        foreach (var @namespace in request.RequiredNamespaces)
-                        {
-                            AddUsing(@namespace);
-                        }
+                        ProcessServiceConfigurationRequest(request);
                     }
+
+                    foreach (var request in _containerRegistrationRequests)
+                    {
+                        ProcessContainerRegistrationRequest(request);
+                    }
+
+                    isBuilt = true;
+
+                    // Our previous subscriptions are configured to only process if "isBuilt" is false,
+                    // and we make new subscriptions below. This is because other templates which are
+                    // also subscribed may have been (by chance) instantiated after this class,
+                    // resulting in their handlers being registered after and thus would only get to
+                    // handle their subscriptions after this class which will read them all as
+                    // unhandled. So we make new subscriptions which we know will process requests
+                    // after the other handlers.
+                    OnEmitOrPublished<ServiceConfigurationRequest>(ProcessServiceConfigurationRequest);
+                    OnEmitOrPublished<ContainerRegistrationRequest>(ProcessContainerRegistrationRequest);
                 });
 
             if (ExecutionContext.GetSettings().GetAzureFunctionsSettings().UseGlobalExceptionMiddleware())
             {
                 CSharpFile.AddUsing(this.GetNamespace());
+            }
+        }
+
+        private void InitializeMissingServiceConfigMetadata(CSharpLambdaBlock configStatements)
+        {
+            foreach (var statement in configStatements.Statements.Where(s => !s.HasMetadata("startup-statement-type")))
+            {
+                statement
+                    .AddMetadata("startup-statement-type", StatementType.ConfigureServices)
+                    .AddMetadata("startup-statement-priority", 0);
             }
         }
 
@@ -174,6 +189,225 @@ namespace Intent.Modules.AzureFunctions.Templates.Isolated.Program
             OutputTarget.GetProject().AddFrameworkDependency("Microsoft.AspNetCore.App");
         }
 
+        private bool IsApplicable(
+            IEnumerable<ITemplateDependency> templateDependencies,
+            [NotNullWhen(true)] out List<(ITemplateDependency Dependency, IClassProvider ClassProvider)>? resolvedDependencies)
+        {
+            resolvedDependencies = [];
+
+            foreach (var dependency in templateDependencies)
+            {
+                var classProvider = ExecutionContext.FindTemplateInstance<IClassProvider>(dependency);
+
+                if (_hasMultipleInstances.Value &&
+                    !ExecutionContext.IsAccessibleTo(classProvider!, OutputTarget))
+                {
+                    resolvedDependencies = null;
+                    return false;
+                }
+
+                resolvedDependencies.Add((dependency, classProvider)!);
+            }
+
+            return true;
+        }
+
+
+        private void ProcessServiceConfigurationRequest(ServiceConfigurationRequest request)
+        {
+            if (request.IsHandled ||
+                !IsApplicable(request.TemplateDependencies, out var resolvedDependencies))
+            {
+                return;
+            }
+
+            foreach (var (dependency, classProvider) in resolvedDependencies)
+            {
+                AddTemplateDependency(dependency);
+                AddUsing(classProvider.Namespace);
+            }
+
+            foreach (var @namespace in request.RequiredNamespaces)
+            {
+                AddUsing(@namespace);
+            }
+
+            AddServiceConfiguration<CSharpStatement>(ctx =>
+            {
+                var parameterList = new List<string>();
+
+                foreach (var parameter in request.ExtensionMethodParameterList)
+                {
+                    switch (parameter)
+                    {
+                        case ServiceConfigurationRequest.ParameterType.Configuration:
+                            parameterList.Add(ctx.Configuration);
+                            break;
+                        default:
+                            //If its not a "magic string" parameter , use what they gave us.
+                            parameterList.Add(parameter);
+                            break;
+                    }
+                }
+
+                return $"{ctx.Services}.{request.ExtensionMethodName}({string.Join(", ", parameterList)});";
+            }, priority: request.Priority);
+        }
+
+        private void ProcessContainerRegistrationRequest(ContainerRegistrationRequest request)
+        {
+            if (request.IsHandled ||
+                !IsApplicable(request.TemplateDependencies, out var resolvedDependencies))
+            {
+                return;
+            }
+
+            foreach (var (dependency, classProvider) in resolvedDependencies)
+            {
+                AddTemplateDependency(dependency);
+                AddUsing(classProvider.Namespace);
+            }
+
+            foreach (var @namespace in request.RequiredNamespaces)
+            {
+                AddUsing(@namespace);
+            }
+
+            AddContainerRegistration<CSharpStatement>(ctx =>
+            {
+                return (request.ConcreteType.StartsWith("typeof("), request.InterfaceType != null) switch
+                {
+                    (false, false) => $"{ctx.Services}.{RegistrationType(request)}<{UseType(request.ConcreteType)}>();",
+                    (false, true) => $"{ctx.Services}.{RegistrationType(request)}<{UseType(request.InterfaceType!)}, {UseType(request.ConcreteType)}>();",
+                    (true, false) => $"{ctx.Services}.{RegistrationType(request)}({UseTypeOf(request.ConcreteType)});",
+                    (true, true) => $"{ctx.Services}.{RegistrationType(request)}({UseTypeOf(request.InterfaceType!)}, {UseTypeOf(request.ConcreteType)});"
+                };
+
+                string UseTypeOf(string type)
+                {
+                    var typeName = type.Substring("typeof(".Length, type.Length - "typeof()".Length);
+                    return $"typeof({UseType(typeName)})";
+                }
+
+                static string RegistrationType(ContainerRegistrationRequest registration)
+                {
+                    return registration.Lifetime switch
+                    {
+                        ContainerRegistrationRequest.LifeTime.Singleton => "AddSingleton",
+                        ContainerRegistrationRequest.LifeTime.PerServiceCall => "AddScoped",
+                        _ => "AddTransient"
+                    };
+                }
+            }, priority: request.Priority);
+        }
+
+        public void ConfigureServices(Action<IHasCSharpStatements, IServiceConfigurationContext> configure)
+        {
+            if (_configureServicesBlock == null)
+            {
+                /*
+                switch (_usesMinimalHostingModel, _usesTopLevelStatements)
+                {
+                    case (false, false) or (false, true):
+                        {
+                            _configureServicesBlock = _cSharpFile.Classes.Single().Methods.First(x => x.Name == "ConfigureServices");
+                            break;
+                        }
+                    case (true, false):
+                        {
+                            var block = new ServiceRegistrationStatementBlock();
+                            _configureServicesBlock = block;
+                            _cSharpFile.Classes.Single().Methods.First(x => x.Name == "Main").AddStatement(block);
+                            break;
+
+                        }
+                    case (true, true):
+                        {
+                            var block = new ServiceRegistrationStatementBlock();
+                            _configureServicesBlock = block;
+                            _cSharpFile.TopLevelStatements.AddStatement(block);
+                            break;
+                        }
+                }*/
+            }
+
+            configure(_configureServicesBlock, _serviceConfigurationContext);
+
+        }
+
+        public void AddServiceConfiguration<TStatement>(
+            Func<IServiceConfigurationContext, TStatement> create,
+            ConfigureServiceStatement<TStatement>? configure = null,
+            int? priority = null) where TStatement : CSharpStatement
+        {
+            ConfigureServices((statements, context) =>
+            {
+                var statement = create(context);
+                Insert(statements, statement, StatementType.ConfigureServices, priority);
+                configure?.Invoke(statement, context);
+            });
+        }
+
+        private static void Insert(
+            IHasCSharpStatements hasCSharpStatements,
+            CSharpStatement statement,
+            StatementType type,
+            int? priority)
+        {
+            priority ??= int.MaxValue;
+
+            statement
+                .AddMetadata("startup-statement-type", type)
+                .AddMetadata("startup-statement-priority", priority);
+
+            var insertBelow = hasCSharpStatements.Statements
+                .Where(x => x.TryGetMetadata<StatementType>("startup-statement-type", out var metadataType) &&
+                            metadataType == type)
+                .LastOrDefault(x => (int)x.GetMetadata("startup-statement-priority") <= priority);
+
+            if (insertBelow == null && type == StatementType.ContainerRegistration)
+            {
+                insertBelow = hasCSharpStatements.Statements
+                    .LastOrDefault(x => x.TryGetMetadata<StatementType>("startup-statement-type", out var metadataType) &&
+                                        metadataType == StatementType.ConfigureServices);
+            }
+
+            if (insertBelow == null)
+            {
+                hasCSharpStatements.InsertStatement(0, statement);
+                return;
+            }
+
+            insertBelow.InsertBelow(statement);
+
+            if (insertBelow.HasMetadata("startup-statement-requires-line-after"))
+            {
+                statement.SeparatedFromPrevious();
+            }
+        }
+
+        public void AddContainerRegistration<TStatement>(
+            Func<IServiceConfigurationContext, TStatement> create,
+            ConfigureServiceStatement<TStatement>? configure = null,
+            int? priority = null) where TStatement : CSharpStatement
+        {
+            ConfigureServices((statements, context) =>
+            {
+                var statement = create(context);
+                Insert(statements, statement, StatementType.ContainerRegistration, priority);
+                configure?.Invoke(statement, context);
+            });
+        }
+
+        private enum StatementType
+        {
+            ConfigureServices,
+            ContainerRegistration,
+            AppConfiguration
+        }
+        private record ServiceConfigurationContext(string Configuration, string Services) : IServiceConfigurationContext;
+
+        /*
         private void HandleServiceConfigurationRequest(ServiceConfigurationRequest request)
         {
             _serviceConfigurations.Add(request);
@@ -232,7 +466,7 @@ namespace Intent.Modules.AzureFunctions.Templates.Isolated.Program
             }
 
             return string.Join(", ", paramList);
-        }
+        }*/
 
         [IntentManaged(Mode.Fully)]
         public CSharpFile CSharpFile { get; }
