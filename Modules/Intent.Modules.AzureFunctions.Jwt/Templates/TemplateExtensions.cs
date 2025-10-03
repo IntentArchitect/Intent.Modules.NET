4f966470d4e9594b515f16b651331ea16b6b2768
Dandré <dandrejvv@users.noreply.github.com>
2025-10-03 12:44:03 +0200

    Add CurrentUserServiceExtension and related templates for JWT authentication support

--- Modules/Intent.Modules.AzureFunctions.Jwt/Templates/TemplateExtensions.cs
+++ Modules/Intent.Modules.AzureFunctions.Jwt/Templates/TemplateExtensions.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Intent.Modules.AzureFunctions.Jwt.Templates.CurrentUser;
 using Intent.Modules.AzureFunctions.Jwt.Templates.JwtClaimsMiddleware;
 using Intent.Modules.Common.Templates;
 using Intent.RoslynWeaver.Attributes;
@@ -11,6 +12,10 @@ namespace Intent.Modules.AzureFunctions.Jwt.Templates
 {
     public static class TemplateExtensions
     {
+        public static string GetCurrentUserName(this IIntentTemplate template)
+        {
+            return template.GetTypeName(CurrentUserTemplate.TemplateId);
+        }
         public static string GetJwtClaimsMiddlewareName(this IIntentTemplate template)
         {
             return template.GetTypeName(JwtClaimsMiddlewareTemplate.TemplateId);
