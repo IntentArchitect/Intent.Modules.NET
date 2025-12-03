723fac31a1644b820a63a906f36b394d652d5f9e
Dandré <dandrejvv@users.noreply.github.com>
2025-12-03 11:11:59 +0200

    Removed test project

--- Modules/CompositePublishTest/Infrastructure.Terraform/01-applications/azure-event-grid-resources.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_eventgrid_topic" "client_created_event" {
-  name                = "client-created-event"
-  location            = azurerm_resource_group.main_rg.location
-  resource_group_name = azurerm_resource_group.main_rg.name
-  input_schema        = "CloudEventSchemaV1_0"
-}
