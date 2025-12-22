7c11b9abde9efbd6edd10068854322b6c2f34bc4
Dandré <dandrejvv@users.noreply.github.com>
2025-12-22 23:32:16 +0200

    Update XML files and templates for improved structure and consistency

--- Modules/Intent.Modules.Entities/Templates/CollectionWrapper/CollectionWrapperTemplatePartial.cs
+++ Modules/Intent.Modules.Entities/Templates/CollectionWrapper/CollectionWrapperTemplatePartial.cs
@@ -16,22 +16,153 @@ using Intent.Templates;
 namespace Intent.Modules.Entities.Templates.CollectionWrapper
 {
     [IntentManaged(Mode.Fully, Body = Mode.Merge)]
-    partial class CollectionWrapperTemplate : CSharpTemplateBase<object>
+    public partial class CollectionWrapperTemplate : CSharpTemplateBase<object>, ICSharpFileBuilderTemplate
     {
         public const string TemplateId = "Intent.Entities.CollectionWrapper";
 
         [IntentManaged(Mode.Fully, Body = Mode.Ignore)]
         public CollectionWrapperTemplate(IOutputTarget outputTarget, object model = null) : base(TemplateId, outputTarget, model)
         {
-        }
+            CSharpFile = new CSharpFile(this.GetNamespace(), this.GetFolderPath())
+                .AddUsing("System.Collections")
+                .AddUsing("System.Collections.Generic")
+                .AddUsing("System.Linq")
+                .AddClass(@"CollectionWrapper", @class =>
+                {
+                    @class.AddGenericParameter("TInterface", out var TInterface);
+                    @class.AddGenericParameter("TImplementation", out var TImplementation);
+                    @class.AddGenericTypeConstraint(TImplementation, type => type.AddType(TInterface));
+                    @class.WithComments($$"""
+                                          /// <summary>
+                                          /// Provides a wrapper over an <see cref="ICollection{{{TImplementation}}}"/> to make it behave as an <see cref="ICollection{{{TInterface}}}"/>.  
+                                          /// </summary>
+                                          /// <typeparam name="{{TInterface}}">The interface type the collection should be exposed as.</typeparam>
+                                          /// <typeparam name="{{TImplementation}}">The actual type of the items in the collection. Must implement <typeparamref name="{{TInterface}}"/>.</typeparam>
+                                          """);
+                    @class.ImplementsInterface($"ICollection<{TInterface}>");
 
-        [IntentManaged(Mode.Fully, Body = Mode.Ignore)]
+                    @class.AddField($"ICollection<{TImplementation}>", "_wrappedCollection", field =>
+                    {
+                        field.PrivateReadOnly();
+                    });
+
+                    @class.AddConstructor(ctor =>
+                    {
+                        ctor.WithComments($$"""
+                                          /// <summary>
+                                          /// Initializes a new instance of the <see cref="CollectionWrapper{{{TInterface}}, {{TImplementation}}}"/> class.
+                                          /// </summary>
+                                          /// <param name="wrappedCollection">The collection to be wrapped.</param>
+                                          """);
+                        ctor.AddParameter($"ICollection<{TImplementation}>", "wrappedCollection");
+                        ctor.AddStatement("_wrappedCollection = wrappedCollection;");
+                    });
+
+                    @class.AddMethod($"IEnumerator<{TInterface}>", "GetEnumerator", method =>
+                    {
+                        method.WithComments("/// <inheritdoc />");
+                        method.AddStatement(@"return _wrappedCollection.Cast<TInterface>().GetEnumerator();");
+                    });
+
+                    @class.AddMethod("IEnumerator", "GetEnumerator", method =>
+                    {
+                        method.WithComments("/// <inheritdoc />");
+                        method.WithExpressionBody("GetEnumerator()");
+                        method.AddMetadata("explicit-interface", "IEnumerable");
+                    });
+
+                    @class.AddMethod("void", "Add", method =>
+                    {
+                        method.WithComments("/// <inheritdoc />");
+                        method.AddParameter("TInterface", "item");
+                        method.AddStatement(@"_wrappedCollection.Add((TImplementation)item!);");
+                    });
+
+                    @class.AddMethod("void", "Clear", method =>
+                    {
+                        method.WithComments("/// <inheritdoc />");
+                        method.AddStatement(@"_wrappedCollection.Clear();");
+                    });
+
+                    @class.AddMethod("bool", "Contains", method =>
+                    {
+                        method.WithComments("/// <inheritdoc />");
+                        method.AddParameter("TInterface", "item");
+                        method.AddStatement(@"return _wrappedCollection.Contains((TImplementation)item!);");
+                    });
+
+                    @class.AddMethod("void", "CopyTo", method =>
+                    {
+                        method.WithComments("/// <inheritdoc />");
+                        method.AddParameter("TInterface[]", "array");
+                        method.AddParameter("int", "arrayIndex");
+                        method.AddStatement(@"_wrappedCollection.Cast<TInterface>().ToArray().CopyTo(array, arrayIndex);");
+                    });
+
+                    @class.AddMethod("bool", "Remove", method =>
+                    {
+                        method.WithComments("/// <inheritdoc />");
+                        method.AddParameter("TInterface", "item");
+                        method.AddStatement(@"return _wrappedCollection.Remove((TImplementation)item!);");
+                    });
+
+                    @class.AddProperty("int", "Count", property =>
+                    {
+                        property.WithComments("/// <inheritdoc />");
+                        property.WithoutSetter();
+                        property.Getter.WithExpressionImplementation("_wrappedCollection.Count");
+                    });
+
+                    @class.AddProperty("bool", "IsReadOnly", property =>
+                    {
+                        property.WithComments("/// <inheritdoc />");
+                        property.WithoutSetter();
+                        property.Getter.WithExpressionImplementation("_wrappedCollection.IsReadOnly");
+                    });
+                })
+                .AddClass(@"CollectionWrapperExtensions", @class =>
+                {
+                    @class.Static();
+                    @class.WithComments("""
+                                        /// <summary>
+                                        /// Provides extension methods for <see cref="ICollection{T}"/>.
+                                        /// </summary>
+                                        """);
+
+                    @class.AddMethod("ICollection<TInterface>", "CreateWrapper", method =>
+                    {
+                        method.Static();
+                        method.AddGenericParameter("TInterface", out var TInterface);
+                        method.AddGenericParameter("TImplementation", out var TImplementation);
+                        method.AddGenericTypeConstraint(TImplementation, type => type.AddType(TInterface));
+                        method.WithComments($$"""
+                                            /// <summary>
+                                            /// Creates a wrapper for a collection to expose it as a different interface.
+                                            /// </summary>
+                                            /// <typeparam name="{{TInterface}}">The interface type the collection should be exposed as.</typeparam>
+                                            /// <typeparam name="{{TImplementation}}">The actual type of the items in the collection. Must implement <typeparamref name="{{TInterface}}"/>.</typeparam>
+                                            /// <param name="collection">The collection to be wrapped.</param>
+                                            /// <returns>An <see cref="ICollection{{{TInterface}}}"/> that wraps the provided collection.</returns>
+                                            """);
+                        method.AddParameter("ICollection<TImplementation>", "collection", param => param.WithThisModifier());
+                        method.AddStatement(@"return new CollectionWrapper<TInterface, TImplementation>(collection);");
+                    });
+                });
+        }
+        
+        [IntentManaged(Mode.Fully)]
+        public CSharpFile CSharpFile { get; }
+
+        [IntentManaged(Mode.Fully)]
         protected override CSharpFileConfig DefineFileConfig()
         {
-            return new CSharpFileConfig(
-                className: $"CollectionWrapper",
-                @namespace: $"{this.GetNamespace()}",
-                relativeLocation: $"{this.GetFolderPath()}");
+            return CSharpFile.GetConfig();
+        }
+
+        [IntentManaged(Mode.Fully)]
+        public override string TransformText()
+        {
+            return CSharpFile.ToString();
         }
 
         public override bool CanRunTemplate()
--- Modules/Intent.Modules.Entities/Templates/UpdateHelper/UpdateHelperTemplatePartial.cs
+++ Modules/Intent.Modules.Entities/Templates/UpdateHelper/UpdateHelperTemplatePartial.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Intent.Engine;
 using Intent.Modules.Common;
+using Intent.Modules.Common.CSharp.Builder;
 using Intent.Modules.Common.CSharp.Templates;
 using Intent.Modules.Common.Templates;
 using Intent.RoslynWeaver.Attributes;
@@ -13,22 +14,144 @@ using Intent.Templates;
 namespace Intent.Modules.Entities.Templates.UpdateHelper
 {
     [IntentManaged(Mode.Fully, Body = Mode.Merge)]
-    partial class UpdateHelperTemplate : CSharpTemplateBase<object>
+    public partial class UpdateHelperTemplate : CSharpTemplateBase<object>, ICSharpFileBuilderTemplate
     {
         public const string TemplateId = "Intent.Entities.UpdateHelper";
 
         [IntentManaged(Mode.Fully, Body = Mode.Ignore)]
         public UpdateHelperTemplate(IOutputTarget outputTarget, object model = null) : base(TemplateId, outputTarget, model)
         {
-        }
+            CSharpFile = new CSharpFile(this.GetNamespace(), this.GetFolderPath())
+                .AddUsing("System")
+                .AddUsing("System.Collections.Generic")
+                .AddClass($"UpdateHelper", @class =>
+                {
+                    @class.Static();
+                    @class.WithComments("""
+                                        /// <summary>
+                                        /// Provides utility methods for updating collections.
+                                        /// </summary>
+                                        """);
 
-        [IntentManaged(Mode.Fully, Body = Mode.Ignore)]
+                    @class.AddMethod("ICollection<TOriginal>", "CreateOrUpdateCollection", method =>
+                    {
+                        method.Static();
+                        method.AddGenericParameter("TChanged", out var TChanged);
+                        method.AddGenericParameter("TOriginal", out var TOriginal);
+                        method.WithComments($$"""
+                                            /// <summary>
+                                            /// Performs mutations to synchronize the baseCollection to end up the same as the changedCollection.
+                                            /// </summary>
+                                            /// <typeparam name="{{TChanged}}">The type of items in the changed collection.</typeparam>
+                                            /// <typeparam name="{{TOriginal}}">The type of items in the base collection.</typeparam>
+                                            /// <param name="baseCollection">The base collection to be updated.</param>
+                                            /// <param name="changedCollection">The collection containing the changes.</param>
+                                            /// <param name="equalityCheck">A predicate that determines if an item from the base collection matches an item from the changed collection.</param>
+                                            /// <param name="assignmentAction">A delegate that defines how to update an item from the base collection using an item from the changed collection.</param>
+                                            /// <returns>The updated base collection.</returns>
+                                            /// <remarks>
+                                            /// If the changed collection is <see langword="null" />, an empty list of type <typeparamref name="{{TOriginal}}"/> will be returned.
+                                            /// If the base collection is <see langword="null" />, a new list of type <typeparamref name="{{TOriginal}}"/> will be created and used.
+                                            /// </remarks>
+                                            """);
+                        method.AddParameter($"ICollection<{TOriginal}>", "baseCollection");
+                        method.AddParameter($"IEnumerable<{TChanged}>?", "changedCollection");
+                        method.AddParameter($"Func<{TOriginal}, {TChanged}, bool>", "equalityCheck");
+                        method.AddParameter($"Func<{TOriginal}?, {TChanged}, {TOriginal}>", "assignmentAction");
+                        method.AddStatement($$"""
+                                            if (changedCollection == null)
+                                            {
+                                                return new List<{{TOriginal}}>();
+                                            }
+                                            """);
+                        method.AddStatement($"baseCollection ??= new List<{TOriginal}>()!;", st => st.SeparatedFromPrevious());
+                        method.AddStatement("var result = baseCollection.CompareCollections(changedCollection, equalityCheck);", st => st.SeparatedFromPrevious());
+                        method.AddStatement("""
+                                            foreach (var elementToAdd in result.ToAdd)
+                                            {
+                                                var newEntity = assignmentAction(default, elementToAdd);
+                                                baseCollection.Add(newEntity);
+                                            }
+                                            """);
+                        method.AddStatement("""
+                                            foreach (var elementToRemove in result.ToRemove)
+                                            {
+                                                baseCollection.Remove(elementToRemove);
+                                            }
+                                            """, st => st.SeparatedFromPrevious());
+                        method.AddStatement("""
+                                            foreach (var elementToEdit in result.PossibleEdits)
+                                            {
+                                                assignmentAction(elementToEdit.Original, elementToEdit.Changed);
+                                            }
+                                            """, st => st.SeparatedFromPrevious());
+                        method.AddReturn("baseCollection", st => st.SeparatedFromPrevious());
+                    });
+
+                    @class.AddMethod("ICollection<TEntity>", "SynchronizeCollection", method =>
+                    {
+                        method.Static();
+                        method.AddGenericParameter("TEntity", out var TEntity);
+                        method.WithComments($$"""
+                                            /// <summary>
+                                            /// Synchronizes a collection by adding and removing items to match the changed collection.
+                                            /// This is useful for many-to-many relationships where entities already exist.
+                                            /// </summary>
+                                            /// <typeparam name="{{TEntity}}">The type of items in the base and changed collection.</typeparam>
+                                            /// <param name="baseCollection">The base collection to be synchronized.</param>
+                                            /// <param name="changedCollection">The collection to synchronize to.</param>
+                                            /// <param name="equalityCheck">A predicate that determines if an item from the base collection matches an item from the changed collection.</param>
+                                            /// <returns>The synchronized base collection.</returns>
+                                            /// <remarks>
+                                            /// This method does not create new entities or update existing ones. It only adds and removes items.
+                                            /// Typically used for many-to-many relationships in EF Core where both sides already exist.
+                                            /// If the changed collection is <see langword="null" />, the base collection will be cleared.
+                                            /// </remarks>
+                                            """);
+                        method.AddParameter($"ICollection<{TEntity}>", "baseCollection");
+                        method.AddParameter($"IEnumerable<{TEntity}>?", "changedCollection");
+                        method.AddParameter($"Func<{TEntity}, {TEntity}, bool>", "equalityCheck");
+                        method.AddStatement($$"""
+                                            if (changedCollection == null)
+                                            {
+                                                foreach (var entity in baseCollection)
+                                                {
+                                                    baseCollection.Remove(entity);
+                                                }
+                                                return baseCollection;
+                                            }
+                                            """);
+                        method.AddStatement("var result = baseCollection.CompareCollections(changedCollection, equalityCheck);", st => st.SeparatedFromPrevious());
+                        method.AddStatement("""
+                                            foreach (var elementToRemove in result.ToRemove)
+                                            {
+                                                baseCollection.Remove(elementToRemove);
+                                            }
+                                            """, st => st.SeparatedFromPrevious());
+                        method.AddStatement("""
+                                            foreach (var elementToAdd in result.ToAdd)
+                                            {
+                                                baseCollection.Add(elementToAdd);
+                                            }
+                                            """, st => st.SeparatedFromPrevious());
+                        method.AddReturn("baseCollection", st => st.SeparatedFromPrevious());
+                    });
+                });
+        }
+        
+        [IntentManaged(Mode.Fully)]
+        public CSharpFile CSharpFile { get; }
+
+        [IntentManaged(Mode.Fully)]
         protected override CSharpFileConfig DefineFileConfig()
         {
-            return new CSharpFileConfig(
-                className: $"UpdateHelper",
-                @namespace: $"{this.GetNamespace()}",
-                relativeLocation: $"{this.GetFolderPath()}");
+            return CSharpFile.GetConfig();
+        }
+        
+        [IntentManaged(Mode.Fully)]
+        public override string TransformText()
+        {
+            return CSharpFile.ToString();
         }
     }
 }
