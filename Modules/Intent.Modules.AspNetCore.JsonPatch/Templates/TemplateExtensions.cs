b8351f1b50cd7bbe9c1e82012a25b59f39ef164b
Dandré <dandrejvv@users.noreply.github.com>
2026-03-12 20:20:08 +0200

    feat: Implement MediatRCommandExtension for JSON Patch support and remove PatchIgnoreAttribute templates

--- Modules/Intent.Modules.AspNetCore.JsonPatch/Templates/TemplateExtensions.cs
+++ Modules/Intent.Modules.AspNetCore.JsonPatch/Templates/TemplateExtensions.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using Intent.Modules.AspNetCore.JsonPatch.Templates.Templates.JsonMergePatchExecutor;
 using Intent.Modules.AspNetCore.JsonPatch.Templates.Templates.JsonMergePatchOperationFilter;
 using Intent.Modules.AspNetCore.JsonPatch.Templates.Templates.PatchExecutorInterface;
-using Intent.Modules.AspNetCore.JsonPatch.Templates.Templates.PatchIgnoreAttribute;
 using Intent.Modules.AspNetCore.JsonPatch.Templates.Templates.PatchIgnoreMetadataProvider;
 using Intent.Modules.Common.Templates;
 using Intent.RoslynWeaver.Attributes;
@@ -28,11 +27,6 @@ namespace Intent.Modules.AspNetCore.JsonPatch.Templates
         public static string GetPatchExecutorInterfaceName(this IIntentTemplate template)
         {
             return template.GetTypeName(PatchExecutorInterfaceTemplate.TemplateId);
-        }
-
-        public static string GetPatchIgnoreAttributeName(this IIntentTemplate template)
-        {
-            return template.GetTypeName(PatchIgnoreAttributeTemplate.TemplateId);
         }
 
         public static string GetPatchIgnoreMetadataProviderName(this IIntentTemplate template)
