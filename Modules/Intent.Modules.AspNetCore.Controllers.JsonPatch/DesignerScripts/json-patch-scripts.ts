74be098c7b3928601d3e095f4cecd4114ba86755
Dandré <dandrejvv@users.noreply.github.com>
2026-03-25 11:42:09 +0200

    feat: Implement JSON Patch service operation conversion and update DTO naming strategy

--- Modules/Intent.Modules.AspNetCore.Controllers.JsonPatch/DesignerScripts/json-patch-scripts.ts
+++ Modules/Intent.Modules.AspNetCore.Controllers.JsonPatch/DesignerScripts/json-patch-scripts.ts
@@ -53,3 +53,58 @@ function convertToJsonPatchCommand(element: MacroApi.Context.IElementApi) {
         dto.setName(newDtoName, true);
     }
 }
+
+function convertToJsonPatchServiceOperation(element: MacroApi.Context.IElementApi) {
+    // element will be the Service Operation. we can extract the HTTP Verb information from it but for its DTOs we'll need to traverse
+    // its parameters first.
+    let httpVerbProperty = element.getStereotype("Http Settings").getProperty("Verb");
+    httpVerbProperty.setValue("PATCH");
+
+    const originalServiceOperationName = element.getName();
+    const originalServiceOperationNameToLower = originalServiceOperationName.toLocaleLowerCase();
+    let newServiceOperationName = originalServiceOperationName;
+    if (originalServiceOperationNameToLower.indexOf("update") > -1) {
+        newServiceOperationName = newServiceOperationName.replace("Update", "Patch");
+    } else if (originalServiceOperationNameToLower.indexOf("edit") > -1) {
+        newServiceOperationName = newServiceOperationName.replace("Edit", "Patch");
+    } else {
+        newServiceOperationName = "Patch" + newServiceOperationName;
+    }
+    element.setName(newServiceOperationName, true);
+
+    // Recursively scan all the DTOs starting from the Service Operation's Parameters.
+
+    let serviceOperationParametersAsDtos = element.getChildren("Parameter")
+        .filter(x => x?.typeReference.getType()?.specialization === "DTO")
+        .map(x => x.typeReference.getType()!);
+    
+    const dtos = new Set<MacroApi.Context.IElementApi>();
+    const dtoStack = new Array<MacroApi.Context.IElementApi>(...serviceOperationParametersAsDtos);
+
+    while (dtoStack.length > 0) {
+        const currentDto = dtoStack.pop()!;
+        if (dtos.has(currentDto)) {
+            continue;
+        }
+        dtos.add(currentDto);
+        const childDtos = currentDto.getChildren("DTO-Field")
+            .filter(x => x?.typeReference.getType()?.specialization === "DTO")
+            .map(x => x.typeReference.getType()!);
+        dtoStack.push(...childDtos);
+    }
+
+    for (let dto of dtos) {
+        // Also update the DTO name if it contains Update or Edit
+        const originalDtoName = dto.getName();
+        const originalDtoNameToLower = originalDtoName.toLocaleLowerCase();
+        let newDtoName = originalDtoName;
+        if (originalDtoNameToLower.indexOf("update") > -1) {
+            newDtoName = newDtoName.replace("Update", "Patch");
+        } else if (originalDtoNameToLower.indexOf("edit") > -1) {
+            newDtoName = newDtoName.replace("Edit", "Patch");
+        } else {
+            newDtoName = "Patch" + newDtoName;
+        }
+        dto.setName(newDtoName, true);
+    }
+}
