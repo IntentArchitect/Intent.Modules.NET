a8a842fca8d8248661f1d83a153d68739cdf5341
Dandré <dandrejvv@users.noreply.github.com>
2026-03-25 11:48:47 +0200

    feat: Refactor JSON Patch command and service operation functions for improved readability and maintainability

--- Modules/Intent.Modules.AspNetCore.Controllers.JsonPatch/DesignerScripts/json-patch-scripts.ts
+++ Modules/Intent.Modules.AspNetCore.Controllers.JsonPatch/DesignerScripts/json-patch-scripts.ts
@@ -1,110 +1,83 @@
+const HTTP_SETTINGS_STEREOTYPE = "Http Settings";
+const HTTP_VERB_PROPERTY = "Verb";
+const PATCH_HTTP_VERB = "PATCH";
+const DTO_FIELD_CHILD_TYPE = "DTO-Field";
+const PARAMETER_CHILD_TYPE = "Parameter";
+const DTO_SPECIALIZATION = "DTO";
+const UPDATE_NAME_PART = "Update";
+const EDIT_NAME_PART = "Edit";
+const PATCH_NAME_PART = "Patch";
+
 function convertToJsonPatchCommand(element: MacroApi.Context.IElementApi) {
-    let httpVerbProperty = element.getStereotype("Http Settings").getProperty("Verb");
-    httpVerbProperty.setValue("PATCH");
-
-    const originalCommandName = element.getName();
-    const originalCommandNameToLower = originalCommandName.toLocaleLowerCase();
-
-    let newCommandName = originalCommandName;
-    if (originalCommandNameToLower.indexOf("update") > -1) {
-        newCommandName = newCommandName.replace("Update", "Patch");
-    } else if (originalCommandNameToLower.indexOf("edit") > -1) {
-        newCommandName = newCommandName.replace("Edit", "Patch");
-    } else {
-        newCommandName = "Patch" + newCommandName;
-    }
-
-    element.setName(newCommandName, true);
+    setHttpVerbToPatch(element);
+    renameElementToPatch(element);
 
     // Recursively scan all the DTOs starting from the Command's Fields.
-
-    let commandFieldsAsDtos = element.getChildren("DTO-Field")
-        .filter(x => x?.typeReference.getType()?.specialization === "DTO")
-        .map(x => x.typeReference.getType()!);
-
-    const dtos = new Set<MacroApi.Context.IElementApi>();
-    const dtoStack = new Array<MacroApi.Context.IElementApi>(...commandFieldsAsDtos);
-
-    while (dtoStack.length > 0) {
-        const currentDto = dtoStack.pop()!;
-        if (dtos.has(currentDto)) {
-            continue;
-        }
-        dtos.add(currentDto);
-        const childDtos = currentDto.getChildren("DTO-Field")
-            .filter(x => x?.typeReference.getType()?.specialization === "DTO")
-            .map(x => x.typeReference.getType()!);
-        dtoStack.push(...childDtos);
-    }
-
-    
-    for (let dto of dtos) {
-        // Also update the DTO name if it contains Update or Edit
-        const originalDtoName = dto.getName();
-        const originalDtoNameToLower = originalDtoName.toLocaleLowerCase();
-        let newDtoName = originalDtoName;
-        if (originalDtoNameToLower.indexOf("update") > -1) {
-            newDtoName = newDtoName.replace("Update", "Patch");
-        } else if (originalDtoNameToLower.indexOf("edit") > -1) {
-            newDtoName = newDtoName.replace("Edit", "Patch");
-        } else {
-            newDtoName = "Patch" + newDtoName;
-        }
-        dto.setName(newDtoName, true);
+    const commandFieldsAsDtos = getChildDtos(element, DTO_FIELD_CHILD_TYPE);
+    const dtos = collectDtos(commandFieldsAsDtos);
+    for (const dto of dtos) {
+        renameElementToPatch(dto);
     }
 }
 
 function convertToJsonPatchServiceOperation(element: MacroApi.Context.IElementApi) {
     // element will be the Service Operation. we can extract the HTTP Verb information from it but for its DTOs we'll need to traverse
     // its parameters first.
-    let httpVerbProperty = element.getStereotype("Http Settings").getProperty("Verb");
-    httpVerbProperty.setValue("PATCH");
-
-    const originalServiceOperationName = element.getName();
-    const originalServiceOperationNameToLower = originalServiceOperationName.toLocaleLowerCase();
-    let newServiceOperationName = originalServiceOperationName;
-    if (originalServiceOperationNameToLower.indexOf("update") > -1) {
-        newServiceOperationName = newServiceOperationName.replace("Update", "Patch");
-    } else if (originalServiceOperationNameToLower.indexOf("edit") > -1) {
-        newServiceOperationName = newServiceOperationName.replace("Edit", "Patch");
-    } else {
-        newServiceOperationName = "Patch" + newServiceOperationName;
-    }
-    element.setName(newServiceOperationName, true);
+    setHttpVerbToPatch(element);
+    renameElementToPatch(element);
 
     // Recursively scan all the DTOs starting from the Service Operation's Parameters.
+    const serviceOperationParametersAsDtos = getChildDtos(element, PARAMETER_CHILD_TYPE);
+    const dtos = collectDtos(serviceOperationParametersAsDtos);
+    for (const dto of dtos) {
+        renameElementToPatch(dto);
+    }
+}
 
-    let serviceOperationParametersAsDtos = element.getChildren("Parameter")
-        .filter(x => x?.typeReference.getType()?.specialization === "DTO")
+function setHttpVerbToPatch(element: MacroApi.Context.IElementApi): void {
+    const httpVerbProperty = element.getStereotype(HTTP_SETTINGS_STEREOTYPE).getProperty(HTTP_VERB_PROPERTY);
+    httpVerbProperty.setValue(PATCH_HTTP_VERB);
+}
+
+function getPatchName(originalName: string): string {
+    const originalNameToLower = originalName.toLocaleLowerCase();
+
+    if (originalNameToLower.indexOf(UPDATE_NAME_PART.toLocaleLowerCase()) > -1) {
+        return originalName.replace(UPDATE_NAME_PART, PATCH_NAME_PART);
+    }
+
+    if (originalNameToLower.indexOf(EDIT_NAME_PART.toLocaleLowerCase()) > -1) {
+        return originalName.replace(EDIT_NAME_PART, PATCH_NAME_PART);
+    }
+
+    return PATCH_NAME_PART + originalName;
+}
+
+function renameElementToPatch(element: MacroApi.Context.IElementApi): void {
+    const newName = getPatchName(element.getName());
+    element.setName(newName, true);
+}
+
+function getChildDtos(element: MacroApi.Context.IElementApi, childType: string): MacroApi.Context.IElementApi[] {
+    return element.getChildren(childType)
+        .filter(x => x?.typeReference.getType()?.specialization === DTO_SPECIALIZATION)
         .map(x => x.typeReference.getType()!);
-    
+}
+
+function collectDtos(seedDtos: MacroApi.Context.IElementApi[]): Set<MacroApi.Context.IElementApi> {
     const dtos = new Set<MacroApi.Context.IElementApi>();
-    const dtoStack = new Array<MacroApi.Context.IElementApi>(...serviceOperationParametersAsDtos);
+    const dtoStack = new Array<MacroApi.Context.IElementApi>(...seedDtos);
 
     while (dtoStack.length > 0) {
         const currentDto = dtoStack.pop()!;
         if (dtos.has(currentDto)) {
             continue;
         }
+
         dtos.add(currentDto);
-        const childDtos = currentDto.getChildren("DTO-Field")
-            .filter(x => x?.typeReference.getType()?.specialization === "DTO")
-            .map(x => x.typeReference.getType()!);
+        const childDtos = getChildDtos(currentDto, DTO_FIELD_CHILD_TYPE);
         dtoStack.push(...childDtos);
     }
 
-    for (let dto of dtos) {
-        // Also update the DTO name if it contains Update or Edit
-        const originalDtoName = dto.getName();
-        const originalDtoNameToLower = originalDtoName.toLocaleLowerCase();
-        let newDtoName = originalDtoName;
-        if (originalDtoNameToLower.indexOf("update") > -1) {
-            newDtoName = newDtoName.replace("Update", "Patch");
-        } else if (originalDtoNameToLower.indexOf("edit") > -1) {
-            newDtoName = newDtoName.replace("Edit", "Patch");
-        } else {
-            newDtoName = "Patch" + newDtoName;
-        }
-        dto.setName(newDtoName, true);
-    }
+    return dtos;
 }
--- Modules/Intent.Modules.AspNetCore.Controllers.JsonPatch/resources/scripts/json-patch-scripts.js
+++ Modules/Intent.Modules.AspNetCore.Controllers.JsonPatch/resources/scripts/json-patch-scripts.js
@@ -1,102 +1,68 @@
+const HTTP_SETTINGS_STEREOTYPE = "Http Settings";
+const HTTP_VERB_PROPERTY = "Verb";
+const PATCH_HTTP_VERB = "PATCH";
+const DTO_FIELD_CHILD_TYPE = "DTO-Field";
+const PARAMETER_CHILD_TYPE = "Parameter";
+const DTO_SPECIALIZATION = "DTO";
+const UPDATE_NAME_PART = "Update";
+const EDIT_NAME_PART = "Edit";
+const PATCH_NAME_PART = "Patch";
 function convertToJsonPatchCommand(element) {
-    let httpVerbProperty = element.getStereotype("Http Settings").getProperty("Verb");
-    httpVerbProperty.setValue("PATCH");
-    const originalCommandName = element.getName();
-    const originalCommandNameToLower = originalCommandName.toLocaleLowerCase();
-    let newCommandName = originalCommandName;
-    if (originalCommandNameToLower.indexOf("update") > -1) {
-        newCommandName = newCommandName.replace("Update", "Patch");
-    }
-    else if (originalCommandNameToLower.indexOf("edit") > -1) {
-        newCommandName = newCommandName.replace("Edit", "Patch");
-    }
-    else {
-        newCommandName = "Patch" + newCommandName;
-    }
-    element.setName(newCommandName, true);
+    setHttpVerbToPatch(element);
+    renameElementToPatch(element);
     // Recursively scan all the DTOs starting from the Command's Fields.
-    let commandFieldsAsDtos = element.getChildren("DTO-Field")
-        .filter(x => { var _a; return ((_a = x === null || x === void 0 ? void 0 : x.typeReference.getType()) === null || _a === void 0 ? void 0 : _a.specialization) === "DTO"; })
-        .map(x => x.typeReference.getType());
-    const dtos = new Set();
-    const dtoStack = new Array(...commandFieldsAsDtos);
-    while (dtoStack.length > 0) {
-        const currentDto = dtoStack.pop();
-        if (dtos.has(currentDto)) {
-            continue;
-        }
-        dtos.add(currentDto);
-        const childDtos = currentDto.getChildren("DTO-Field")
-            .filter(x => { var _a; return ((_a = x === null || x === void 0 ? void 0 : x.typeReference.getType()) === null || _a === void 0 ? void 0 : _a.specialization) === "DTO"; })
-            .map(x => x.typeReference.getType());
-        dtoStack.push(...childDtos);
-    }
-    for (let dto of dtos) {
-        // Also update the DTO name if it contains Update or Edit
-        const originalDtoName = dto.getName();
-        const originalDtoNameToLower = originalDtoName.toLocaleLowerCase();
-        let newDtoName = originalDtoName;
-        if (originalDtoNameToLower.indexOf("update") > -1) {
-            newDtoName = newDtoName.replace("Update", "Patch");
-        }
-        else if (originalDtoNameToLower.indexOf("edit") > -1) {
-            newDtoName = newDtoName.replace("Edit", "Patch");
-        }
-        else {
-            newDtoName = "Patch" + newDtoName;
-        }
-        dto.setName(newDtoName, true);
+    const commandFieldsAsDtos = getChildDtos(element, DTO_FIELD_CHILD_TYPE);
+    const dtos = collectDtos(commandFieldsAsDtos);
+    for (const dto of dtos) {
+        renameElementToPatch(dto);
     }
 }
 function convertToJsonPatchServiceOperation(element) {
     // element will be the Service Operation. we can extract the HTTP Verb information from it but for its DTOs we'll need to traverse
     // its parameters first.
-    let httpVerbProperty = element.getStereotype("Http Settings").getProperty("Verb");
-    httpVerbProperty.setValue("PATCH");
-    const originalServiceOperationName = element.getName();
-    const originalServiceOperationNameToLower = originalServiceOperationName.toLocaleLowerCase();
-    let newServiceOperationName = originalServiceOperationName;
-    if (originalServiceOperationNameToLower.indexOf("update") > -1) {
-        newServiceOperationName = newServiceOperationName.replace("Update", "Patch");
-    }
-    else if (originalServiceOperationNameToLower.indexOf("edit") > -1) {
-        newServiceOperationName = newServiceOperationName.replace("Edit", "Patch");
-    }
-    else {
-        newServiceOperationName = "Patch" + newServiceOperationName;
-    }
-    element.setName(newServiceOperationName, true);
+    setHttpVerbToPatch(element);
+    renameElementToPatch(element);
     // Recursively scan all the DTOs starting from the Service Operation's Parameters.
-    let serviceOperationParametersAsDtos = element.getChildren("Parameter")
-        .filter(x => { var _a; return ((_a = x === null || x === void 0 ? void 0 : x.typeReference.getType()) === null || _a === void 0 ? void 0 : _a.specialization) === "DTO"; })
+    const serviceOperationParametersAsDtos = getChildDtos(element, PARAMETER_CHILD_TYPE);
+    const dtos = collectDtos(serviceOperationParametersAsDtos);
+    for (const dto of dtos) {
+        renameElementToPatch(dto);
+    }
+}
+function setHttpVerbToPatch(element) {
+    const httpVerbProperty = element.getStereotype(HTTP_SETTINGS_STEREOTYPE).getProperty(HTTP_VERB_PROPERTY);
+    httpVerbProperty.setValue(PATCH_HTTP_VERB);
+}
+function getPatchName(originalName) {
+    const originalNameToLower = originalName.toLocaleLowerCase();
+    if (originalNameToLower.indexOf(UPDATE_NAME_PART.toLocaleLowerCase()) > -1) {
+        return originalName.replace(UPDATE_NAME_PART, PATCH_NAME_PART);
+    }
+    if (originalNameToLower.indexOf(EDIT_NAME_PART.toLocaleLowerCase()) > -1) {
+        return originalName.replace(EDIT_NAME_PART, PATCH_NAME_PART);
+    }
+    return PATCH_NAME_PART + originalName;
+}
+function renameElementToPatch(element) {
+    const newName = getPatchName(element.getName());
+    element.setName(newName, true);
+}
+function getChildDtos(element, childType) {
+    return element.getChildren(childType)
+        .filter(x => { var _a; return ((_a = x === null || x === void 0 ? void 0 : x.typeReference.getType()) === null || _a === void 0 ? void 0 : _a.specialization) === DTO_SPECIALIZATION; })
         .map(x => x.typeReference.getType());
+}
+function collectDtos(seedDtos) {
     const dtos = new Set();
-    const dtoStack = new Array(...serviceOperationParametersAsDtos);
+    const dtoStack = new Array(...seedDtos);
     while (dtoStack.length > 0) {
         const currentDto = dtoStack.pop();
         if (dtos.has(currentDto)) {
             continue;
         }
         dtos.add(currentDto);
-        const childDtos = currentDto.getChildren("DTO-Field")
-            .filter(x => { var _a; return ((_a = x === null || x === void 0 ? void 0 : x.typeReference.getType()) === null || _a === void 0 ? void 0 : _a.specialization) === "DTO"; })
-            .map(x => x.typeReference.getType());
+        const childDtos = getChildDtos(currentDto, DTO_FIELD_CHILD_TYPE);
         dtoStack.push(...childDtos);
     }
-    for (let dto of dtos) {
-        // Also update the DTO name if it contains Update or Edit
-        const originalDtoName = dto.getName();
-        const originalDtoNameToLower = originalDtoName.toLocaleLowerCase();
-        let newDtoName = originalDtoName;
-        if (originalDtoNameToLower.indexOf("update") > -1) {
-            newDtoName = newDtoName.replace("Update", "Patch");
-        }
-        else if (originalDtoNameToLower.indexOf("edit") > -1) {
-            newDtoName = newDtoName.replace("Edit", "Patch");
-        }
-        else {
-            newDtoName = "Patch" + newDtoName;
-        }
-        dto.setName(newDtoName, true);
-    }
+    return dtos;
 }
