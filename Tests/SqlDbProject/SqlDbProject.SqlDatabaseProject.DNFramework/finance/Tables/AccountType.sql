e03c2f55a7fe0f6dca5349573b284d3e4233806f
JP <jp.seini@intentarchitect.com>
2026-05-13 16:53:06 +0200

    Updated tests

--- Tests/SqlDbProject/SqlDbProject.SqlDatabaseProject.DNFramework/finance/Tables/AccountType.sql
+++ Tests/SqlDbProject/SqlDbProject.SqlDatabaseProject.DNFramework/finance/Tables/AccountType.sql
@@ -2,5 +2,6 @@ CREATE TABLE [finance].[AccountType]
 (
     [AccountTypeId] INT NOT NULL,
     [Description] VARCHAR(260) NOT NULL,
+    [Blank] NVARCHAR(MAX) NOT NULL,
     CONSTRAINT [PK_AccountType] PRIMARY KEY CLUSTERED ([AccountTypeId] ASC)
 );
