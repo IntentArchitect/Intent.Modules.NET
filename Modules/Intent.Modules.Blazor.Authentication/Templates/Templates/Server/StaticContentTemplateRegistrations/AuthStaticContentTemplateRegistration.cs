ca954232a322735e4b69dc83ef49aeb3fc978e26
Matthew Holmes <matthew.holmes@intentarchitect.com>
2026-06-15 15:01:27 +0200

    Finish refactor

--- Modules/Intent.Modules.Blazor.Authentication/Templates/Templates/Server/StaticContentTemplateRegistrations/AuthStaticContentTemplateRegistration.cs
+++ Modules/Intent.Modules.Blazor.Authentication/Templates/Templates/Server/StaticContentTemplateRegistrations/AuthStaticContentTemplateRegistration.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.Linq;
 using Intent.Engine;
 using Intent.Modules.Common.Templates.StaticContent;
 using Intent.Registrations;
@@ -27,11 +26,7 @@ namespace Intent.Modules.Blazor.Authentication.Templates.Templates.Server.Static
                 return;
             }
 
-            var allFiles = Directory.EnumerateFiles(contentDir, "*.*", System.IO.SearchOption.AllDirectories).ToArray();
-            var binaryFiles = GetBinaryFiles(contentDir);
-            var textFiles = allFiles.Except(binaryFiles).ToArray();
-
-            foreach (var fileFullPath in textFiles)
+            foreach (var fileFullPath in Directory.EnumerateFiles(contentDir, "*.*", System.IO.SearchOption.AllDirectories))
             {
                 var ext = Path.GetExtension(fileFullPath);
                 if (extensionFilter != null && !extensionFilter(ext))
@@ -46,22 +41,6 @@ namespace Intent.Modules.Blazor.Authentication.Templates.Templates.Server.Static
                     outputTarget => CreateTemplate(outputTarget, capturedPath, capturedRel,
                         GetOverwriteBehaviour(outputTarget, capturedRel)));
             }
-
-            foreach (var fileFullPath in binaryFiles)
-            {
-                var ext = Path.GetExtension(fileFullPath);
-                if (extensionFilter != null && !extensionFilter(ext))
-                {
-                    continue;
-                }
-
-                var fileRelativePath = Path.GetRelativePath(contentDir, fileFullPath);
-                var capturedPath = fileFullPath;
-                var capturedRel = fileRelativePath;
-                RegisterTemplate(registry, application,
-                    outputTarget => CreateBinaryTemplate(outputTarget, capturedPath, capturedRel,
-                        GetOverwriteBehaviour(outputTarget, capturedRel)));
-            }
         }
 
         private OverwriteBehaviour GetOverwriteBehaviour(IOutputTarget outputTarget, string fileRelativePath)
@@ -71,29 +50,6 @@ namespace Intent.Modules.Blazor.Authentication.Templates.Templates.Server.Static
                    || fileRelativePath.EndsWith(RazorCodeBehindSuffix, StringComparison.OrdinalIgnoreCase)
                 ? OverwriteBehaviour.Always
                 : GetDefaultOverrideBehaviour(outputTarget);
-        }
-
-        private string[] GetBinaryFiles(string contentDir)
-        {
-            if (BinaryFileGlobbingPatterns.Length == 0)
-            {
-                return Array.Empty<string>();
-            }
-
-            return Directory.EnumerateFiles(contentDir, "*.*", System.IO.SearchOption.AllDirectories)
-                .Where(IsBinaryFile)
-                .ToArray();
-        }
-
-        // All BinaryFileGlobbingPatterns are simple "*.ext" globs, so a suffix match is sufficient. This
-        // avoids both a Microsoft.Extensions.FileSystemGlobbing dependency and reflecting into the base
-        // class's private binary-file detection.
-        private bool IsBinaryFile(string fileFullPath)
-        {
-            var extension = Path.GetExtension(fileFullPath);
-            return BinaryFileGlobbingPatterns.Any(pattern =>
-                pattern.StartsWith("*.", StringComparison.Ordinal)
-                && extension.Equals(pattern.Substring(1), StringComparison.OrdinalIgnoreCase));
         }
     }
 }
