dacc1c86d088e55f40c3a1dd885663d8e0270060
Dandré <dandrejvv@users.noreply.github.com>
2026-07-31 14:29:55 +0200

    Refactor tests to use dynamic path creation
    - Updated test cases to use `Path.Combine` for paths
    - Removed hardcoded paths for better portability
    - Cleaned up logging in `NugetInstallerFactoryExtension`

--- Modules/Intent.Modules.VisualStudio.Projects.Tests/OutputTargets/OutputLocationOptionsTests.cs
+++ Modules/Intent.Modules.VisualStudio.Projects.Tests/OutputTargets/OutputLocationOptionsTests.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using Intent.Modules.VisualStudio.Projects.OutputTargets;
 using Shouldly;
 using Xunit;
@@ -6,18 +7,21 @@ namespace Intent.Modules.VisualStudio.Projects.Tests.OutputTargets
 {
     public class OutputLocationOptionsTests
     {
+        private static readonly string ContainerDirectory = Path.Combine(Path.GetTempPath(), "Container");
+        private static readonly string MyAppDirectory = Path.Combine(ContainerDirectory, "MyApp");
+
         [Fact]
         public void WhenRelativeLocationUnset_RootDirectory_ShouldBeOutputRootDirectory()
         {
-            var sut = new OutputLocationOptions(@"C:\Container\MyApp", relativeLocation: "");
+            var sut = new OutputLocationOptions(MyAppDirectory, relativeLocation: "");
 
-            sut.RootDirectory.ShouldBe(@"C:\Container\MyApp");
+            sut.RootDirectory.ShouldBe(MyAppDirectory);
         }
 
         [Fact]
         public void WhenRelativeLocationUnset_Combine_ShouldReturnValueUnchanged()
         {
-            var sut = new OutputLocationOptions(@"C:\Container\MyApp", relativeLocation: "");
+            var sut = new OutputLocationOptions(MyAppDirectory, relativeLocation: "");
 
             sut.Combine("MyProject").ShouldBe("MyProject");
         }
@@ -25,35 +29,36 @@ namespace Intent.Modules.VisualStudio.Projects.Tests.OutputTargets
         [Fact]
         public void WhenRelativeLocationIsParentShift_RootDirectory_ShouldResolveToContainer()
         {
-            var sut = new OutputLocationOptions(@"C:\Container\MyApp", relativeLocation: "..");
+            var sut = new OutputLocationOptions(MyAppDirectory, relativeLocation: "..");
 
-            sut.RootDirectory.ShouldBe(@"C:\Container");
+            sut.RootDirectory.ShouldBe(ContainerDirectory);
         }
 
         [Fact]
         public void WhenRelativeLocationIsParentShift_Combine_ShouldPrefixTheShift()
         {
-            var sut = new OutputLocationOptions(@"C:\Container\MyApp", relativeLocation: "..");
+            var sut = new OutputLocationOptions(MyAppDirectory, relativeLocation: "..");
 
-            sut.Combine("MyApp").ShouldBe(@"..\MyApp");
+            sut.Combine("MyApp").ShouldBe(Path.Combine("..", "MyApp"));
         }
 
         [Fact]
         public void WhenRawValueIsExplicit_GetEffectiveRelativeLocation_ShouldBypassTheShift()
         {
-            var sut = new OutputLocationOptions(@"C:\Container\MyApp", relativeLocation: "..");
+            var sut = new OutputLocationOptions(MyAppDirectory, relativeLocation: "..");
 
-            sut.GetEffectiveRelativeLocation(rawRelativeLocation: @".\Chain\Chain", fallbackName: "Chain")
-                .ShouldBe(@".\Chain\Chain");
+            var explicitLocation = Path.Combine(".", "Chain", "Chain");
+            sut.GetEffectiveRelativeLocation(rawRelativeLocation: explicitLocation, fallbackName: "Chain")
+                .ShouldBe(explicitLocation);
         }
 
         [Fact]
         public void WhenRawValueIsBlank_GetEffectiveRelativeLocation_ShouldCombineShiftWithName()
         {
-            var sut = new OutputLocationOptions(@"C:\Container\Intent.Modules.Foo", relativeLocation: "..");
+            var sut = new OutputLocationOptions(Path.Combine(ContainerDirectory, "Intent.Modules.Foo"), relativeLocation: "..");
 
             sut.GetEffectiveRelativeLocation(rawRelativeLocation: null, fallbackName: "Intent.Modules.Foo")
-                .ShouldBe(@"..\Intent.Modules.Foo");
+                .ShouldBe(Path.Combine("..", "Intent.Modules.Foo"));
         }
 
         [Fact]
