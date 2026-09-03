a6051e3da76361b8b7860fe60788472b38eba92d
JP <jp.seini@intentarchitect.com>
2026-09-03 07:53:02 +0200

    Updated tests

---
+++ Tests/Blazor.InteractiveAuto.Jwt/Blazor.InteractiveAuto.Jwt/Common/ServerAuthorizationMessageHandler.cs
@@ -0,0 +1,34 @@
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.Blazor.Authentication.Templates.Server.ServerAuthorizationMessageHandlerTemplate", Version = "1.0")]
+
+namespace Blazor.InteractiveAuto.Jwt.Common
+{
+    public class ServerAuthorizationMessageHandler : DelegatingHandler
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly string[] _authorizedUrls;
+
+        public ServerAuthorizationMessageHandler(IHttpContextAccessor httpContextAccessor, string[] authorizedUrls)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _authorizedUrls = authorizedUrls;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            var context = _httpContextAccessor.HttpContext;
+            var token = context?.User?.Claims.FirstOrDefault(c => c.Type == "access_token")?.Value;
+            var requestUrl = request.RequestUri?.AbsoluteUri ?? string.Empty;
+
+            if (!string.IsNullOrEmpty(token) && _authorizedUrls.Any(url => requestUrl.StartsWith(url, StringComparison.OrdinalIgnoreCase)))
+            {
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            }
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ Tests/Blazor.InteractiveServer.Oidc/Blazor.InteractiveServer.Oidc/Common/ServerAuthorizationMessageHandler.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Intent.RoslynWeaver.Attributes;
+using Microsoft.AspNetCore.Http;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.Blazor.Authentication.Templates.Server.ServerAuthorizationMessageHandlerTemplate", Version = "1.0")]
+
+namespace Blazor.InteractiveServer.Oidc.Common
+{
+    public class ServerAuthorizationMessageHandler : DelegatingHandler
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly string[] _authorizedUrls;
+
+        public ServerAuthorizationMessageHandler(IHttpContextAccessor httpContextAccessor, string[] authorizedUrls)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _authorizedUrls = authorizedUrls;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            var context = _httpContextAccessor.HttpContext;
+            var token = context?.User?.Claims.FirstOrDefault(c => c.Type == "access_token")?.Value;
+            var requestUrl = request.RequestUri?.AbsoluteUri ?? string.Empty;
+
+            if (!string.IsNullOrEmpty(token) && _authorizedUrls.Any(url => requestUrl.StartsWith(url, StringComparison.OrdinalIgnoreCase)))
+            {
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            }
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
